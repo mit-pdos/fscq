6f8f22b84138dbbe0e7d79cfb1283c469d10061c
Haogang Chen <hchen@csail.mit.edu>
2014-05-19 05:00:01 -0400

    prove more lemmas for fun

--- src/Fs4.v
+++ src/Fs4.v
@@ -64,6 +64,108 @@ Inductive trace_legal : trace -> Prop :=
   | trace_legal_read_after_crash: forall (t:trace) (s:state),
     no_write_since_crash t -> could_persist t s -> trace_legal t -> trace_legal ((Read s) :: t).
 
+(* Some theorems *)
+
+Theorem legal_subtrace:
+  forall (t:trace) (e:event),
+  trace_legal (e :: t) -> trace_legal t.
+Proof.
+  intros.
+  inversion H; crush.
+Qed.
+
+
+Lemma last_write_uniqueness:
+  forall (t:trace) (a b:state),
+  last_write_since_crash t a /\ last_write_since_crash t b -> a = b.
+Proof.
+  crush.
+  induction H0; inversion H1; crush.
+Qed.
+
+Ltac write_contradict :=
+  match goal with
+  | H1: last_write_since_crash ?T ?s, 
+    H2: no_write_since_crash ?T |- _ =>
+      unfold no_write_since_crash in H2; destruct H2; exists s; assumption
+  | H1: last_write_since_crash ?T ?a,
+    H2: last_write_since_crash ?T ?b |- _ =>
+      apply last_write_uniqueness with (t:=T); crush
+  | _ => idtac
+  end.
+
+Theorem read_immutability:
+  forall (t:trace) (a b: state),
+  trace_legal ((Read a) :: (Read b) :: t) -> a = b.
+Proof.
+  intros.  inversion H.
+  - inversion H3; inversion H2; write_contradict.
+  - inversion H4.
+    + apply last_write_read with (rs:=b) in H7;  write_contradict.
+    + inversion H3; crush; write_contradict.
+Qed.
+
+Lemma write_complement:
+  forall (t:trace),
+  (exists (s:state), last_write_since_crash t s) \/ no_write_since_crash t.
+Proof.
+  induction t; unfold no_write_since_crash.
+  - right. crush. inversion H.
+  - unfold no_write_since_crash in IHt; destruct IHt; destruct a.
+    + left. crush. exists x. constructor. assumption.
+    + left. exists s. constructor.
+    + left. crush. exists x. constructor. assumption.
+    + right. crush. inversion H0.
+    + right. crush. inversion H0. apply H. exists x. assumption.
+    + left. exists s. constructor.
+    + right. crush. inversion H0. apply H. exists x. assumption.
+    + right. crush. inversion H0.
+Qed.
+ 
+Lemma sync_before_read_irrelevence:
+  forall (t:trace) (s:state),
+  trace_legal ((Read s) :: Sync :: t) -> trace_legal ((Read s) :: t).
+Proof.
+  intros. inversion H; inversion H3.
+  - inversion H2.
+    cut (trace_legal (Read s ::t)); crush. 
+    apply trace_legal_read_after_write; crush.
+  - destruct H2. exists s. constructor. assumption.
+  - inversion H4. apply trace_legal_read_after_crash; crush.
+Qed.
+
+Inductive could_read : trace -> state -> Prop :=
+  | could_read_nil:
+    could_read nil IS
+  | could_read_write: forall (t:trace) (s:state),
+    could_read ((Write s) :: t) s
+  | could_read_crash: forall (t:trace) (s:state),
+    could_persist t s -> could_read (Crash :: t) s
+  | could_read_read:  forall (t:trace) (s rs:state),
+    could_read t s -> could_read ((Read rs) :: t) s
+  | could_read_sync:  forall (t:trace) (s:state),
+    could_read t s -> could_read (Sync :: t) s.
+
+Theorem legal_could_read :
+  forall (t:trace) (s:state),
+  trace_legal ((Read s) :: t) -> could_read t s.
+Proof.
+  induction t; intros.
+  - inversion H. inversion H2. inversion H3. constructor.
+  - destruct a.
+    + constructor. assert (Hs:=H).
+      apply read_immutability in H.
+      apply legal_subtrace in Hs. crush.
+    + replace s0 with s. constructor. inversion H; crush.
+      * inversion H2. reflexivity.
+      * destruct H2. exists s0. constructor.
+    + constructor.
+      apply sync_before_read_irrelevence in H. crush.
+    + constructor.
+      inversion H. inversion H2. inversion H3. assumption.
+Qed.
+
+
 (* Testing *)
 
 Ltac trace_resolve :=
@@ -103,44 +205,4 @@ Theorem test_5:
 Proof.
   constructor 6; repeat trace_resolve.
   Abort.
-
-(* Some theorems *)
-
-Theorem leagl_subtrace:
-  forall (t:trace) (e:event),
-  trace_legal (e :: t) -> trace_legal t.
-Proof.
-  intros.
-  inversion H; crush.
-Qed.
-
-Lemma last_write_uniqueness:
-  forall (t:trace) (a b:state),
-  last_write_since_crash t a /\ last_write_since_crash t b -> a = b.
-Proof.
-  crush.
-  induction H0; inversion H1; crush.
-Qed.
-
-Ltac write_contradict :=
-  match goal with
-  | H1: last_write_since_crash ?T ?s, 
-    H2: no_write_since_crash ?T |- _ =>
-      unfold no_write_since_crash in H2; destruct H2; exists s; assumption
-  | H1: last_write_since_crash ?T ?a,
-    H2: last_write_since_crash ?T ?b |- _ =>
-      apply last_write_uniqueness with (t:=T); crush
-  | _ => idtac
-  end.
-
-Theorem read_immutability:
-  forall (t:trace) (a b: state),
-  trace_legal ((Read a) :: (Read b) :: t) -> a = b.
-Proof.
-  intros.  inversion H.
-  - inversion H3; inversion H2; write_contradict.
-  - inversion H4.
-    + apply last_write_read with (rs:=b) in H7;  write_contradict.
-    + inversion H3; crush; write_contradict.
-Qed.
 
