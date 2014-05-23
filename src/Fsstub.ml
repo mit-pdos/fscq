3924c8c0f929f251e50a611c98ee6690562e578b
Haogang Chen <hchen@csail.mit.edu>
2014-05-23 17:25:05 -0400

    x

--- src/Fsstub.ml
+++ src/Fsstub.ml
@@ -1,19 +1,19 @@
-open Fs4;;
-open Camlcoq;;
 open Printf;;
 
 (* I/O stubs *)
 
 let file = "fs_disk";;
 
-let _read_disk () =
+let _read_disk b =
     let ic = open_in file in
         let s = input_line ic in
-        close_in ic;;
+        close_in ic;
+    s;;
 
 let _write_disk s =
     let oc = open_out file in
         fprintf oc "%s\n" s;
-        close_out oc;;
+        close_out oc;
+    s;;
 
 
--- src/codegen/extraction.v
+++ src/codegen/extraction.v
@@ -7,8 +7,8 @@ Extraction Blacklist List String Int.
 Extract Constant state => "string".
 Extract Constant IS => """""".
 
-Extract Constant read_disk => "_read_disk".
-Extract Constant write_disk => "_write_disk".
+Extract Constant read_disk => "Fsstub._read_disk".
+Extract Constant write_disk => "Fsstub._write_disk".
 
 Cd "codegen".
 Extraction Library Fs4.
