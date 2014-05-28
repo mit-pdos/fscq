a4733b6b42b85fb4db6522c4373aa7760156eb3b
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-05-28 14:50:00 -0400

    extraction minor cleanup

--- src/codegen/extraction.v
+++ src/codegen/extraction.v
@@ -8,10 +8,12 @@ Extraction Blacklist List String Int.
 Extract Constant state => "string".
 Extract Constant IS => """""".
 
+Extract Constant st_init => "Fsstub._init_disk".
 Extract Constant st_read => "Fsstub._read_disk".
 Extract Constant st_write => "Fsstub._write_disk".
 
 Cd "codegen".
 Extraction Library Monad.
 Extraction Library Fs4.
+Extraction Library Datatypes.
 
