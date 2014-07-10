fd4db58c9422f119538b797f92d32e79501452bc
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-10 15:31:18 -0400

    Trans2.v: introduce explicit T2Begin

--- src/Trans2.v
+++ src/Trans2.v
@@ -3,11 +3,11 @@ Require Import Storage.
 Require Import Disk.
 
 Inductive t2prog :=
+  | T2Begin  (rx:t2prog)
   | T2DProg  (d:dprog) (rx:t2prog)
   | T2Commit (rx:t2prog)
   | T2Abort  (rx:t2prog)
-  | T2Halt
-  .
+  | T2Halt.
 
 Record t2state := T2St {
   T2SProg: t2prog;
@@ -18,11 +18,12 @@ Record t2state := T2St {
 
 Inductive t2smstep : t2state -> t2state -> Prop :=
   | T2smHalt: forall d ad dt,
-    t2smstep (T2St T2Halt d ad dt)          (T2St T2Halt d ad dt)
-  | T2smProg: forall d ad dt dp rx,
-    t2smstep (T2St (T2DProg dp rx) d ad dt) (T2St rx d (drun dp ad) dt)
-  | T2smCommit: forall d ad dt rx,
-    t2smstep (T2St (T2Commit rx) d ad dt)   (T2St rx ad ad false)
-  | T2smAbort: forall d ad dt rx,
-    t2smstep (T2St (T2Abort rx) d ad dt)    (T2St rx d d false)
-  .
+    t2smstep (T2St T2Halt d ad dt)            (T2St T2Halt d ad dt)
+  | T2smBegin: forall d ad rx,
+    t2smstep (T2St (T2Begin rx) d ad false)   (T2St rx d ad true)
+  | T2smProg: forall d ad dp rx,
+    t2smstep (T2St (T2DProg dp rx) d ad true) (T2St rx d (drun dp ad) true)
+  | T2smCommit: forall d ad rx,
+    t2smstep (T2St (T2Commit rx) d ad true)   (T2St rx ad ad false)
+  | T2smAbort: forall d ad rx,
+    t2smstep (T2St (T2Abort rx) d ad true)    (T2St rx d d false).
