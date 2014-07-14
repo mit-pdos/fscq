c286409aa559ad0c7e236fd6df9cfc8688ae3c12
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-14 12:17:53 -0400

    get extraction to work again

--- src/codegen/extraction.v
+++ src/codegen/extraction.v
@@ -1,20 +1,26 @@
 Require Import ExtrOcamlBasic.
 Require Import ExtrOcamlString.
 Require Import ExtrOcamlNatInt.
+Require Import Disk.
 Require Import DiskLog.
 Require Import MemLog.
 Require Import Trans.
+Require Import Trans2.
 Require Import Bank.
 Require Import Storage.
+Require Import Util.
 
 Extraction Blacklist List String Int.
 
 Cd "codegen".
+Extraction Library Disk.
 Extraction Library DiskLog.
 Extraction Library MemLog.
 Extraction Library Trans.
+Extraction Library Trans2.
 Extraction Library Bank.
 Extraction Library Storage.
+Extraction Library Util.
 Extraction Library Datatypes.
 Extraction Library Peano.
 Extraction Library Peano_dec.
