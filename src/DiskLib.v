9de3ddcbb95810b49fea63d294c18d94d8293fd2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-24 20:51:46 -0400

    checkpoint some progress on a better way of structuring programs
    this seems promising, but perhaps i went overboard with the Coq module
    system, as the module system appears to be half-baked in several ways..
    maybe i need to understand modules better, or switch to records instead.

--- src/DiskLib.v
+++ src/DiskLib.v
@@ -4,7 +4,18 @@ Require Import Closures.
 Require Import Util.
 Require Import Arith.
 Require Import Smallstep.
-Open Scope fscq.
+
+
+(* Common notation *)
+
+Notation "a ;; b" := (progseq1 a (fun _: unit => b))
+  (right associativity, at level 60) : fscq'_scope.
+
+Notation "ra <- a ; b" := (progseq2 a (fun ra => b))
+  (right associativity, at level 60) : fscq'_scope.
+
+Delimit Scope fscq'_scope with fscq'.
+Open Scope fscq'.
 
 
 (* Common infrastructure for nested programs *)
@@ -33,81 +44,104 @@ Inductive progmatch {R:Type} {P1 P2:Type->Type} {S1 S2:Type}
 Hint Constructors progmatch.
 
 
-Section DISK.
+Module Type SmallStepLang.
 
-Inductive diskprog {R:Type} : Type :=
-  | DRead (b:nat) (rx:nat->diskprog)
-  | DWrite (b:nat) (v:nat) (rx:diskprog)
-  | DReturn (v:R).
+Parameter Prog: Type->Type.
+Parameter ReturnOp: forall (R:Type), R->(Prog R).
+Parameter State: Type.
+Parameter Step: forall (R:Type), @progstate R (@Prog) State ->
+                                 @progstate R (@Prog) State -> Prop.
 
-Definition diskstate := nat -> nat.
-
-Inductive diskstep {R:Type} : @progstate R (@diskprog) diskstate ->
-                              @progstate R (@diskprog) diskstate -> Prop :=
-  | StepDRead: forall d b rx,
-    diskstep (PS (DRead b rx) d)
-             (PS (rx (d b)) d)
-  | StepDWrite: forall d b v rx,
-    diskstep (PS (DWrite b v rx) d)
-             (PS rx (setidx eq_nat_dec d b v)).
-
-Definition diskreturns {R:Type} := progreturns diskstep (@DReturn R).
-
-End DISK.
+End SmallStepLang.
 
 
-Section PAIR.
+Module Type Refines (L1 L2: SmallStepLang).
 
-Inductive pairprog {R:Type} : Type :=
-  | PRead (n:nat) (rx:(nat*nat)->pairprog)
-  | PWrite (n:nat) (p:nat*nat) (rx:pairprog)
-  | PReturn (v:R).
+Parameter Compile: forall (R:Type), L1.Prog R -> L2.Prog R.
+Parameter StateMatch: L1.State -> L2.State -> Prop.
+Axiom FSim: forall R, forward_simulation (L1.Step R) (L2.Step R).
 
-Definition pairstate := nat -> (nat*nat).
-
-Inductive pairstep {R:Type} : @progstate R (@pairprog) pairstate ->
-                              @progstate R (@pairprog) pairstate -> Prop :=
-  | StepPRead: forall d b rx,
-    pairstep (PS (PRead b rx) d)
-             (PS (rx (d b)) d)
-  | StepPWrite: forall d b v rx,
-    pairstep (PS (PWrite b v rx) d)
-             (PS rx (setidx eq_nat_dec d b v)).
-
-Definition pairreturns {R:Type} := progreturns pairstep (@PReturn R).
-
-End PAIR.
+End Refines.
 
 
-Section PAIR_ON_DISK.
+Module Disk <: SmallStepLang.
 
-Fixpoint compile_pair_disk {R:Type} (p:@pairprog R) : diskprog :=
+Inductive prog {R:Type} : Type :=
+  | Read (b:nat) (rx:nat->prog)
+  | Write (b:nat) (v:nat) (rx:unit->prog)
+  | Return (v:R).
+Definition Prog := @prog.
+Definition ReturnOp := @Return.
+Definition State := nat -> nat.
+
+Inductive step {R:Type} : @progstate R Prog State ->
+                          @progstate R Prog State -> Prop :=
+  | StepRead: forall d b rx,
+    step (PS (Read b rx) d)
+         (PS (rx (d b)) d)
+  | StepWrite: forall d b v rx,
+    step (PS (Write b v rx) d)
+         (PS (rx tt) (setidx eq_nat_dec d b v)).
+Definition Step := @step.
+
+End Disk.
+
+
+Module Pair <: SmallStepLang.
+
+Inductive prog {R:Type} : Type :=
+  | Read (n:nat) (rx:(nat*nat)->prog)
+  | Write (n:nat) (p:nat*nat) (rx:unit->prog)
+  | Return (v:R).
+Definition Prog := @prog.
+Definition ReturnOp := @Return.
+Definition State := nat -> (nat*nat).
+
+Inductive step {R:Type} : @progstate R Prog State ->
+                          @progstate R Prog State -> Prop :=
+  | StepRead: forall d b rx,
+    step (PS (Read b rx) d)
+         (PS (rx (d b)) d)
+  | StepWrite: forall d b v rx,
+    step (PS (Write b v rx) d)
+         (PS (rx tt) (setidx eq_nat_dec d b v)).
+Definition Step := @step.
+
+End Pair.
+
+
+Module PairToDisk <: Refines Pair Disk.
+
+Fixpoint Compile {R:Type} (p:@Pair.Prog R) : (Disk.Prog R) :=
   match p with
-  | PRead n rx => a <- DRead (2*n) ; b <- DRead (2*n+1) ; compile_pair_disk (rx (a, b))
-  | PWrite n v rx => let (a,b) := v in DWrite (2*n) a ;; DWrite (2*n+1) b ;; compile_pair_disk rx
-  | PReturn r => DReturn r
+  | Pair.Read n rx =>
+    a <- Disk.Read (2*n) ; b <- Disk.Read (2*n+1) ; Compile (rx (a, b))
+  | Pair.Write n v rx =>
+    let (a,b) := v in Disk.Write (2*n) a ;; Disk.Write (2*n+1) b ;; Compile (rx tt)
+  | Pair.Return r =>
+    Disk.Return r
   end.
 
-Inductive statematch_pair_disk : pairstate -> diskstate -> Prop :=
-  | PairDiskStateMatch: forall p d
+Inductive statematch : Pair.State -> Disk.State -> Prop :=
+  | Match: forall p d
     (M: forall n, p n = (d (2*n), d (2*n+1))),
-    statematch_pair_disk p d.
+    statematch p d.
+Definition StateMatch := statematch.
+Hint Constructors statematch.
 
-Hint Constructors statematch_pair_disk.
-
-Theorem pair_disk_fsim:
+Theorem FSim:
   forall R,
-  forward_simulation (@pairstep R) (@diskstep R).
+  forward_simulation (Pair.Step R) (Disk.Step R).
 Proof.
-  intros; exists (progmatch compile_pair_disk statematch_pair_disk); intros.
+  intros; exists (progmatch Compile statematch); intros.
 
   repeat match goal with
   | [ x: progmatch _ _ _ _ |- _ ] => inversion x; clear x; subst
-  | [ x: statematch_pair_disk _ _ |- _ ] => inversion x; clear x; subst
+  | [ x: statematch _ _ |- _ ] => inversion x; clear x; subst
   end.
 
   match goal with
-  | [ x: pairstep _ _ |- _ ] => inversion x; clear x; subst
+  | [ x: Pair.Step _ _ _ |- _ ] => inversion x; clear x; subst
   end.
 
   - (* PRead *)
@@ -125,63 +159,177 @@ Proof.
       destruct (eq_nat_dec b n1); repeat resolve_setidx omega; crush.
 Qed.
 
-End PAIR_ON_DISK.
+End PairToDisk.
 
 
-Section DUAL_PROG.
+Module RefineSelf (L: SmallStepLang) <: Refines L L.
 
-(* XXX *)
+Definition Compile (R:Type) (p:L.Prog R) := p.
 
-End DUAL_PROG.
+Inductive statematch : L.State -> L.State -> Prop :=
+  | Match: forall s, statematch s s.
+Definition StateMatch := statematch.
+Hint Constructors statematch.
 
+Theorem FSim:
+  forall R,
+  forward_simulation (L.Step R) (L.Step R).
+Proof.
+  intros; exists (@progmatch R L.Prog L.Prog L.State L.State (Compile R) statematch); intros.
 
-Section SINGLE_DISK_PROG.
+  repeat match goal with
+  | [ x: progmatch _ _ _ _ |- _ ] => inversion x; clear x; subst
+  | [ x: statematch _ _ |- _ ] => inversion x; clear x; subst
+  end.
 
-Inductive singlediskprog :=
-  | SDOp {R:Type} (o:diskop R) (rx:R->singlediskprog)
-  | SDHalt.
+  exists A2; split.
+  - eapply star_step; [ apply H | apply star_refl ].
+  - destruct A2. crush.
+Qed.
 
-Record singlediskstate := SDSt {
-  SDProg: singlediskprog;
-  SDState: diskstate
-}.
-
-Inductive singlediskstep : singlediskstate -> singlediskstate -> Prop :=
-  | StepSDOp: forall R op (r:R) rx s s'
-    (OS: diskstep R op s s' r),
-    singlediskstep (SDSt (SDOp op rx) s)
-                   (SDSt (rx r) s')
-  | StepSDHalt: forall s,
-    singlediskstep (SDSt SDHalt s)
-                   (SDSt SDHalt s).
-
-End SINGLE_DISK_PROG.
+End RefineSelf.
 
 
-Section DOUBLE_DISK_PROG.
+Module DualProg (Left: SmallStepLang) (Right: SmallStepLang) <: SmallStepLang.
 
-Inductive doublediskprog :=
-  | DDOp1 {R:Type} (o:diskop R) (rx:R->doublediskprog)
-  | DDOp2 {R:Type} (o:diskop R) (rx:R->doublediskprog)
-  | DDHalt.
+Inductive prog {R:Type} : Type :=
+  | DoLeft {T:Type} (p:Left.Prog T) (rx:T->prog)
+  | DoRight {T:Type} (p:Right.Prog T) (rx:T->prog)
+  | Return (r:R).
+Definition Prog := @prog.
+Definition ReturnOp := @Return.
 
-Record doublediskstate := DDSt {
-  DDProg: doublediskprog;
-  DDState1: diskstate;
-  DDState2: diskstate
-}.
+Inductive state :=
+  | DualState (l:Left.State) (r:Right.State).
+Definition State := state.
 
-Inductive doublediskstep : doublediskstate -> doublediskstate -> Prop :=
-  | StepDDOp1: forall R op (r:R) rx s1 s1' s2
-    (OS: diskstep R op s1 s1' r),
-    doublediskstep (DDSt (DDOp1 op rx) s1 s2)
-                   (DDSt (rx r) s1' s2)
-  | StepDDOp2: forall R op (r:R) rx s1 s2 s2'
-    (OS: diskstep R op s2 s2' r),
-    doublediskstep (DDSt (DDOp2 op rx) s1 s2)
-                   (DDSt (rx r) s1 s2')
-  | StepDDHalt: forall s1 s2,
-    doublediskstep (DDSt DDHalt s1 s2)
-                   (DDSt DDHalt s1 s2).
+Inductive step {R:Type} : @progstate R Prog State ->
+                          @progstate R Prog State -> Prop :=
+  | DualStepLeft: forall p rx ls ls' rs r
+    (LS: progreturns (Left.Step R) (Left.ReturnOp R) p ls ls' r),
+    step (PS (DoLeft p rx) (DualState ls rs))
+         (PS (rx r) (DualState ls' rs))
+  | DualStepRight: forall p rx ls rs rs' r
+    (RS: progreturns (Right.Step R) (Right.ReturnOp R) p rs rs' r),
+    step (PS (DoRight p rx) (DualState ls rs))
+         (PS (rx r) (DualState ls rs')).
+Definition Step := @step.
 
-End DOUBLE_DISK_PROG.
+End DualProg.
+
+
+Module FSimReturn (L1 L2: SmallStepLang)
+                  (Ref12: Refines L1 L2).
+
+(* XXX need to prove that forward simulation implies same return value *)
+
+Theorem fsim_implies_returns:
+  forall (R:Type) p s1 s1' s2 r,
+  progreturns (L1.Step R) (L1.ReturnOp R) p s1 s1' r ->
+  exists s2',
+  progreturns (L2.Step R) (L2.ReturnOp R) (Ref12.Compile R p) s2 s2' r /\
+  Ref12.StateMatch s1' s2'.
+Proof.
+  admit.
+Qed.
+
+End FSimReturn.
+
+
+(* XXX Module system limitation:
+
+Module RefineDual (L1 R1 L2 R2: SmallStepLang)
+                  (L12: Refines L1 L2)
+                  (R12: Refines R1 R2) <: Refines (DualProg L1 R1) (DualProg L2 R2).
+
+*)
+
+Module RefineDual (L1 R1 L2 R2: SmallStepLang)
+                  (L12: Refines L1 L2)
+                  (R12: Refines R1 R2).
+
+Module DP1 := DualProg L1 R1.
+Module DP2 := DualProg L2 R2.
+
+Module FSR_L := FSimReturn L1 L2 L12.
+Module FSR_R := FSimReturn R1 R2 R12.
+
+Fixpoint Compile {R:Type} (p:@DP1.Prog R) : @DP2.Prog R :=
+  match p with
+  | DP1.DoLeft T p rx => DP2.DoLeft (L12.Compile T p) (fun x => Compile (rx x))
+  | DP1.DoRight T p rx => DP2.DoRight (R12.Compile T p) (fun x => Compile (rx x))
+  | DP1.Return v => DP2.Return v
+  end.
+
+Inductive statematch : DP1.State -> DP2.State -> Prop :=
+  | Match: forall l1 r1 l2 r2
+    (ML: L12.StateMatch l1 l2)
+    (MR: R12.StateMatch r1 r2),
+    statematch (DP1.DualState l1 r1) (DP2.DualState l2 r2).
+Definition StateMatch := statematch.
+Hint Constructors statematch.
+
+Theorem FSim:
+  forall R,
+  forward_simulation (DP1.Step R) (DP2.Step R).
+Proof.
+  intros; exists (progmatch Compile statematch); intros.
+
+  repeat match goal with
+  | [ x: progmatch _ _ _ _ |- _ ] => inversion x; clear x; subst
+  | [ x: statematch _ _ |- _ ] => inversion x; clear x; subst
+  end.
+
+  match goal with
+  | [ x: DP1.Step _ _ _ |- _ ] => inversion x; clear x; subst
+  end.
+
+  - (* DoLeft *)
+    destruct FSR_L.fsim_implies_returns with
+      (R:=R) (p:=p) (s1:=l1) (s1':=ls') (s2:=l2) (r:=r); auto. Tactics.destruct_pairs.
+    eexists; split.
+    + eapply star_step; [constructor; eauto|apply star_refl].
+    + constructor; [|constructor]; auto.
+  - (* DoRight *)
+    destruct FSR_R.fsim_implies_returns with
+      (R:=R) (p:=p) (s1:=r1) (s1':=rs') (s2:=r2) (r:=r); auto. Tactics.destruct_pairs.
+    eexists; split.
+    + eapply star_step; [constructor; eauto|apply star_refl].
+    + constructor; [|constructor]; auto.
+Qed.
+
+End RefineDual.
+
+
+(* An example of how to write a program in a dual language *)
+Module DDisk := DualProg Disk Disk.
+Definition example_dd_prog : DDisk.Prog nat :=
+  DDisk.DoLeft (Disk.Write 0 5 ;; Disk.Write 1 6 ;; Disk.Return tt) ;;
+  a <- DDisk.DoLeft (x <- Disk.Read 0 ; Disk.Return x) ;
+  b <- DDisk.DoRight (Disk.Write 2 a ;; x <- Disk.Read 2 ; Disk.Return x) ;
+  DDisk.Return b.
+
+Module PairDisk := DualProg Pair Disk.
+Definition example_pd_prog : PairDisk.Prog nat :=
+  PairDisk.DoLeft (Pair.Write 0 (1,2) ;; Pair.Return tt) ;;
+  a <- PairDisk.DoLeft (x <- Pair.Read 0 ; Pair.Return x) ;
+  b <- PairDisk.DoRight (Disk.Write 7 (fst a) ;; x <- Disk.Read 7 ; Disk.Return x) ;
+  PairDisk.Return b.
+
+Module DiskToDisk := RefineSelf Disk.
+Module PairDiskToDDisk := RefineDual Pair Disk Disk Disk PairToDisk DiskToDisk.
+
+(* XXX Module system wart: PairDiskToDDisk.DP1 <> PairDisk *)
+Definition example_pd_prog' : PairDiskToDDisk.DP1.Prog nat :=
+  PairDiskToDDisk.DP1.DoLeft (Pair.Write 0 (1,2) ;; Pair.Return tt) ;;
+  a <- PairDiskToDDisk.DP1.DoLeft (x <- Pair.Read 0 ; Pair.Return x) ;
+  b <- PairDiskToDDisk.DP1.DoRight (Disk.Write 7 (fst a) ;; x <- Disk.Read 7 ; Disk.Return x) ;
+  PairDiskToDDisk.DP1.Return b.
+
+Eval compute in PairDiskToDDisk.Compile example_pd_prog'.
+(*
+ * Cool: even though I never wrote how to compile (Pair+Disk) -> (Disk+Disk),
+ * and never proved that it correctly refines, this module figures out how to
+ * compile these programs, and constructs a proof of correct refinement (as
+ * long as each component refines).
+ *)
