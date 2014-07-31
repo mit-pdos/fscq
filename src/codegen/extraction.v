ae38e58af39b657a5c94224130a2dcd6f128c3f6
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-07-31 14:10:00 -0400

    extract and run new hoare-logic-based code

--- src/codegen/extraction.v
+++ src/codegen/extraction.v
@@ -1,38 +1,9 @@
 Require Import ExtrOcamlBasic.
 Require Import ExtrOcamlString.
 Require Import ExtrOcamlNatInt.
-Require Import Disk.
-Require Import DiskLog.
-Require Import MemLog.
-Require Import Trans.
-Require Import Trans2.
-Require Import Storage.
-Require Import Util.
-Require Import FileSpec.
-Require Import FsLayout.
-Require Import Balloc.
-Require Import File.
+Require Import HoareLogicN.
 
 Extraction Blacklist List String Int.
 
 Cd "codegen".
-Extraction Library Disk.
-Extraction Library DiskLog.
-Extraction Library MemLog.
-Extraction Library Trans.
-Extraction Library Trans2.
-Extraction Library Storage.
-Extraction Library Util.
-Extraction Library FileSpec.
-Extraction Library FsLayout.
-Extraction Library Balloc.
-Extraction Library File.
-
-Extraction Library Datatypes.
-Extraction Library Peano.
-Extraction Library Peano_dec.
-Extraction Library Compare_dec.
-Extraction Library NPeano.
-Extraction Library Bool.
-Extraction Library Div2.
-
+Recursive Extraction Library HoareLogicN.
--- src/tests/fs.ml
+++ src/tests/fs.ml
@@ -1,18 +1,11 @@
-open Disk
-open DiskLog
+open HoareLogicN
 open Printf
-open Datatypes
-open FileSpec
-open FsLayout
 open Sys
 
-let choose_disk d =
-  match d with
-  | NDataDisk -> "disk-data"
-  | NLogDisk  -> "disk-log"
+let disk_fn = "disk.img"
 
-let read_disk d b =
-  let ic = open_in_gen [Open_rdonly;Open_creat] 0o666 (choose_disk d) in
+let read_disk b =
+  let ic = open_in_gen [Open_rdonly;Open_creat] 0o666 disk_fn in
   seek_in ic b;
   try
     let v = input_byte ic in
@@ -21,58 +14,42 @@ let read_disk d b =
   with
     End_of_file -> close_in ic; 0
 
-let write_disk d b v =
-  let oc = open_out_gen [Open_wronly;Open_creat] 0o666 (choose_disk d) in
+let write_disk b v =
+  let oc = open_out_gen [Open_wronly;Open_creat] 0o666 disk_fn in
   seek_out oc b;
   output_byte oc v;
   close_out oc
 
-let rec run_dcode2_real p =
-  match p with
-  | DiskLog.DRead (d, b, rx) ->
-    let v = read_disk d b in
-    Printf.printf "read(%s, %d): %d\n" (choose_disk d) b v;
-    run_dcode2_real (rx v)
-  | DiskLog.DWrite (d, b, v, rx) ->
-    Printf.printf "write(%s, %d, %d)\n" (choose_disk d) b v;
-    write_disk d b v; run_dcode2_real rx
-  | DiskLog.DHalt -> 0
-
-let rec run_dcode_real p =
-  match p with
-  | Disk.DRead (b, rx) ->
-    let v = read_disk NDataDisk b in
+let rec run_dcode = function
+  | HoareLogicN.Halt_ r ->
+    r
+  | HoareLogicN.Crash_ ->
+    Printf.printf "crash\n";
+    raise Exit
+  | HoareLogicN.Read b ->
+    let v = read_disk b in
     Printf.printf "read(%d): %d\n" b v;
-    run_dcode_real (rx v)
-  | Disk.DWrite (b, v, rx) ->
+    Obj.magic v
+  | HoareLogicN.Write (b, v) ->
     Printf.printf "write(%d, %d)\n" b v;
-    write_disk NDataDisk b v; run_dcode_real rx
-  | Disk.DHalt -> 0
+    write_disk b v;
+    Obj.magic ()
+  | HoareLogicN.Seq (p1, p2) ->
+    let v = run_dcode (Obj.magic p1) in
+    run_dcode (Obj.magic p2 v);;
 
-let fcode = FileSpec.FCommon (FileSpec.FAlloc,
-            fun o -> match Obj.magic o with
-            | None -> FileSpec.FHalt
-            | Some f -> FileSpec.FCommon (FileSpec.FTrunc (f, 2),
-            fun x -> FileSpec.FCommon (FileSpec.FWrite (f, 0, 7),
-            fun x -> FileSpec.FCommon (FileSpec.FWrite (f, 1, 8),
-            fun x -> FileSpec.FHalt))));;
+let xp =
+  { coq_DataStart = 0;
+    coq_DataLen = 20;
+    coq_LogLength = 20;
+    coq_LogCommit = 21;
+    coq_LogStart = 22;
+    coq_LogLen = 20;
+    coq_Temp = 42 };;
 
-let dcode = FsLayout.compile_id (FsLayout.do_init (Balloc.compile_bi (Balloc.do_init (File.compile_fb fcode))));;
+Printf.printf "Running write_two_blocks on disk..\n";;
 
-let t2code = Trans2.T2Begin (Trans2.T2DProg (dcode, Trans2.T2Commit (Trans2.T2Halt)));;
+try Sys.remove disk_fn with Sys_error _ -> ();;
 
-let dcode2 = DiskLog.compile_pd (MemLog.compile_tp (Trans.compile_t2t t2code));;
-
-Printf.printf "Running fcode on bare disk..\n";;
-
-try Sys.remove (choose_disk NDataDisk) with Sys_error _ -> ();;
-
-run_dcode_real dcode;;
-
-Printf.printf "Running fcode on logged disk..\n";;
-
-try Sys.remove (choose_disk NDataDisk) with Sys_error _ -> ();;
-try Sys.remove (choose_disk NLogDisk)  with Sys_error _ -> ();;
-
-run_dcode2_real dcode2;;
+run_dcode (write_two_blocks xp 3 4 5 6);;
 
