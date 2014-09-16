d1a318acff233a1c9b12fed8668d17dff16561ad
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-09-16 04:18:09 +0000

    set more coqide flags for perhaps more parallelism

--- src/coqide.sh
+++ src/coqide.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 # set COQIDE_JOBS to the number of coqtop processes that coqide
 # should run in parallel; e.g., 8 on an 8-core system.
-exec coqide -async-proofs-j ${COQIDE_JOBS:-1} -R . Fscq "$@"
+exec coqide -async-proofs-j ${COQIDE_JOBS:-1} \
+            -async-proofs-tac-j ${COQIDE_JOBS:-1} \
+            -async-proofs-always-delegate \
+            -R . Fscq "$@"
