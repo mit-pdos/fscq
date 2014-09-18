4010f1ddd1ae44a53941cd07d540e0aa3246fa32
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-09-18 14:41:14 -0400

    Idempotent.v: checkpoint some ideas
    trying to prove [preserves_precondition] from hoare triples in
    [corr_to_pp], so that we might be able to avoid building a parallel
    universe for reasoning about [exec] in addition to the current set
    of tools for reasoning about [exec_recover].  not quite sure if
    this will work yet.

--- src/Idempotent.v
+++ src/Idempotent.v
@@ -2,22 +2,14 @@ Require Import Hoare.
 Require Import Prog.
 Require Import Pred.
 
-Parameter recover : prog -> prog.
-Parameter rpre : pred.
-Parameter rpost : pred.
-Parameter rpost2 : pred.
-
 Definition preserves_precondition (pre : pred) p :=
   forall m m' out, pre m -> exec m p m' out -> pre m' /\ out <> Failed.
 
-Axiom recover_preserves: forall rx, preserves_precondition rpre (recover rx).
-
-Theorem idempotent_ok : forall p p1 p2,
-  preserves_precondition rpre p ->
+Theorem idempotent_ok' : forall p p1 p2 pre,
+  preserves_precondition pre p ->
   p1 = p ->
   p2 = p ->
-  {{ rpre
-  }} p1 >> p2.
+  {{ pre }} p1 >> p2.
 Proof.
   unfold corr at 1.
   intros.
@@ -30,3 +22,35 @@ Proof.
   apply IHexec_recover; auto.
   edestruct H; eauto.
 Qed.
+
+Theorem idempotent_ok : forall p pre,
+  preserves_precondition pre p ->
+  {{ pre }} p >> p.
+Proof.
+  intros.
+  eapply idempotent_ok'; eauto.
+Qed.
+
+Parameter recover : prog -> prog.
+Parameter rpre : pred.
+
+Axiom recover_preserves : forall rx, preserves_precondition rpre (recover rx).
+
+Theorem recover_idempotent : forall rx,
+  {{ rpre }} recover rx >> recover rx.
+Proof.
+  intros.
+  apply idempotent_ok.
+  apply recover_preserves.
+Qed.
+
+Theorem corr_to_pp : forall p1 p2 pre1 pre2,
+  {{ pre1 }} p1 >> p2 ->
+  (pre1 ==> [ {{ pre2 }} p2 >> p2 ]) ->
+  (pre1 ==> [ pre2 ==> pre1 ]) ->
+  preserves_precondition pre1 p1.
+Proof.
+  unfold preserves_precondition.
+  intros.
+  unfold corr in H.
+  
