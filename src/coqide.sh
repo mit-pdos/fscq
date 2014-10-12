3e3d63724183cc065d1c0ad0a67b6af2ec3f277f
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-10-11 20:22:35 -0400

    coqide.sh note

--- src/coqide.sh
+++ src/coqide.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # set COQIDE_JOBS to the number of coqtop processes that coqide
 # should run in parallel; e.g., 8 on an 8-core system.
+#
+# other useful flags that might be worth passing:
+#
+#   -async-proofs lazy
+#
 exec coqide -async-proofs-j ${COQIDE_JOBS:-1} \
             -async-proofs-tac-j ${COQIDE_JOBS:-1} \
             -async-proofs-always-delegate \
