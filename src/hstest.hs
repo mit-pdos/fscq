b109fa4dc277df9f3dec258c3c3c81cb8d84b5fd
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-10-17 00:14:14 -0400

    get extraction working again

--- src/hstest.hs
+++ src/hstest.hs
@@ -39,8 +39,7 @@ write_disk f (W64 a) (W4096 v) = do
 write_disk _ _ _ = error "write_disk: unexpected addr or val"
 
 run_dcode :: Handle -> Prog.Coq_prog -> IO (Prelude.Maybe Word.Coq_word)
-run_dcode _ (Done _ r) = return $ unsafeCoerce r
-run_dcode f (CheckID _ rx) = run_dcode f $ rx ()
+run_dcode _ (Done r) = return $ unsafeCoerce r
 run_dcode f (Read a rx) = do val <- read_disk f a; run_dcode f $ rx val
 run_dcode f (Write a v rx) = do write_disk f a v; run_dcode f $ rx ()
 
@@ -51,7 +50,7 @@ the_prog xp =
   _LOG__read xp (W64 5) $ \v ->
   _LOG__write xp (W64 6) v $ \_ ->
   _LOG__commit xp $ \_ ->
-  Prog.Done () $ unsafeCoerce Nothing
+  Prog.Done $ unsafeCoerce Nothing
 
 lxp :: Log.Coq_xparams
 lxp = Log.Build_xparams
@@ -70,8 +69,8 @@ main = do
   putStrLn "Running program.."
   f <- openFile disk_fn ReadWriteMode
   -- r <- run_dcode f $ the_prog lxp
-  -- r <- run_dcode f $ Testprog.testcopy lxp $ Prog.Done () $ unsafeCoerce Nothing
-  r <- run_dcode f $ Testprog.testalloc lxp bxp $ \x -> Prog.Done () $ unsafeCoerce x
+  -- r <- run_dcode f $ Testprog.testcopy lxp $ Prog.Done $ unsafeCoerce Nothing
+  r <- run_dcode f $ Testprog.testalloc lxp bxp $ \x -> Prog.Done $ unsafeCoerce x
   hClose f
   putStrLn $ "Done: " ++ (show r)
 
