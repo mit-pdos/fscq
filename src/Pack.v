4b93f9e31411b238d5209d5fbc8392bac98537f8
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-11-05 17:19:46 -0500

    Pack.v: use a Section instead of a Module
    using a Module required the use of another module to pass in
    the expected item size, which in turn made the item size too
    opaque for the caller of Pack.v to manipulate.

--- src/Pack.v
+++ src/Pack.v
@@ -3,43 +3,40 @@ Require Import Prog.
 Require Import Arith.
 Require Import Eqdep.
 
+Set Implicit Arguments.
 
-Module Type ItemSize.
-  Parameter itemsz : nat.
-  Parameter items_per_valu : addr.
-  Axiom itemsz_ok : wordToNat items_per_valu * itemsz = valulen.
-  (* This is in lieu of actually being able to do division.. *)
-End ItemSize.
+Section Packer.
+  Variable itemsz : nat.
+  Variable items_per_valu : addr.
+  Variable itemsz_ok : wordToNat items_per_valu * itemsz = valulen.
 
-
-Module Packer (IS: ItemSize).
-  Theorem extract_len : forall pos, (pos < IS.items_per_valu)%word
-    -> wordToNat pos * IS.itemsz + IS.itemsz + (valulen - wordToNat pos * IS.itemsz - IS.itemsz)
+  Theorem extract_len : forall pos, (pos < items_per_valu)%word
+    -> wordToNat pos * itemsz + itemsz + (valulen - wordToNat pos * itemsz - itemsz)
        = valulen.
   Proof.
     intros pos H. rewrite <- Nat.sub_add_distr. rewrite le_plus_minus_r.
-    reflexivity. rewrite <- Nat.mul_succ_l. rewrite <- IS.itemsz_ok.
+    reflexivity. rewrite <- Nat.mul_succ_l. rewrite <- itemsz_ok.
     apply mult_le_compat_r. apply Nat.le_succ_l. apply wlt_lt. assumption.
   Qed.
 
-  Definition extract (v : valu) (pos : addr) : word IS.itemsz.
-    refine (if wlt_dec pos IS.items_per_valu then _ else wzero IS.itemsz).
-    refine (let below := (wordToNat pos) * IS.itemsz in _).
-    refine (let above := valulen - below - IS.itemsz in _).
+  Definition extract (v : valu) (pos : addr) : word itemsz.
+    refine (if wlt_dec pos items_per_valu then _ else wzero itemsz).
+    refine (let below := (wordToNat pos) * itemsz in _).
+    refine (let above := valulen - below - itemsz in _).
     erewrite <- extract_len in v by eassumption.
-    refine (let v_lower := split1 (below + IS.itemsz) above v in _).
-    refine (let result := split2 below IS.itemsz v_lower in _).
+    refine (let v_lower := split1 (below + itemsz) above v in _).
+    refine (let result := split2 below itemsz v_lower in _).
     exact result.
   Defined.
 
-  Definition update (v : valu) (pos : addr) (n : word IS.itemsz) : valu.
-    refine (if wlt_dec pos IS.items_per_valu then _ else v).
-    refine (let below := (wordToNat pos) * IS.itemsz in _).
-    refine (let above := valulen - below - IS.itemsz in _).
+  Definition update (v : valu) (pos : addr) (n : word itemsz) : valu.
+    refine (if wlt_dec pos items_per_valu then _ else v).
+    refine (let below := (wordToNat pos) * itemsz in _).
+    refine (let above := valulen - below - itemsz in _).
     erewrite <- extract_len in v by eassumption.
-    refine (let v_above := split2 (below + IS.itemsz) above v in _).
-    refine (let v_lower := split1 (below + IS.itemsz) above v in _).
-    refine (let v_below := split1 below IS.itemsz v_lower in _).
+    refine (let v_above := split2 (below + itemsz) above v in _).
+    refine (let v_lower := split1 (below + itemsz) above v in _).
+    refine (let v_below := split1 below itemsz v_lower in _).
     refine (let result := combine (combine v_below n) v_above in _).
     subst below; subst above.
     rewrite extract_len in result by eassumption.
@@ -49,31 +46,31 @@ Module Packer (IS: ItemSize).
   (* extract' and update' avoid rewriting types, so they have no eq_rect terms,
    * but instead they have dependent matches..  unclear which is worse.
    *)
-  Definition extract' (v : valu) (pos : addr) : word IS.itemsz.
-    refine (if wlt_dec pos IS.items_per_valu then _ else wzero IS.itemsz).
-    refine (let below := (wordToNat pos) * IS.itemsz in _).
-    refine (let above := valulen - below - IS.itemsz in _).
-    refine (let v' := match (eq_sym (extract_len pos w)) in _ = N return word N with
+  Definition extract' (v : valu) (pos : addr) : word itemsz.
+    refine (if wlt_dec pos items_per_valu then _ else wzero itemsz).
+    refine (let below := (wordToNat pos) * itemsz in _).
+    refine (let above := valulen - below - itemsz in _).
+    refine (let v' := match (eq_sym (extract_len w)) in _ = N return word N with
                       | refl_equal => v
                       end in _).
-    refine (let v_lower := split1 (below + IS.itemsz) above v' in _).
-    refine (let result := split2 below IS.itemsz v_lower in _).
+    refine (let v_lower := split1 (below + itemsz) above v' in _).
+    refine (let result := split2 below itemsz v_lower in _).
     exact result.
   Defined.
 
-  Definition update' (v : valu) (pos : addr) (n : word IS.itemsz) : valu.
-    refine (if wlt_dec pos IS.items_per_valu then _ else v).
-    refine (let below := (wordToNat pos) * IS.itemsz in _).
-    refine (let above := valulen - below - IS.itemsz in _).
-    refine (let v' := match (eq_sym (extract_len pos w)) in _ = N return word N with
+  Definition update' (v : valu) (pos : addr) (n : word itemsz) : valu.
+    refine (if wlt_dec pos items_per_valu then _ else v).
+    refine (let below := (wordToNat pos) * itemsz in _).
+    refine (let above := valulen - below - itemsz in _).
+    refine (let v' := match (eq_sym (extract_len w)) in _ = N return word N with
                       | refl_equal => v
                       end in _).
-    refine (let v_above := split2 (below + IS.itemsz) above v' in _).
-    refine (let v_lower := split1 (below + IS.itemsz) above v' in _).
-    refine (let v_below := split1 below IS.itemsz v_lower in _).
+    refine (let v_above := split2 (below + itemsz) above v' in _).
+    refine (let v_lower := split1 (below + itemsz) above v' in _).
+    refine (let v_below := split1 below itemsz v_lower in _).
     refine (let result := combine (combine v_below n) v_above in _).
     subst below; subst above.
-    refine (let result' := match (extract_len pos w) in _ = N return word N with
+    refine (let result' := match (extract_len w) in _ = N return word N with
                            | refl_equal => result
                            end in _).
     exact result'.
@@ -91,12 +88,12 @@ Module Packer (IS: ItemSize).
   Qed.
 
   Theorem extract_same : forall v pos pos' n, pos = pos'
-    -> (pos < IS.items_per_valu)%word
+    -> (pos < items_per_valu)%word
     -> extract (update v pos' n) pos = n.
   Proof.
     intros; subst pos'.
     unfold extract, update.
-    destruct (wlt_dec pos IS.items_per_valu); try congruence.
+    destruct (wlt_dec pos items_per_valu); try congruence.
 
     unfold eq_rec_r, eq_rec.
     repeat rewrite eq_rect_double.
@@ -112,8 +109,8 @@ Module Packer (IS: ItemSize).
   Proof.
     intros.
     unfold extract, update.
-    destruct (wlt_dec pos IS.items_per_valu); auto.
-    destruct (wlt_dec pos' IS.items_per_valu); auto.
+    destruct (wlt_dec pos items_per_valu); auto.
+    destruct (wlt_dec pos' items_per_valu); auto.
 
     unfold eq_rec_r, eq_rec.
     repeat rewrite eq_rect_double.
