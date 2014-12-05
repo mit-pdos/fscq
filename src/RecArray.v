495f138e385732e87f779c91103f0958b46e4256
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-12-05 09:30:28 -0800

    checkpoint

--- src/RecArray.v
+++ src/RecArray.v
@@ -7,6 +7,7 @@ Require Import List.
 Require Import Pack.
 Require Import Eqdep_dec.
 Require Import Arith.
+Require Import Omega.
 
 (**
  * A variant of array that packs multiple items into a single disk block.
@@ -20,14 +21,14 @@ Section RECARRAY.
   Definition blocktype : Rec.type := Rec.ArrayF itemtype (wordToNat items_per_valu).
   Definition block := Rec.data blocktype.
   Definition block_zero := @Rec.of_word blocktype $0.
-  Variable blocksz : Rec.len blocktype = valulen.
+  Variable blocksz_ok : Rec.len blocktype = valulen.
 
   Definition rep_block (b : block) : valu.
-    rewrite <- blocksz. apply (Rec.to_word b).
+    rewrite <- blocksz_ok. apply (Rec.to_word b).
   Defined.
 
   Definition valu_to_block (v : valu) : block.
-    rewrite <- blocksz in v. apply (Rec.of_word v).
+    rewrite <- blocksz_ok in v. apply (Rec.of_word v).
   Defined.
 
   Lemma rep_valu_id : forall b, Rec.well_formed b -> valu_to_block (rep_block b) = b.
@@ -49,12 +50,41 @@ Section RECARRAY.
 
   (* XXX what lemmas would be helpful if Inode.v and Balloc.v were to use this? *)
 
+  Theorem list_selN_ext' : forall len T (a b : list T) default,
+    length a = len
+    -> length b = len
+    -> (forall pos, selN a pos default = selN b pos default)
+    -> a = b.
+  Proof.
+    induction len; intros; destruct a; destruct b; simpl in *; try congruence.
+    f_equal.
+    apply (H1 0).
+    eapply IHlen; [ omega | omega | ].
+    intros.
+    apply (H1 (S pos)).
+  Qed.
+
+  Theorem list_selN_ext : forall T (a b : list T) default,
+    length a = length b
+    -> (forall pos, selN a pos default = selN b pos default)
+    -> a = b.
+  Proof.
+    intros. apply list_selN_ext' with (len:=length a) (default:=default); auto.
+  Qed.
+
   Theorem upd_divmod : forall (l : list block) (pos : addr) (v : item),
     Forall Rec.well_formed l
     -> Array.upd (fold_right (@app _) nil l) pos v =
        fold_right (@app _) nil (Array.upd l (pos ^/ items_per_valu)
          (Array.upd (sel l (pos ^/ items_per_valu) nil) (pos ^% items_per_valu) v)).
   Proof.
+    intros.
+    apply list_selN_ext with (default:=@Rec.of_word itemtype $0).
+    rewrite length_upd.
+
+    admit.
+
+    intros.
     admit.
   Qed.
 
