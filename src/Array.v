38a12003da06554fb1229bbe00606def8c4e7600
Haogang Chen <hchen@csail.mit.edu>
2015-01-14 03:52:14 -0500

    checkpoint some fact about listpred

--- src/Array.v
+++ src/Array.v
@@ -674,6 +674,27 @@ Proof.
 Qed.
 
 
+Definition combine_updN : forall A B i a b (va:A) (vb:B),
+  List.combine (updN a i va) (updN b i vb) = updN (List.combine a b) i (va, vb).
+Proof.
+  induction i; intros; destruct a, b; simpl; auto.
+  rewrite IHi; auto.
+Qed.
+
+Definition removeN {V} (l : list V) i :=
+   (firstn i l) ++ (skipn (S i) l).
+
+Theorem removeN_updN : forall V l i (v : V),
+   removeN (updN l i v) i = removeN l i.
+Proof.
+   unfold removeN; intros.
+   rewrite firstn_updN; auto.
+   simpl; rewrite skipn_updN; auto.
+Qed.
+
+
+
+
 (* A general list predicate *)
 
 Section LISTPRED.
@@ -681,7 +702,6 @@ Section LISTPRED.
   Variable T : Type.
   Variable V : Type.
   Variable prd : T -> @pred V.
-  Variable def : T.
 
   Fixpoint listpred (ts : list T) :=
     match ts with
@@ -689,7 +709,13 @@ Section LISTPRED.
     | t :: ts' => (prd t) * listpred ts'
     end%pred.
 
-  Theorem listpred_fwd : forall l i, 
+  Lemma listpred_nil: forall T V (prd : T -> @pred V),
+    listpred nil = emp.
+  Proof.
+    unfold listpred; intros; auto.
+  Qed.
+
+  Theorem listpred_fwd : forall l i def, 
     i < length l ->
       listpred l =p=> listpred (firstn i l) * (prd (selN l i def)) * listpred (skipn (S i) l).
   Proof.
@@ -698,7 +724,7 @@ Section LISTPRED.
     apply IHl; omega.
   Qed.
 
-  Theorem listpred_bwd : forall l i, 
+  Theorem listpred_bwd : forall l i def, 
     i < length l ->
       listpred (firstn i l) * (prd (selN l i def)) * listpred (skipn (S i) l) =p=> listpred l.
   Proof.
@@ -711,11 +737,78 @@ Section LISTPRED.
     omega.
   Qed.
 
-  Lemma listpred_nil: forall T V (prd : T -> @pred V),
-    listpred nil = emp.
+  Theorem listpred_extract : forall l i def,
+    i < length l ->
+    listpred l =p=> exists F, F * prd (selN l i def).
   Proof.
-    unfold listpred; intros; auto.
+    intros; rewrite listpred_fwd with (def:=def) by eauto; cancel.
   Qed.
 
+  Theorem listpred_inj: forall l1 l2,
+     l1 = l2 -> listpred l1 <=p=> listpred l2.
+  Proof.
+     intros; subst; auto.
+  Qed.
+
+  Definition sep_star_fold : (T -> @pred V -> @pred V) := fun x => sep_star (prd x).
+  Definition listpred' := fold_right sep_star_fold emp.
+
+  Theorem listpred_listpred': forall l,
+    listpred l = listpred' l.
+  Proof.
+    induction l; unfold listpred, listpred', sep_star_fold, fold_right; auto.
+  Qed.
+
+  Theorem listpred'_app_fwd: forall l1 l2,
+    listpred' (l1 ++ l2) =p=> listpred' l1 * listpred' l2.
+  Proof.
+    unfold listpred'.
+    induction l1; destruct l2; unfold sep_star_fold; try cancel;
+    rewrite IHl1; unfold sep_star_fold; cancel.
+  Qed.
+
+  Theorem listpred'_app_bwd: forall l1 l2,
+    listpred' l1 * listpred' l2 =p=> listpred' (l1 ++ l2).
+  Proof.
+    unfold listpred'.
+    induction l1; destruct l2; unfold sep_star_fold; try cancel;
+    rewrite <- IHl1; unfold sep_star_fold; cancel.
+  Qed.
+
+  Theorem listpred_app: forall l1 l2,
+    listpred (l1 ++ l2) <=p=> listpred l1 * listpred l2.
+  Proof.
+    intros; replace listpred with listpred'.
+    unfold piff; split.
+    apply listpred'_app_fwd.
+    apply listpred'_app_bwd.
+    apply functional_extensionality.
+    apply listpred_listpred'.
+  Qed.
+
+  Theorem listpred_isolate : forall l i def,
+    i < length l ->
+    listpred l <=p=> listpred (removeN l i) * prd (selN l i def).
+  Proof.
+    intros.
+    unfold removeN.
+    rewrite listpred_app.
+    unfold piff; split.
+    rewrite listpred_fwd by eauto; cancel.
+    eapply pimpl_trans2.
+    apply listpred_bwd; eauto.
+    cancel.
+  Qed.
+
+  Theorem listpred_updN : forall l i v,
+    i < length l ->
+    listpred (updN l i v) <=p=> listpred (removeN l i) * prd v.
+  Proof.
+    intros; rewrite listpred_isolate with (def:=v);
+       [ | rewrite length_updN; eauto].
+    rewrite removeN_updN.
+    rewrite selN_updN_eq; auto.
+  Qed.
 
 End LISTPRED.
+
