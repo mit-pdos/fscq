8d9aa5633f62275500907790df10a6c32230c967
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-01-29 21:49:00 -0500

    convert the new Integer-based word to/from ByteString, thanks to David Lazar

--- src/hstest.hs
+++ src/hstest.hs
@@ -9,6 +9,7 @@ import qualified System.Directory
 import qualified System.Exit
 import qualified System.Random -- apt-get install libghc-random-dev
 import qualified Data.ByteString
+import qualified Data.Bits
 import qualified Testprog
 
 disk_fn :: String
@@ -38,22 +39,26 @@ maybeCrash = do
   else
     return ()
 
+bs2i :: Data.ByteString.ByteString -> Integer
+bs2i = Data.ByteString.foldl' (\i b -> (i `Data.Bits.shiftL` 8) + fromIntegral b) 0
+
+i2bs :: Integer -> Data.ByteString.ByteString
+i2bs = Data.ByteString.reverse . Data.ByteString.unfoldr (\i -> if i == 0 then Nothing else Just (fromIntegral i, i `Data.Bits.shiftR` 8))
+
 read_disk :: Handle -> Coq_word -> IO Coq_word
-read_disk f (W64 a) = do
+read_disk f (W a) = do
   debugmsg $ "read(" ++ (show a) ++ ")"
-  hSeek f AbsoluteSeek (512 * (fromIntegral a))
+  hSeek f AbsoluteSeek $ 512*a
   bs <- Data.ByteString.hGet f 512
-  return $ W4096 bs
-read_disk _ _ = error "read_disk: non-W64 addr"
+  return $ W $ bs2i bs
 
 write_disk :: Handle -> Coq_word -> Coq_word -> IO ()
-write_disk f (W64 a) (W4096 v) = do
+write_disk f (W a) (W v) = do
   maybeCrash
   debugmsg $ "write(" ++ (show a) ++ ")"
-  hSeek f AbsoluteSeek (512 * (fromIntegral a))
-  Data.ByteString.hPut f v
+  hSeek f AbsoluteSeek $ 512*a
+  Data.ByteString.hPut f $ i2bs v
   return ()
-write_disk _ _ _ = error "write_disk: unexpected addr or val"
 
 run_dcode :: Handle -> Prog.Coq_prog a -> IO a
 run_dcode _ (Done r) = return r
@@ -71,15 +76,15 @@ run_dcode f (Write a v rx) = do write_disk f a v; run_dcode f $ rx ()
 
 lxp :: MemLog.Coq_xparams
 lxp = MemLog.Build_xparams
-  (W64 0x1000)  -- log header sector
-  (W64 0x1001)  -- commit flag sector
-  (W64 0x1010)  -- log start sector
-  (W64 0x1000)  -- log length, and MemLog uses one more for a block of addrs
+  (W 0x1000)  -- log header sector
+  (W 0x1001)  -- commit flag sector
+  (W 0x1010)  -- log start sector
+  (W 0x1000)  -- log length, and MemLog uses one more for a block of addrs
 
 bxp :: Balloc.Coq_xparams
 bxp = Balloc.Build_xparams
-  (W64 0x500)   -- bitmap start sector
-  (W64 0x10)    -- bitmap length
+  (W 0x500)   -- bitmap start sector
+  (W 0x10)    -- bitmap length
 
 main :: IO ()
 main = do
