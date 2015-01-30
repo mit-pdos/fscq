161e6827d8bce456a1a0107cbec69c685b9300c4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-01-30 13:11:03 -0500

    replace unary nat plus and mult with Prelude.+ and Prelude.*

--- src/ExtrHaskellPrelude.v
+++ src/ExtrHaskellPrelude.v
@@ -31,3 +31,5 @@ Extract Inlined Constant negb => "Prelude.not".
  *)
 Extract Inductive nat => "Prelude.Integer" [ "0" "Prelude.succ" ]
   "(\fO fS n -> if n Prelude.== 0 then fO () else fS (n Prelude.- 1))".
+Extract Inlined Constant Nat.add => "(Prelude.+)".
+Extract Inlined Constant Nat.mul => "(Prelude.*)".
