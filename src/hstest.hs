b576d9e742341f11dfe806e0739913c788bb3f12
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-04 13:07:10 -0500

    hstest: efficient conversion from Integer to ByteString (i2bs)
    use GMPI.exportIntegerToAddr to reduce memory and CPU overheads of
    converting from Integer to ByteString for writing to disk.
    
    current performance numbers:
    
      benchmark: 1000 iterations of:
        txn 1: grow file, write block, commit
        txn 2: read block, shrink file, commit
    
      runtime without profiling: 0.28 sec
      mem allocations (with profiling): 319MB

--- src/hstest.hs
+++ src/hstest.hs
@@ -13,10 +13,8 @@ import qualified System.Directory
 -- import qualified System.Random
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Internal as BSI
-import qualified Data.Bits
 import qualified Testprog
 import qualified GHC.Integer.GMP.Internals as GMPI
-import GHC.Word
 import GHC.Base
 import GHC.Exts
 import Foreign.ForeignPtr
@@ -55,42 +53,20 @@ debugmsg s =
 -- Snippets of ByteArray# manipulation code from GHC's
 -- testsuite/tests/lib/integer/integerGmpInternals.hs
 
-data MBA = MBA { unMBA :: !(MutableByteArray# RealWorld) }
-data BA  = BA  { unBA  :: !ByteArray# }
-
-newByteArray :: Word# -> IO MBA
-newByteArray sz = IO $ \s -> case newPinnedByteArray# (word2Int# sz) s of (# s', arr #) -> (# s', MBA arr #)
-
-writeByteArray :: MutableByteArray# RealWorld -> Int# -> Word8 -> IO ()
-writeByteArray arr i (W8# w) = IO $ \s -> case writeWord8Array# arr i w s of s' -> (# s', () #)
-
-freezeByteArray :: MutableByteArray# RealWorld -> IO BA
-freezeByteArray arr = IO $ \s -> case unsafeFreezeByteArray# arr s of (# s', arr' #) -> (# s', BA arr' #)
-
-bs2ba :: BS.ByteString -> IO BA
-bs2ba (BSI.PS fp _ _) = do
-  MBA mba <- newByteArray 512##
-  _ <- withForeignPtr fp $ \p -> case p of
-    (GHC.Exts.Ptr a) -> IO $ \s -> case copyAddrToByteArray# a mba 0# 512# s of
-      s' -> (# s', () #)
-  freezeByteArray mba
-
-ba2i :: BA -> Integer
-ba2i (BA ba) = GMPI.importIntegerFromByteArray ba 0## 512## -1#
-
 bs2i :: BS.ByteString -> IO Integer
 bs2i (BSI.PS fp _ _) = do
   withForeignPtr fp $ \p -> case p of
     (GHC.Exts.Ptr a) -> IO $ \s -> case GMPI.importIntegerFromAddr a 512## -1# s of
       (# s', i #) -> (# s', i #)
 
-i2bs :: Integer -> BS.ByteString
-i2bs i = BS.append bs (BS.replicate (512 - BS.length bs) 0)
+i2bs :: Integer -> IO BS.ByteString
+i2bs i = do
+    bs <- BSI.createAndTrim 512 f
+    return $ BS.append bs (BS.replicate (512 - BS.length bs) 0)
   where
-    bs = BS.unfoldr shifter i
-    shifter = \x -> if x == 0
-                    then Nothing
-                    else Just (fromIntegral x, x `Data.Bits.shiftR` 8)
+    f = \p -> case p of
+      (GHC.Exts.Ptr a) -> IO $ \s -> case GMPI.exportIntegerToAddr i a -1# s of
+        (# s', nwritten #) -> (# s', I# (word2Int# nwritten) #)
 
 read_disk :: Handle -> Coq_word -> IO Coq_word
 read_disk f (W a) = do
@@ -104,8 +80,9 @@ write_disk :: Handle -> Coq_word -> Coq_word -> IO ()
 write_disk f (W a) (W v) = do
   -- maybeCrash
   debugmsg $ "write(" ++ (show a) ++ ")"
+  bs <- i2bs v
   hSeek f AbsoluteSeek $ 512*a
-  BS.hPut f $ i2bs v
+  BS.hPut f bs
   return ()
 
 run_dcode :: Handle -> Prog.Coq_prog a -> IO a
