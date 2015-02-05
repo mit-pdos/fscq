ad62464c9d39ee1645914384b9006fcebbc118cc
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-05 14:55:51 -0500

    factor out disk IO into hslib/Disk.hs

--- src/hstest.hs
+++ src/hstest.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE MagicHash, UnboxedTuples #-}
-
 module Main where
 
 import System.IO
@@ -8,29 +6,14 @@ import Balloc
 import Inode
 import Prog
 import Word
+import qualified Disk
 import qualified System.Directory
 -- import qualified System.Exit
 -- import qualified System.Random
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Internal as BSI
 import qualified Testprog
-import qualified GHC.Integer.GMP.Internals as GMPI
-import GHC.Base
-import GHC.Exts
-import Foreign.ForeignPtr
 
 disk_fn :: String
 disk_fn = "disk.img"
-
-verbose :: Bool
-verbose = False
-
-debugmsg :: String -> IO ()
-debugmsg s =
-  if verbose then
-    putStrLn s
-  else
-    return ()
 
 -- crashRandom :: IO Int
 -- crashRandom = System.Random.getStdRandom (System.Random.randomR (1, 20))
@@ -47,48 +30,10 @@ debugmsg s =
 --   else
 --     return ()
 
--- For a more efficient array implementation, perhaps worth checking out:
--- http://www.macs.hw.ac.uk/~hwloidl/hackspace/ghc-6.12-eden-gumsmp-MSA-IFL13/libraries/dph/dph-base/Data/Array/Parallel/Arr/BUArr.hs
-
--- Snippets of ByteArray# manipulation code from GHC's
--- testsuite/tests/lib/integer/integerGmpInternals.hs
-
-bs2i :: BS.ByteString -> IO Integer
-bs2i (BSI.PS fp _ _) = do
-  withForeignPtr fp $ \p -> case p of
-    (GHC.Exts.Ptr a) -> IO $ \s -> case GMPI.importIntegerFromAddr a 512## -1# s of
-      (# s', i #) -> (# s', i #)
-
-i2bs :: Integer -> IO BS.ByteString
-i2bs i = BSI.create 512 f
-  where
-    f = \p -> do
-      _ <- BSI.memset p 0 512
-      case p of
-        (GHC.Exts.Ptr a) -> IO $ \s -> case GMPI.exportIntegerToAddr i a -1# s of
-          (# s', _ #) -> (# s', () #)
-
-read_disk :: Handle -> Coq_word -> IO Coq_word
-read_disk f (W a) = do
-  debugmsg $ "read(" ++ (show a) ++ ")"
-  hSeek f AbsoluteSeek $ 512*a
-  bs <- BS.hGet f 512
-  i <- bs2i bs
-  return $ W i
-
-write_disk :: Handle -> Coq_word -> Coq_word -> IO ()
-write_disk f (W a) (W v) = do
-  -- maybeCrash
-  debugmsg $ "write(" ++ (show a) ++ ")"
-  bs <- i2bs v
-  hSeek f AbsoluteSeek $ 512*a
-  BS.hPut f bs
-  return ()
-
 run_dcode :: Handle -> Prog.Coq_prog a -> IO a
 run_dcode _ (Done r) = return r
-run_dcode f (Read a rx) = do val <- read_disk f a; run_dcode f $ rx val
-run_dcode f (Write a v rx) = do write_disk f a v; run_dcode f $ rx ()
+run_dcode f (Read a rx) = do val <- Disk.read_disk f a; run_dcode f $ rx val
+run_dcode f (Write a v rx) = do Disk.write_disk f a v; run_dcode f $ rx ()
 
 -- the_prog :: Log.Coq_xparams -> Prog.Coq_prog ()
 -- the_prog xp =
