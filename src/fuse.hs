2220e5e28912a9e18d93d684253a5cf3a7a2526a
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-05 21:31:20 -0500

    fuse.hs: basic fuse interface seems to be working, with lots of caveats
    Using the interface provided by FS.v, fuse.hs can now read/write a
    single file in fscq's file system.  It does not currently use fscq's
    directories, and instead always maps /hello to inode 0.  It ignores
    offsets and sizes, and reads/writes the entire first block.  However,
    it does sort-of work:
    
      % ./fuse /tmp/ft -s -f -d &
      % ls -l /tmp/ft/hello
      -rw-rw-rw- 1 nickolai nickolai 0 Dec 31  1969 /tmp/ft/hello
      % cat /tmp/ft/hello
      % echo hello world > /tmp/ft/hello
      % cat /tmp/ft/hello
      hello world
      % ls -l /tmp/ft/hello
      -rw-rw-rw- 1 nickolai nickolai 512 Dec 31  1969 /tmp/ft/hello
      % fusermount -u /tmp/ft
      % ls -l disk.img
      -rw-rw-r-- 1 nickolai nickolai 4204544 Feb  5 21:33 disk.img
      %

--- src/fuse.hs
+++ src/fuse.hs
@@ -1,33 +1,72 @@
 module Main where
 
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8
+import qualified Data.ByteString as BS
+import qualified System.Directory
 import Foreign.C.Error
 import System.Posix.Types
 import System.Posix.Files
 import System.Posix.IO
-import Word
-
+import System.IO
 import System.Fuse
+import Word
+import Disk
+import Interpreter as I
+import qualified FS
+import qualified MemLog
+import qualified Balloc
+import qualified Inode
 
 -- Handle type for open files; we will use the inode number
 type HT = Coq_word
 
-main :: IO ()
-main = fuseMain fscqFSOps defaultExceptionHandler
+disk_fn :: String
+disk_fn = "disk.img"
 
-fscqFSOps :: FuseOperations HT
-fscqFSOps = defaultFuseOps
-  { fuseGetFileStat = fscqGetFileStat
+lxp :: MemLog.Coq_xparams
+lxp = MemLog.Build_xparams
+  (W 0x2000)  -- log header sector
+  (W 0x2001)  -- commit flag sector
+  (W 0x2010)  -- log start sector
+  (W 0x1000)  -- log length, and MemLog uses one more for a block of addrs
+
+bxp :: Balloc.Coq_xparams
+bxp = Balloc.Build_xparams
+  (W 0x1100)  -- bitmap start sector
+  (W 0x1)     -- bitmap length
+
+ixp :: Inode.Coq_xparams
+ixp = Inode.Build_xparams
+  (W 0x1000)   -- inode start sector
+  (W 0x100)    -- number of inode sectors
+
+main :: IO ()
+main = do
+  fileExists <- System.Directory.doesFileExist disk_fn
+  f <- openFile disk_fn ReadWriteMode
+  if fileExists
+  then
+    do
+      putStrLn "Recovering disk.."
+      I.run f $ MemLog._MEMLOG__recover lxp
+  else
+    do
+      putStrLn "Initializing disk.."
+      I.run f $ MemLog._MEMLOG__init lxp
+  putStrLn "Starting file system.."
+  fuseMain (fscqFSOps f) defaultExceptionHandler
+  hClose f
+
+fscqFSOps :: Handle -> FuseOperations HT
+fscqFSOps f = defaultFuseOps
+  { fuseGetFileStat = fscqGetFileStat f
   , fuseOpen = fscqOpen
-  , fuseRead = fscqRead
+  , fuseRead = fscqRead f
+  , fuseWrite = fscqWrite f
+  , fuseSetFileSize = fscqSetFileSize f
   , fuseOpenDirectory = fscqOpenDirectory
-  , fuseReadDirectory = fscqReadDirectory
+  , fuseReadDirectory = fscqReadDirectory f
   , fuseGetFileSystemStats = fscqGetFileSystemStats
   }
-
-fscqString :: B.ByteString
-fscqString = Data.ByteString.Char8.pack "Hello World, HFuse!\n"
 
 fscqPath :: FilePath
 fscqPath = "/hello"
@@ -36,12 +75,9 @@ dirStat :: FuseContext -> FileStat
 dirStat ctx = FileStat
   { statEntryType = Directory
   , statFileMode = foldr1 unionFileModes
-                     [ ownerReadMode
-                     , ownerExecuteMode
-                     , groupReadMode
-                     , groupExecuteMode
-                     , otherReadMode
-                     , otherExecuteMode
+                     [ ownerReadMode, ownerExecuteMode
+                     , groupReadMode, groupExecuteMode
+                     , otherReadMode, otherExecuteMode
                      ]
   , statLinkCount = 2
   , statFileOwner = fuseCtxUserID ctx
@@ -54,61 +90,79 @@ dirStat ctx = FileStat
   , statStatusChangeTime = 0
   }
 
-fileStat :: FuseContext -> FileStat
-fileStat ctx = FileStat
+fileStat :: FuseContext -> FileOffset -> FileStat
+fileStat ctx len = FileStat
   { statEntryType = RegularFile
   , statFileMode = foldr1 unionFileModes
-                     [ ownerReadMode
-                     , groupReadMode
-                     , otherReadMode
+                     [ ownerReadMode, ownerWriteMode
+                     , groupReadMode, groupWriteMode
+                     , otherReadMode, otherWriteMode
                      ]
   , statLinkCount = 1
   , statFileOwner = fuseCtxUserID ctx
   , statFileGroup = fuseCtxGroupID ctx
   , statSpecialDeviceID = 0
-  , statFileSize = fromIntegral $ B.length fscqString
+  , statFileSize = len
   , statBlocks = 1
   , statAccessTime = 0
   , statModificationTime = 0
   , statStatusChangeTime = 0
   }
 
-fscqGetFileStat :: FilePath -> IO (Either Errno FileStat)
-fscqGetFileStat "/" = do
+fscqGetFileStat :: Handle -> FilePath -> IO (Either Errno FileStat)
+fscqGetFileStat _ "/" = do
   ctx <- getFuseContext
   return $ Right $ dirStat ctx
-fscqGetFileStat path | path == fscqPath = do
+fscqGetFileStat f path | path == fscqPath = do
   ctx <- getFuseContext
-  return $ Right $ fileStat ctx
-fscqGetFileStat _ =
+  (W len) <- I.run f $ FS.file_len lxp ixp (W 0)
+  return $ Right $ fileStat ctx $ fromIntegral len
+fscqGetFileStat _ _ =
   return $ Left eNOENT
 
 fscqOpenDirectory :: FilePath -> IO Errno
 fscqOpenDirectory "/" = return eOK
 fscqOpenDirectory _   = return eNOENT
 
-fscqReadDirectory :: FilePath -> IO (Either Errno [(FilePath, FileStat)])
-fscqReadDirectory "/" = do
+fscqReadDirectory :: Handle -> FilePath -> IO (Either Errno [(FilePath, FileStat)])
+fscqReadDirectory f "/" = do
   ctx <- getFuseContext
-  return $ Right [(".",          dirStat  ctx)
-                 ,("..",         dirStat  ctx)
-                 ,(fscqName,    fileStat ctx)
+  (W len) <- I.run f $ FS.file_len lxp ixp (W 0)
+  return $ Right [(".",          dirStat ctx)
+                 ,("..",         dirStat ctx)
+                 ,(fscqName,    fileStat ctx $ fromIntegral len)
                  ]
   where (_:fscqName) = fscqPath
-fscqReadDirectory _ = return (Left (eNOENT))
+fscqReadDirectory _ _ = return (Left (eNOENT))
 
 fscqOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno HT)
 fscqOpen path mode _
-  | path == fscqPath = case mode of
-                         ReadOnly -> return (Right $ W 0)
-                         _        -> return (Left eACCES)
+  | path == fscqPath = return $ Right $ W 0
   | otherwise        = return (Left eNOENT)
 
-fscqRead :: FilePath -> HT -> ByteCount -> FileOffset -> IO (Either Errno B.ByteString)
-fscqRead path _ byteCount offset
-  | path == fscqPath =
-      return $ Right $ B.take (fromIntegral byteCount) $ B.drop (fromIntegral offset) fscqString
-  | otherwise        = return $ Left eNOENT
+fscqRead :: Handle -> FilePath -> HT -> ByteCount -> FileOffset -> IO (Either Errno BS.ByteString)
+fscqRead f _ inum byteCount offset = do
+  -- Ignore the count and offset for now..
+  (W w) <- I.run f $ FS.read_block lxp ixp inum (W 0)
+  bs <- i2bs w
+  return $ Right $ BS.take (fromIntegral byteCount) $ BS.drop (fromIntegral offset) bs
+
+fscqWrite :: Handle -> FilePath -> HT -> BS.ByteString -> FileOffset -> IO (Either Errno ByteCount)
+fscqWrite f _ inum bs offset = do
+  -- Ignore the offset for now..
+  w <- bs2i bs_pad
+  ok <- I.run f $ FS.write_block lxp bxp ixp inum (W 0) (W w)
+  if ok then
+    return $ Right $ fromIntegral bs_len
+  else
+    return $ Left eIO
+  where
+    bs_pad = BS.append bs (BS.replicate (512 - bs_len) 0)
+    bs_len = BS.length bs
+
+fscqSetFileSize :: Handle -> FilePath -> FileOffset -> IO Errno
+fscqSetFileSize f path size =
+  return eOK
 
 fscqGetFileSystemStats :: String -> IO (Either Errno FileSystemStats)
 fscqGetFileSystemStats _ =
