40c425a38b70154a2b138f1a4664524e297521b4
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-05 21:30:23 -0500

    FS.v: top-level file system interface intended for use by FUSE
    currently this is far from what we'd want for POSIX, but does provide
    some wrapping around BFile.  in particular, the write function will
    grow the file if it's not big enough yet.

--- src/Extraction.v
+++ src/Extraction.v
@@ -1,7 +1,9 @@
 Require Import ExtrHaskellPrelude.
+Require Import FS.
 Require Import Testprog.
 
 Extraction Language Haskell.
 
 Cd "codegen".
+Recursive Extraction Library FS.
 Recursive Extraction Library Testprog.
