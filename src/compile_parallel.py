41adb3db266cd81d66e240a98212e7be1a89755f
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-09 20:09:04 -0500

    compile_parallel.py: ditch pexpect
    pexpect has trouble with large (multi-megabyte) inputs.
    
    instead, use Popen.communicate() directly
    
    this seems to be close to working, except that now we run into
    subproofs created by coq -- e.g., the proof term for [roundTrip_0]
    in Word.v contains a reference to [roundTrip_0_subproof], which
    seems to be created by Coq behind the scenes.  not sure yet how
    to find and extract such subterms.

--- src/compile_parallel.py
+++ src/compile_parallel.py
@@ -7,15 +7,14 @@
 
 import argparse
 import sys
-import pexpect
 import re
 import concurrent.futures
 import multiprocessing
-import io
+import subprocess
 
 debug = False
 max_workers = multiprocessing.cpu_count()
-coqtop = "coqtop -R coqbuild Fscq"
+coqtop = ("coqtop", "-R", "coqbuild", "Fscq")
 
 def coq_remove_comments(str):
   # This is hairy because Coq has nested comments.
@@ -40,43 +39,49 @@ def coq_remove_comments(str):
 def coqtop_simpl_proof(coqtop, term):
   # Takes a coq proof (up to, but not including Qed.) and returns an
   # explicit Coq term for the resulting proof.
-  prompt = "\<\/prompt\>"
-  coqtop = pexpect.spawn(coqtop + " -emacs", timeout=None)
-  buf = io.BytesIO()
+  p = subprocess.Popen(coqtop, stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
 
   if debug:
     print("Sending", file=sys.stderr)
     print("<<", term, ">>", file=sys.stderr)
-    coqtop.logfile = sys.stderr.buffer
-  else:
-    coqtop.logfile = buf
 
-  coqtop.expect(prompt)
+  cin = term + \
+        "\n" + \
+        "Set Printing Depth 2000.\n" + \
+        "Set Printing All.\n" + \
+        "Inductive SYNC_FLAG := .\n" + \
+        "Show Proof.\n"
+  (cout, cerr) = p.communicate(cin.encode('utf-8'))
 
-  coqtop.sendline(term)
+  proofterm = ""
+  syncdone = False
+  inproof = False
+  problem = False
+  for cl in cout.decode('utf-8', 'replace').splitlines():
+    if inproof and cl == 'No more subgoals.':
+      inproof = False
+      syncdone = False
+    if inproof:
+      proofterm += cl + "\n"
+    if syncdone and not inproof:
+      if cl == 'No more subgoals.':
+        inproof = True
+      else:
+        problem = True
+    if cl == 'SYNC_FLAG_rec is defined':
+      syncdone = True
+  if inproof or syncdone or proofterm == "":
+    problem = True
 
-  ## Wait for coqtop to process our term
-  coqtop.sendline("Inductive SYNC_FLAG := .\n")
-  coqtop.expect("SYNC_FLAG is defined")
-  coqtop.expect(prompt)
-
-  coqtop.sendline("Set Printing Depth 2000.")
-  coqtop.expect("\<prompt\>")
-
-  coqtop.sendline("Set Printing All.")
-  coqtop.expect("\<prompt\>")
-
-  coqtop.sendline("Show Proof.")
-  coqtop.expect("Show Proof.")
-  idx = coqtop.expect(["No more subgoals.", "subgoals, subgoal"])
-
-  if idx == 0:
-    proofterm = coqtop.before.decode("utf-8")
-    return "refine (" + proofterm + ").\n" + "Qed.\n"
-  else:
+  if problem:
     print("Unable to complete the proof:", file=sys.stderr)
-    print(buf.getvalue().decode("utf-8", "replace"), file=sys.stderr)
+    print("Proof script sent to coqtop: <<" + cin + ">>", file=sys.stderr)
+    print("Result from coqtop: <<" + cout.decode('utf-8', 'replace') + ">>", file=sys.stderr)
     panic("Proof worker unable to complete the proof.")
+  else:
+    return "refine (" + proofterm + ").\n" + "Qed.\n"
 
 def queue_to_string(queue):
   val = ""
