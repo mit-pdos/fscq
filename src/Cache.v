fc0a6e1d9d82059e471584fcaba67146a20414a7
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-02-10 08:49:00 -0500

    Cache.v: add a placeholder [trim] function to evict something

--- src/Cache.v
+++ src/Cache.v
@@ -22,7 +22,11 @@ Module BUFCACHE.
   Definition rep (cs : cachestate) (l : list valu) :=
     (array $0 l $1 * [[ forall a v, Map.MapsTo a v cs -> sel l a $0 = v ]])%pred.
 
+  Definition trim T (cs : cachestate) rx : prog T :=
+    rx cs.
+
   Definition read T a (cs : cachestate) rx : prog T :=
+    cs <- trim cs;
     match Map.find a cs with
     | Some v => rx (cs, v)
     | None =>
@@ -31,10 +35,24 @@ Module BUFCACHE.
     end.
 
   Definition write T a v (cs : cachestate) rx : prog T :=
+    cs <- trim cs;
     ArrayWrite $0 a $1 v;;
     rx (Map.add a v cs).
 
   Hint Resolve list2mem_ptsto_bounds.
+  Ltac unfold_rep := unfold rep.
+
+  Theorem trim_ok : forall cs,
+    {< l,
+    PRE      rep cs l
+    POST:cs' rep cs' l
+    CRASH    rep cs l
+    >} trim cs.
+  Proof.
+    unfold trim; hoare.
+  Qed.
+
+  Hint Extern 1 ({{_}} progseq (trim _) _) => apply trim_ok : prog.
 
   Theorem read_ok : forall cs a,
     {< l F v,
@@ -43,22 +61,25 @@ Module BUFCACHE.
     CRASH    rep cs l
     >} read a cs.
   Proof.
-    unfold read, rep.
-    intros.
-    destruct (Map.find a cs) eqn:Hfind; hoare.
-    inversion H7; clear H7; subst. eauto.
-    inversion H7; clear H7; subst.
-    apply list2mem_sel with (def:=$0) in H.
-    apply Map.find_2 in Hfind.
-    apply H5 in Hfind. congruence.
+    unfold read.
+    hoare_unfold unfold_rep.
+
+    destruct (Map.find a r_) eqn:Hfind; hoare.
+
+    apply Map.find_2 in Hfind. apply H8 in Hfind.
+    apply list2mem_sel with (def:=$0) in H. congruence.
+
     destruct (weq a a0); subst.
     apply Map.find_1 in H0.
     erewrite Map.find_1 in H0 by (apply Map.add_1; auto).
     inversion H0; subst. reflexivity.
     apply Map.add_3 in H0; eauto.
+
     apply list2mem_sel with (def:=$0) in H.
     congruence.
   Qed.
+
+  Hint Extern 1 ({{_}} progseq (read _ _) _) => apply read_ok : prog.
 
   Theorem write_ok : forall cs a v,
     {< l F v0,
@@ -70,15 +91,18 @@ Module BUFCACHE.
              rep cs' l' * [[ (F * a |-> v)%pred (list2mem l') ]]
     >} write a v cs.
   Proof.
-    unfold write, rep.
-    hoare.
+    unfold write.
+    hoare_unfold unfold_rep.
+
     destruct (weq a a0); subst.
     autorewrite_fast; eauto.
     apply Map.find_1 in H0. erewrite Map.find_1 in H0 by (apply Map.add_1; eauto).
     congruence.
     apply Map.add_3 in H0; eauto.
     rewrite sel_upd_ne; eauto.
+
     eapply list2mem_upd; eauto.
+
     apply pimpl_or_r. right. cancel.
     instantiate (a:=(Map.add a v cs)).
     destruct (weq a a0); subst.
@@ -89,5 +113,7 @@ Module BUFCACHE.
     rewrite sel_upd_ne; eauto.
     eapply list2mem_upd; eauto.
   Qed.
+
+  Hint Extern 1 ({{_}} progseq (write _ _ _) _) => apply write_ok : prog.
 
 End BUFCACHE.
