dfbd1a4cf46aa3d96dbdf1dfd6a4977b8cc61c3c
Frans Kaashoek <kaashoek@mit.edu>
2015-03-17 08:15:42 -0400

    First pass over related work.  Experiment with moving related work to early in the paper to give the reader a sense what the landscape is since many readers are likely to be very familiar with verification and it allows us to highlight the strength of our contributions.

--- src/ExampleMemLog.v
+++ src/ExampleMemLog.v
@@ -9,6 +9,8 @@ Require Import Bool.
 Require Import GenSep.
 Require Import Idempotent.
 Require Import FSLayout.
+Require Import Cache.
+Require Import FS.
 
 Set Implicit Arguments.
 
@@ -46,11 +48,11 @@ Theorem log_inc_two_body_ok: forall xp s0 s1 mscs,
                   [[ (s0 |-> v0 * s1 |-> v1 * F)%pred (list2mem m)]]
   POST RET:mscs   exists m', MEMLOG.rep xp Fm (ActiveTxn mbase m') mscs *
                   [[ (s0 |-> (v0 ^+ $1) * s1 |-> (v1 ^+ $1) * F)%pred (list2mem m') ]]
-  CRASH           MEMLOG.would_recover_old xp Fm mbase.
+  CRASH           MEMLOG.would_recover_old xp Fm mbase
   >} log_inc_two_body xp s0 s1 mscs.
 Proof.
   unfold log_inc_two_body.
-  hoare.
+  hoare; apply MEMLOG.activetxn_would_recover_old.
 Qed.
 
 Hint Extern 1 ({{_}} progseq (log_inc_two_body _ _ _ _) _) => apply log_inc_two_body_ok : prog.
@@ -78,8 +80,12 @@ Theorem log_inc_two_ok: forall xp s0 s1 mscs,
                   [[ (s0 |-> (v0 ^+ $1) * s1 |-> (v1 ^+ $1) * F)%pred (list2mem m') ]]
   >} log_inc_two xp s0 s1 mscs.
 Proof.
-  unfold log_inc_two, MEMLOG.would_recover_old.
+  unfold log_inc_two.
   hoare.
+  rewrite MEMLOG.notxn_would_recover_old.
+  cancel.
+  rewrite MEMLOG.activetxn_would_recover_old.
+  cancel.
 Qed.
 
 Hint Extern 1 ({{_}} progseq (log_inc_two _ _ _ _) _) => apply log_inc_two_ok : prog.
@@ -89,20 +95,26 @@ Hint Rewrite crash_xform_sep_star_dist crash_xform_or_dist crash_xform_exists_co
 Definition i2 xp s0 s1 T := @log_inc_two T xp s0 s1.
 
 Theorem log_inc_two_recover_ok: forall xp s0 s1 mscs,
-  {<<< v0 v1 F Fm mbase,
+  {<< v0 v1 F Fm mbase,
   PRE
     MEMLOG.rep xp Fm (NoTransaction mbase) mscs *
     [[ (s0 |-> v0 * s1 |-> v1 * F)%pred (list2mem mbase)]]
-  POST RET:rr
-    exists mscs,
-   ([[ rr = OK ^(mscs, false) \/ rr = Recover mscs ]] * MEMLOG.rep xp Fm (NoTransaction mbase) mscs \/
-    exists m',
-    [[ (s0 |-> (v0 ^+ $1) * s1 |-> (v1 ^+ $1) * F)%pred (list2mem m') ]] *
-    [[ rr = OK ^(mscs, true) \/ rr = Recover mscs ]] * MEMLOG.rep xp Fm (NoTransaction m') mscs)
-  >>>} log_inc_two xp s0 s1 mscs >> MEMLOG.recover xp (fst (snd mscs)).
+  POST RET:^(mscs, r)
+    [[ r = false ]] * MEMLOG.rep xp Fm (NoTransaction mbase) mscs \/
+    [[ r = true ]] * exists m', MEMLOG.rep xp Fm (NoTransaction m') mscs *
+    [[ (s0 |-> (v0 ^+ $1) * s1 |-> (v1 ^+ $1) * F)%pred (list2mem m') ]]
+  REC RET:^(mscs, xp)
+    MEMLOG.rep xp Fm (NoTransaction mbase) mscs \/
+    exists m', MEMLOG.rep xp Fm (NoTransaction m') mscs *
+               [[ (s0 |-> (v0 ^+ $1) * s1 |-> (v1 ^+ $1) * F)%pred (list2mem m') ]]
+  >>} log_inc_two xp s0 s1 mscs >> MEMLOG.recover.
 Proof.
   intros.
   unfold forall_helper at 1 2; intros v0 v1 F.
+
+  eapply pimpl_ok3.
+  eapply corr3_from_corr2_rx; eauto with prog.
+  
   eapply (MEMLOG.recover_corr2_to_corr3 (i2 xp s0 s1)).
   unfold i2.
   intros.
