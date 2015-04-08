fa1097d1d8bbffca4c0f956bde2a853603e01097
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-04-08 11:27:38 -0400

    use [Require Import ExtrHaskellBasic]

--- src/ExtrHaskellPrelude.v
+++ src/ExtrHaskellPrelude.v
@@ -1,32 +1,16 @@
 (** Extraction to Haskell: assume Prelude imported *)
 
+Require Import ExtrHaskellBasic.
 Require Import Coq.Program.Basics.
 Require Import Coq.Strings.Ascii.
 Require Import Coq.Strings.String.
 Require Import ZArith.
 
-Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
 Extract Inlined Constant Bool.bool_dec => "(Prelude.==)".
-
-Extract Inductive option => "Prelude.Maybe" [ "Prelude.Just" "Prelude.Nothing" ].
-
-Extract Inductive unit => "()" [ "()" ].
 Extract Inlined Constant id => "(Prelude.id)".
-
-Extract Inductive list => "([])" [ "([])" "(:)" ].
 Extract Inlined Constant app => "(Prelude.++)".
-(* NB: length returns nat *)
-
-Extract Inductive prod => "(,)" [ "(,)" ].
 Extract Inlined Constant fst => "Prelude.fst".
 Extract Inlined Constant snd => "Prelude.snd".
-
-Extract Inductive sumbool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
-Extract Inductive sum => "Prelude.Either" [ "Prelude.Left" "Prelude.Right" ].
-
-Extract Inlined Constant andb => "(Prelude.&&)".
-Extract Inlined Constant orb => "(Prelude.||)".
-Extract Inlined Constant negb => "Prelude.not".
 
 (* Int isn't quite right, because it can go negative, and
  * subtraction does not saturate at zero.  But good enough for
