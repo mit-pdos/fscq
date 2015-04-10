a2de3ef387beb8b29725b7799796f8a5d435f308
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-04-10 10:57:20 -0400

    clean up Lock.v a bit

--- src/Lock.v
+++ src/Lock.v
@@ -1,23 +1,17 @@
-Require Import Word.
-
 Lemma master_key : unit. Proof. exact tt. Qed.
 
 Definition locked {A} := let 'tt := master_key in fun x : A => x.
 
-Lemma lock A (x : A) : x = locked x.
-Proof. unfold locked; case master_key; reflexivity. Qed.
+Definition unlock {A} : locked A -> A.
+  unfold locked; destruct master_key; intro; assumption.
+Defined.
 
-(*
-Definition sz1 := 64.
-Definition sz2 := locked 64.
+Definition lock {A} : A -> locked A.
+  unfold locked; destruct master_key; intro; assumption.
+Defined.
 
-Definition x1 : word sz1 := $1.
-Definition x2 : word sz2 := $1.
-
-Theorem sz2sz1 : sz2 = sz1.
+Lemma locked_eq :
+  forall A (x : A), locked x = x.
 Proof.
-  unfold sz1, sz2.
-  rewrite <- lock.
-  reflexivity.
+  unfold locked; destruct master_key; auto.
 Qed.
-*)
