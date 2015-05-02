4dd15b23de5e0905461f2722114bce468329f6a0
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-05-02 00:41:18 -0400

    get ByteFile.v:write_byte to type-check
    very messy; probably worth figuring out a cleaner way to do this..

--- src/ByteFile.v
+++ src/ByteFile.v
@@ -23,16 +23,12 @@ Require Import Rec.
 Require Import RecArray.
 Require Import Omega.
 Require Import Eqdep_dec.
+Require Import Bytes.
 
 Set Implicit Arguments.
 Import ListNotations.
 
-Definition bbits := 8.
-Definition byte := word bbits.
-
 Module BYTEFILE.
-
-  Definition block_bytes := valulen / bbits.
 
   Fixpoint list2word elen (l : list (word elen)) : word (length l * elen).
     refine
@@ -92,43 +88,37 @@ Module BYTEFILE.
 
   Lemma block_byte_match_1 :
     forall x,
-      valulen =
-      ((Nat.min x block_bytes) * bbits) + (block_bytes - (Nat.min x block_bytes)) * bbits.
+      valubytes = (Nat.min x valubytes) + (valubytes - (Nat.min x valubytes)).
   Proof.
     intros.
-    rewrite Nat.mul_sub_distr_r.
-    rewrite le_plus_minus_r.
-    unfold block_bytes. rewrite valulen_is. vm_compute; auto.
-    apply mult_le_compat_r.
+    rewrite le_plus_minus_r; auto.
     apply Min.le_min_r.
   Qed.
 
   Lemma block_byte_match_2 :
     forall lenbytes x,
-      lenbytes * bbits =
-      (Nat.min lenbytes x) * bbits + (lenbytes - (Nat.min lenbytes x)) * bbits.
+      lenbytes =
+      (Nat.min lenbytes x) + (lenbytes - (Nat.min lenbytes x)).
   Proof.
     intros.
-    rewrite Nat.mul_sub_distr_r.
-    rewrite le_plus_minus_r.
-    auto.
-    apply mult_le_compat_r.
+    rewrite le_plus_minus_r; auto.
     apply Min.le_min_l.
   Qed.
 
-  Fixpoint block_byte_match (blocks : list valu) (bytes : list byte) : Prop :=
+  Fixpoint block_byte_match (blocks : list valu) (bytelist : list byte) : Prop :=
     match blocks with
-    | nil => bytes = nil
+    | nil => bytelist = nil
     | b :: blocks' =>
-      let this_block_bytes := Nat.min (length bytes) block_bytes in
-      let b_split := eq_rect valulen word b
-        (this_block_bytes*8 + (block_bytes - this_block_bytes) * 8) (block_byte_match_1 _) in
-      let bytes_split := eq_rect ((length bytes) * 8) word (list2word bytes)
-        (this_block_bytes*8 + (length bytes - this_block_bytes) * 8) (block_byte_match_2 _ _) in
-      (split1 (this_block_bytes*8) ((block_bytes - this_block_bytes)*8) b_split =
-       split1 (this_block_bytes*8) ((length bytes - this_block_bytes)*8) bytes_split) /\
+      let this_block_bytes := Nat.min (length bytelist) valubytes in
+      let b_split := eq_rect valubytes bytes (valu2bytes b)
+        (this_block_bytes + (valubytes - this_block_bytes)) (block_byte_match_1 _) in
+      let bytes_split := eq_rect (length bytelist) bytes (list2word bytelist)
+        (this_block_bytes + (length bytelist - this_block_bytes)) (block_byte_match_2 _ _) in
+      (bsplit1 (this_block_bytes) (valubytes - this_block_bytes) b_split =
+       bsplit1 (this_block_bytes) (length bytelist - this_block_bytes) bytes_split) /\
       block_byte_match blocks'
-      (word2list bbits (length bytes - this_block_bytes) (split2 (this_block_bytes*8) ((length bytes - this_block_bytes)*8) bytes_split))
+        (word2list 8 (length bytelist - this_block_bytes)
+          (bsplit2 this_block_bytes (length bytelist - this_block_bytes) bytes_split))
     end.
 
   Definition rep (bytes : list byte) (bfattr : BFILE.bfattr) : @pred nat eq_nat_dec valu :=
@@ -142,24 +132,24 @@ Module BYTEFILE.
                    chunk_block : addr;
                    chunk_boff : nat;
                    chunk_bend : nat;
-                   chunk_boff_proof : chunk_boff < valulen;
-                   chunk_bend_proof : chunk_bend <= valulen;
+                   chunk_boff_proof : chunk_boff < valubytes;
+                   chunk_bend_proof : chunk_bend <= valubytes;
                    chunk_boff_bend_proof : chunk_boff <= chunk_bend
                    }.
 
-  Lemma off_lt_valulen :
+  Lemma off_lt_valubytes :
     forall off,
-      off mod valulen < valulen.
+      off mod valubytes < valubytes.
   Proof.
     intros.
     apply Nat.mod_bound_pos.
     omega.
-    rewrite valulen_is; omega.
+    rewrite valubytes_is; omega.
   Qed.
 
-  Lemma end_lt_valulen :
+  Lemma end_lt_valubytes :
     forall x,
-      Nat.min x valulen <= valulen.
+      Nat.min x valubytes <= valubytes.
   Proof.
     intros.
     apply Nat.le_min_r.
@@ -167,7 +157,7 @@ Module BYTEFILE.
 
   Lemma off_end_lt :
     forall off sz,
-      off mod valulen <= Nat.min (off mod valulen + sz) valulen.
+      off mod valubytes <= Nat.min (off mod valubytes + sz) valubytes.
   Proof.
     intros.
     apply Nat.min_glb.
@@ -175,20 +165,20 @@ Module BYTEFILE.
     apply Nat.lt_le_incl.
     apply Nat.mod_bound_pos.
     omega.
-    rewrite valulen_is; omega.
+    rewrite valubytes_is; omega.
   Qed.
 
   (* fix point for computing list of byte chunks to write, one entry per block *)
-  Fixpoint chunkList (b: nat) (sz:nat) (off:nat) : list chunk :=
+  Fixpoint chunkList (b : nat) (sz : nat) (off : nat) : list chunk :=
     match b with
       | O => nil   (* XXX maybe the last chunk: [(off, sz)] *)
       | S b' =>
-        let blk := off / valulen in   (* XXX blk should be addr, very slow! *)
-        let boff := off mod valulen in
-        let bend := Nat.min (boff + sz) valulen in
+        let blk := off / valubytes in   (* XXX blk should be addr, very slow! *)
+        let boff := off mod valubytes in
+        let bend := Nat.min (boff + sz) valubytes in
         let bsz := bend - boff in
         (@Build_chunk ($ blk) boff bend
-          (off_lt_valulen off) (end_lt_valulen (boff + sz))
+          (off_lt_valubytes off) (end_lt_valubytes (boff + sz))
           (off_end_lt off sz)
         ) :: chunkList b' (sz - bsz) (off+boff)
      end.
@@ -209,18 +199,18 @@ Module BYTEFILE.
       rx ^(mscs, true)
     }.
 
-  Lemma boff_valulen_boff :
+  Lemma boff_valubytes_boff :
     forall boff,
-      boff < valulen ->
-      valulen = boff + (valulen - boff).
+      boff < valubytes ->
+      valubytes = boff + (valubytes - boff).
   Proof.
     intros. omega.
   Qed.
 
-  Lemma bend_valulen_bend :
+  Lemma bend_valubytes_bend :
     forall bend,
-      bend <= valulen ->
-      valulen = bend + (valulen - bend).
+      bend <= valubytes ->
+      valubytes = bend + (valubytes - bend).
   Proof.
     intros. omega.
   Qed.
@@ -232,14 +222,30 @@ Module BYTEFILE.
 (boff + (bend - boff + (valulen - bend)))
 *)
 
-  Record byteword := {
-    byteword_len : nat;
-    byteword_word : word (byteword_len * 8)
+  Record len_bytes := {
+    len_bytes_len : nat;
+    len_bytes_data : bytes len_bytes_len
   }.
 
-  Definition write_byte T fsxp inum off len (data:word (len*8)) sz mscs rx : prog T :=
-    mscs <- LOG.begin (FSXPLog fsxp) mscs;
-    mscs <- ForEach ck ckrest (chunkList (((sz-off)/valulen)+1) sz off)
+  Theorem boff_bend_boff_valubytes_bend :
+    forall boff bend,
+      boff <= bend ->
+      bend <= valubytes ->
+      boff + (bend - boff + (valubytes - bend)) = valubytes.
+  Proof.
+    intros.
+    omega.
+  Qed.
+
+  Theorem bend_boff_lenleft_bend_boff :
+    forall bend boff lenleft,
+      lenleft = bend - boff + (lenleft - (bend - boff)).
+  Proof.
+    admit.
+  Admitted.
+
+  Definition write_byte T fsxp inum (off : addr) len (data : bytes len) mscs rx : prog T :=
+    let^ (mscs, _) <- ForEach ck ckrest (chunkList (((len - # off)/valulen)+1) len (# off))
       Ghost [ mbase m F ]
       Loopvar [ mscs dataleft' ]
       Continuation lrx
@@ -249,8 +255,8 @@ Module BYTEFILE.
       OnCrash
         LOG.would_recover_old fsxp.(FSXPLog) F mbase
       Begin
-        let lenleft := byteword_len dataleft' in
-        let dataleft := byteword_word dataleft' in
+        let lenleft := len_bytes_len dataleft' in
+        let dataleft := len_bytes_data dataleft' in
         let b := chunk_block ck in
         let boff1 := chunk_boff ck in
         let bend := chunk_bend ck in
@@ -260,29 +266,31 @@ Module BYTEFILE.
         let^ (mscs, ok) <- grow_if_needed fsxp inum b mscs;
         If (bool_dec ok true) {
           let^ (mscs, v) <- BFILE.bfread  (FSXPLog fsxp) (FSXPInode fsxp) inum b mscs;
-          let v_boff := eq_rect valulen word v (boff1+(valulen-boff1)) (@boff_valulen_boff boff1 boffProof) in
-          let v_bend := eq_rect valulen word v (bend + (valulen - bend)) (@bend_valulen_bend bend bendProof) in
-          let x := split1 (boff1) (valulen - boff1) v_boff in
-          let y := split2 bend (valulen - bend) v_bend in
-          let dataleft_cast := eq_rect _ word dataleft (bend - boff1 + (lenleft - (bend - boff1))) _ in
-          let z := split1 (bend-boff1) (lenleft-(bend-boff1)) dataleft_cast in
-          let z_rest := split2 (bend-boff1) (lenleft-(bend-boff1)) dataleft_cast in
-          let v' := Word.combine x (Word.combine z y) in
-          let v'' := eq_rect _ word v' valulen _ in
-          mscs <- BFILE.bfwrite (FSXPLog fsxp) (FSXPInode fsxp) inum b v'' mscs;
-          lrx ^(mscs, (Build_byteword _ z_rest))
+          let vb := valu2bytes v in
+          let v_boff := eq_rect valubytes bytes vb (boff1 + (valubytes-boff1)) (@boff_valubytes_boff boff1 boffProof) in
+          let v_bend := eq_rect valubytes bytes vb (bend + (valubytes - bend)) (@bend_valubytes_bend bend bendProof) in
+          let x := bsplit1 (boff1) (valubytes - boff1) v_boff in
+          let y := bsplit2 bend (valubytes - bend) v_bend in
+          let dataleft_cast := eq_rect _ bytes dataleft
+            (bend - boff1 + (lenleft - (bend - boff1))) (bend_boff_lenleft_bend_boff bend boff1 _) in
+          let z := bsplit1 (bend-boff1) (lenleft-(bend-boff1)) dataleft_cast in
+          let z_rest := bsplit2 (bend-boff1) (lenleft-(bend-boff1)) dataleft_cast in
+          let v' := bcombine x (bcombine z y) in
+          let v'' := eq_rect _ bytes v' valubytes
+            (@boff_bend_boff_valubytes_bend boff1 bend boffendProof bendProof) in
+          mscs <- BFILE.bfwrite (FSXPLog fsxp) (FSXPInode fsxp) inum b (bytes2valu v'') mscs;
+          lrx ^(mscs, (Build_len_bytes z_rest))
         } else {
           mscs <- LOG.abort (FSXPLog fsxp) mscs;
           rx ^(mscs, false)
         }
-      Rof ^(mscs, Build_byteword _ data);
+      Rof ^(mscs, Build_len_bytes data);
     let^ (mscs, oldattr) <- BFILE.bfgetattr (FSXPLog fsxp) (FSXPInode fsxp) inum mscs;
     mscs <- BFILE.bfsetattr (FSXPLog fsxp) (FSXPInode fsxp) inum
-                            (INODE.Build_iattr (off ^+ sz)
+                            (INODE.Build_iattr (off ^+ ($ len))
                                                (INODE.IMTime oldattr)
                                                (INODE.IType oldattr))
                             mscs;
-    let^ (mscs, ok) <- LOG.commit (FSXPLog fsxp) mscs;
-    rx ^(mscs, ok).
+    rx ^(mscs, true).
 
-END BYTEFILE.
+End BYTEFILE.
