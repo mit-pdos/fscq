061c4372a556196d8a27cdc7531d17ecc492b87e
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-06-30 11:02:09 -0400

    try to get rid of dependent types for return values in ProgConcur

--- src/ProgConcur.v
+++ src/ProgConcur.v
@@ -5,6 +5,7 @@ Require Import Hoare.
 Require Import Pred.
 Require Import RG.
 Require Import Arith.
+Require Import SepAuto.
 
 Set Implicit Arguments.
 
@@ -28,48 +29,33 @@ End STAR.
 
 Section ExecConcur.
 
-  Variable doneTs : nat -> Type.
-
-  Inductive threadstate (T : Type) :=
+  Inductive threadstate :=
   | TNone
-  | TRunning (p : prog T)
-  | TFinished (r : T)
+  | TRunning (T : Type) (p : prog T)
+  | TFinished (T : Type) (r : T)
   | TFailed.
 
-  Definition threadstates := forall (tid : nat), threadstate (doneTs tid).
-  Definition results := forall (tid : nat), option (doneTs tid).
+  Inductive some_result :=
+  | Res (T : Type) (r : T).
 
-  Definition upd_prog (ap : threadstates) (tid : nat) (p : threadstate (doneTs tid)) : threadstates.
-    refine (fun tid' => if eq_nat_dec tid' tid then _ else ap tid').
-    rewrite e.
-    exact p.
-  Defined.
+  Definition threadstates := forall (tid : nat), threadstate.
+  Definition results := forall (tid : nat), option some_result.
 
-  Definition upd_result (ar : results) (tid : nat) (p : doneTs tid) : results.
-    refine (fun tid' => if eq_nat_dec tid' tid then _ else ar tid').
-    rewrite e.
-    exact (Some p).
-  Defined.
-
-  (**
-   * It's a little weird that [tid] is an implicit argument for [upd_prog].
-   * That's because Coq can figure out which thread ID you're updating based
-   * on the type of [p'], since its return type is already dependent on its
-   * thread ID..
-   *)
+  Definition upd_prog (ap : threadstates) (tid : nat) (p : threadstate) :=
+    fun tid' => if eq_nat_dec tid' tid then p else ap tid'.
 
   Inductive cstep : nat -> mem -> threadstates -> mem -> threadstates -> Prop :=
-  | cstep_step : forall tid ts m p m' p',
+  | cstep_step : forall T tid ts m (p : prog T) m' p',
     ts tid = TRunning p ->
     step m p m' p' ->
-    cstep tid m ts m' (upd_prog ts (TRunning p))
-  | cstep_fail : forall tid ts m p,
+    cstep tid m ts m' (upd_prog ts tid (TRunning p))
+  | cstep_fail : forall T tid ts m (p : prog T),
     ts tid = TRunning p ->
     (~exists m' p', step m p m' p') -> (~exists r, p = Done r) ->
-    cstep tid m ts m (upd_prog ts (@TFailed (doneTs tid)))
-  | cstep_done : forall tid ts m r,
+    cstep tid m ts m (upd_prog ts tid TFailed)
+  | cstep_done : forall T tid ts m (r : T),
     ts tid = TRunning (Done r) ->
-    cstep tid m ts m (upd_prog ts (TFinished r)).
+    cstep tid m ts m (upd_prog ts tid (TFinished r)).
 
   (**
    * The first argument of [cstep] is the PID that caused the step.
@@ -85,19 +71,17 @@ Section ExecConcur.
                            @pred addr (@weq addrlen) valuset)
                     (p : prog T) : Prop.
     refine (forall (tid : nat), (_ : Prop)).
-    refine (forall (done : donecond (doneTs tid)), (_ : Prop)).
+    refine (forall (done : donecond T), (_ : Prop)).
     refine (forall (rely : @action addr (@weq addrlen) valuset), (_ : Prop)).
     refine (forall (guarantee : @action addr (@weq addrlen) valuset), (_ : Prop)).
     refine (forall (m : @mem addr (@weq addrlen) valuset), (_ : Prop)).
     refine (forall (ts : threadstates), (_ : Prop)).
-    refine (forall (H : T = doneTs tid), (_ : Prop)).
-    subst T.
     refine (ts tid = TRunning p -> (_ : Prop)).
     refine (pre done rely guarantee m -> (_ : Prop)).
     refine ((forall tid' m' ts', tid' <> tid ->
              cstep tid' m ts m' ts' -> rely m m') /\ (_ : Prop)).
     refine (forall m' ts', cstep tid m ts m' ts' ->
-            (guarantee m m' /\ ts' tid <> (@TFailed (doneTs tid)))).
+            (guarantee m m' /\ ts' tid <> TFailed)).
   Defined.
 
   Inductive coutcome :=
@@ -105,18 +89,18 @@ Section ExecConcur.
   | CFinished (m : @mem addr (@weq addrlen) valuset) (rs : results).
 
   Inductive cexec : mem -> threadstates -> coutcome -> Prop :=
-  | CStep : forall tid ts m m' p p' out,
+  | CStep : forall T tid ts m m' (p : prog T) p' out,
     ts tid = TRunning p ->
     step m p m' p' ->
-    cexec m' (upd_prog ts (TRunning p')) out ->
+    cexec m' (upd_prog ts tid (TRunning p')) out ->
     cexec m ts out
-  | CFail : forall tid ts m p,
+  | CFail : forall T tid ts m (p : prog T),
     ts tid = TRunning p ->
     (~exists m' p', step m p m' p') -> (~exists r, p = Done r) ->
     cexec m ts CFailed
   | CDone : forall ts m rs,
-    (forall tid, (ts tid = (@TNone (doneTs tid)) /\ rs tid = None) \/
-                 exists (r : doneTs tid), (ts tid = TRunning (Done r) /\ rs tid = Some r)) ->
+    (forall tid, (ts tid = TNone /\ rs tid = None) \/
+                 exists r, (ts tid = TRunning (Done r) /\ rs tid = Some r)) ->
     cexec m ts (CFinished m rs).
 
 End ExecConcur.
@@ -130,7 +114,7 @@ Definition write2 :=
   Write $1 $22;;
   Done 2.
 
-Theorem write1_ok : forall doneTs, ccorr2 doneTs
+Theorem write1_ok : ccorr2
   (fun done rely guarantee =>
    exists F v0,
    F * $0 |-> v0 *
@@ -138,12 +122,11 @@ Theorem write1_ok : forall doneTs, ccorr2 doneTs
    [[ forall F a b, (F * $0 |-> a ~> F * $0 |-> b) =a=> guarantee ]]
   )%pred write1.
 Proof.
-  unfold ccorr2; intros.
-  generalize dependent done.
-  (* XXX dependent type mess with [doneTs].. *)
+  unfold ccorr2, write1; intros.
+  destruct_lift H0.
 Admitted.
 
-Theorem write2_ok : forall doneTs, ccorr2 doneTs
+Theorem write2_ok : ccorr2
   (fun done rely guarantee =>
    exists F v0,
    F * $1 |-> v0 *
