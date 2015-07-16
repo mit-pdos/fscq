668f72a5a5dc6b9aab6676c580aa507f1b78e5c2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-07-16 10:55:58 -0400

    efficient extraction for Nat.modulo

--- src/ExtrHaskellPrelude.v
+++ src/ExtrHaskellPrelude.v
@@ -10,3 +10,8 @@ Extract Inlined Constant id => "(Prelude.id)".
 Extract Inlined Constant app => "(Prelude.++)".
 Extract Inlined Constant fst => "Prelude.fst".
 Extract Inlined Constant snd => "Prelude.snd".
+
+Extract Constant PeanoNat.Nat.pred => "(\n -> Prelude.max 0 (Prelude.pred n))".
+Extract Constant PeanoNat.Nat.sub => "(\n m -> Prelude.max 0 (n Prelude.- m))".
+Extract Constant PeanoNat.Nat.modulo =>
+  "(\n m -> if m Prelude.== 0 then 0 else Prelude.mod n m)".
