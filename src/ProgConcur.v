ac86d601c21cbaefd2bd6c80f7c736e959e6fa80
Tej Chajed <tchajed@mit.edu>
2015-07-23 20:08:54 -0400

    Add local notations for star

--- src/ProgConcur.v
+++ src/ProgConcur.v
@@ -12,57 +12,71 @@ Import ListNotations.
 
 Set Implicit Arguments.
 
+(** STAR provides a type star to represent repeated applications of
+    an arbitrary binary relation R over values in A.
 
+    We will use star here to represent the transitive closure of an
+    action; that is, star a is an action where there is some sequence
+    m1 m2 ... mN where a m1 m2, a m2 m3, ... a mN-1 mN hold. *)
 Section STAR.
 
-  Variable state : Type.
-  Variable step : state -> state -> Prop.
+  Variable A : Type.
+  Variable R : A -> A -> Prop.
 
-  Inductive star : state -> state -> Prop :=
+  Infix "-->" := R (at level 40).
+
+  Reserved Notation "s1 -->* s2" (at level 50).
+
+  Inductive star : A -> A -> Prop :=
   | star_refl : forall s,
-    star s s
+    s -->* s
   | star_step : forall s1 s2 s3,
-    step s1 s2 ->
-    star s2 s3 ->
-    star s1 s3.
+    s1 --> s2 ->
+    s2 -->* s3 ->
+    s1 -->* s3
+  where "s1 -->* s2" := (star s1 s2).
 
   Hint Constructors star.
 
-  Inductive star_r : state -> state -> Prop :=
+  Reserved Notation "s1 ==>* s2" (at level 50).
+
+  Inductive star_r : A -> A -> Prop :=
   | star_r_refl : forall s,
-    star_r s s
+    s ==>* s
   | star_r_step : forall s1 s2 s3,
-    star_r s1 s2 ->
-    step s2 s3 ->
-    star_r s1 s3.
+    s1 ==>* s2 ->
+    s2 --> s3 ->
+    s1 ==>* s3
+  where "s2 ==>* s1" := (star_r s1 s2).
 
   Hint Constructors star_r.
 
   Lemma star_r_trans : forall s0 s1 s2,
-    star_r s1 s2 ->
-    star_r s0 s1 ->
-    star_r s0 s2.
+    s1 ==>* s2 ->
+    s0 ==>* s1 ->
+    s0 ==>* s2.
   Proof.
     induction 1; eauto.
   Qed.
+
+  Hint Resolve star_r_trans.
 
   Lemma star_trans : forall s0 s1 s2,
-    star s0 s1 ->
-    star s1 s2 ->
-    star s0 s2.
+    s0 -->* s1 ->
+    s1 -->* s2 ->
+    s0 -->* s2.
   Proof.
     induction 1; eauto.
   Qed.
 
+  Hint Resolve star_trans.
+
   Theorem star_lr_eq : forall s s',
-    star s s' <-> star_r s s'.
+    s -->* s' <-> s ==>* s'.
   Proof.
     intros.
-    split; intros.
-    induction H; eauto.
-    eapply star_r_trans; eauto.
-    induction H; eauto.
-    eapply star_trans; eauto.
+    split; intros;
+      induction H; eauto.
   Qed.
 
 
