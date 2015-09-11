98c4dccea148ce96e22db1e0ef2e7d0702bd8bb1
Nickolai Zeldovich <nickolai@csail.mit.edu>
2015-09-11 07:08:14 -0400

    mkfs: allow specifying different number of data/inode bitmap blocks
    useful for making a larger file system

--- src/mkfs.hs
+++ src/mkfs.hs
@@ -8,21 +8,26 @@ import Disk
 import Errno
 import System.Environment
 
+do_mkfs :: String -> Integer -> Integer -> IO ()
+do_mkfs fn data_bitmaps inode_bitmaps = do
+  ds <- init_disk fn
+  putStrLn $ "Initializing file system"
+  res <- I.run ds $ FS.mkfs (W data_bitmaps) (W inode_bitmaps)
+  case res of
+    Err _ -> error $ "mkfs failed"
+    OK (_, (fsxp, ())) ->
+      putStrLn $ "Initialization OK, " ++ (show $ coq_FSXPMaxBlock fsxp) ++ " blocks"
+  stats <- close_disk ds
+  print_stats stats
+
 main :: IO ()
 main = do
   args <- getArgs
 
   case args of
-    [fn] -> do
-      ds <- init_disk fn
-      putStrLn $ "Initializing file system"
-      res <- I.run ds $ FS.mkfs (W 1) (W 1)
-      case res of
-        Err _ -> error $ "mkfs failed"
-        OK (_, (fsxp, ())) ->
-          putStrLn $ "Initialization OK, " ++ (show $ coq_FSXPMaxBlock fsxp) ++ " blocks"
-
-      stats <- close_disk ds
-      print_stats stats
+    [fn] -> do_mkfs fn 1 1
+    [fn, data_bitmaps_s, inode_bitmaps_s] -> do_mkfs fn (read data_bitmaps_s) (read inode_bitmaps_s)
     _ -> do
-      putStrLn $ "Usage: mkfs diskpath"
+      putStrLn $ "Usage:"
+      putStrLn $ "  mkfs diskpath"
+      putStrLn $ "  mkfs diskpath n_data_bitmaps n_inode_bitmaps"
