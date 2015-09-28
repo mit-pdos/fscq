3f58377d53e43aca08266e271c76f16a56eacf53
stephanie-wang <swang93@mit.edu>
2015-09-28 15:40:25 -0400

    ChecksumLog.v: first attempt at theorem definitions for 2-block put, get, recover example

--- src/ChecksumLog.v
+++ src/ChecksumLog.v
@@ -6,6 +6,7 @@ Require Import BasicProg.
 Require Import Compare.
 Require Import Cache.
 Require Import Pred.
+Require Import PredCrash.
 Require Import Hoare.
 Require Import Mem.
 Require Import GenSep.
@@ -49,8 +50,8 @@ Definition put T cs d1 d2 rx : prog T :=
   rx cs.
 
 Definition get T cs rx : prog T :=
-  d1 <- BUFCACHE.read block1 cs;
-  d2 <- BUFCACHE.read block2 cs;
+  let^ (cs, d1) <- BUFCACHE.read block1 cs;
+  let^ (cs, d2) <- BUFCACHE.read block2 cs;
   rx ^(d1, d2).
 
 Definition recover T cs rx : prog T :=
@@ -64,3 +65,40 @@ Definition recover T cs rx : prog T :=
     cs <- put cs default_valu default_valu;
     rx cs
   }.
+
+
+Theorem put_ok : forall cs d1 d2,
+  {< d d1_old d2_old,
+  PRE
+    BUFCACHE.rep cs d *
+    rep d1_old d2_old
+  POST RET:cs
+    rep d1 d2
+  CRASH
+    exists cs', BUFCACHE.rep cs' d *
+    crep d1_old d2_old d1 d2
+  >} put cs d1 d2.
+
+Theorem get_ok : forall cs,
+  {< d d1 d2,
+  PRE
+    BUFCACHE.rep cs d *
+    rep d1 d2
+  POST RET:^(d1', d2')
+    [[ d1 = d1' /\ d2 = d2' ]]
+  CRASH
+    BUFCACHE.rep cs d
+  >} get cs.
+
+Theorem recover_ok : forall cs,
+  {< d d1_old d2_old d1 d2,
+  PRE
+    BUFCACHE.rep cs d *
+    crash_xform (crep d1_old d2_old d1 d2)
+  POST RET:cs
+    rep d1_old d2_old \/
+    rep d1 d2 \/
+    rep default_valu default_valu
+  CRASH
+    crep d1_old d2_old d1 d2
+  >} recover cs.
