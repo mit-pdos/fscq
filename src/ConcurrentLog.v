873495c48a35ebf58c1a0cff913cac0f36955f6d
Tej Chajed <tchajed@mit.edu>
2015-10-30 14:05:41 -0400

    Simplify log to just two entries

--- src/ConcurrentLog.v
+++ src/ConcurrentLog.v
@@ -9,11 +9,11 @@ Require Import Rec.
 Record entry := { log_addr : addr;
                       log_valu : valu }.
 
-(* we will store the log length in commit_sector *)
-Definition max_entries := 5.
-Definition commit_sector : addr := $ (max_entries * 2).
-
-Hint Opaque max_entries.
+Definition addr1_loc : addr := $0.
+Definition valu1_loc : addr := $1.
+Definition addr2_loc : addr := $2.
+Definition valu2_loc : addr := $3.
+Definition commit_sector : addr := $4.
 
 Definition addr_record_type : Rec.type :=
   Rec.RecF [("addr", Rec.WordF addrlen);
@@ -33,42 +33,70 @@ Inductive log_state :=
 | WrittenUnsynced
 | Synced.
 
-Implicit Type (entries : list entry).
+Inductive log_entries :=
+| Empty
+| OneEntry (e: entry)
+| TwoEntries (e1: entry) (e2: entry).
 
-Fixpoint log_entries_unsynced entries k : DISK_PRED :=
+Implicit Type (entries : log_entries).
+
+Definition entries_length entries :=
   match entries with
-  | nil => emp
-  | {| log_addr := a; log_valu := v |} :: entries' =>
-    let a := serialize_addr a in
-    let addr_loc := $ (k * 2) in
-    let valu_loc := $ (k * 2 + 1) in
-    ((exists arest vrest, addr_loc |-> Valuset a arest * valu_loc |-> Valuset v vrest) *
-     log_entries_unsynced entries' (k+1))%pred
+  | Empty => 0
+  | OneEntry _ => 1
+  | TwoEntries _ _ => 2
   end.
 
-Fixpoint log_entries_synced entries k : DISK_PRED :=
+Definition log_entries_unsynced entries : DISK_PRED :=
   match entries with
-  | nil => emp
-  | {| log_addr := a; log_valu := v |} :: entries' =>
-    let a := serialize_addr a in
-    let addr_loc := $ (k * 2) in
-    let valu_loc := $ (k * 2 + 1) in
-    (addr_loc |-> Valuset a nil * valu_loc |-> Valuset v nil *
-     log_entries_synced entries' (k+1))%pred
+  | Empty => emp
+  | OneEntry e =>
+    let a := serialize_addr (log_addr e) in
+    let v := log_valu e in
+    (exists arest vrest, addr1_loc |-> Valuset a arest *
+                    valu1_loc |-> Valuset v vrest)%pred
+  | TwoEntries e1 e2 =>
+    let a1 := serialize_addr (log_addr e1) in
+    let a2 := serialize_addr (log_addr e2) in
+    let v1 := log_valu e1 in
+    let v2 := log_valu e2 in
+    (exists arest1 vrest1 arest2 vrest2, addr1_loc |-> Valuset a1 arest1 *
+                                    valu1_loc |-> Valuset v1 vrest1 *
+                                    addr2_loc |-> Valuset a2 arest2 *
+                                    valu2_loc |-> Valuset v2 vrest2)%pred
   end.
 
-Fixpoint log_entries_disk_pred (p: entry -> pred) entries : DISK_PRED :=
+Definition log_entries_synced entries : DISK_PRED :=
   match entries with
-  | nil => emp
-  | e :: entries' =>
-    p e * log_entries_disk_pred p entries'
+  | Empty => emp
+  | OneEntry e =>
+    let a := serialize_addr (log_addr e) in
+    let v := log_valu e in
+    (addr1_loc |-> Valuset a nil *
+     valu1_loc |-> Valuset v nil)%pred
+  | TwoEntries e1 e2 =>
+    let a1 := serialize_addr (log_addr e1) in
+    let a2 := serialize_addr (log_addr e2) in
+    let v1 := log_valu e1 in
+    let v2 := log_valu e2 in
+    (addr1_loc |-> Valuset a1 nil *
+     valu1_loc |-> Valuset v1 nil *
+     addr2_loc |-> Valuset a2 nil *
+     valu2_loc |-> Valuset v2 nil)%pred
   end.
 
-Definition log_entries_disk_unwritten :=
+Definition log_entries_disk_pred (p: entry -> pred) entries : DISK_PRED :=
+  match entries with
+  | Empty => emp
+  | OneEntry e => p e
+  | TwoEntries e1 e2 => p e1 * p e2
+  end.
+
+Definition log_entries_disk_unwritten entries :=
   log_entries_disk_pred
     (fun e =>
        let '{| log_addr := a; log_valu := _ |} := e in
-       a |->?)%pred.
+       a |->?)%pred entries.
 
 Definition log_entries_disk_unsynced :=
   log_entries_disk_pred
@@ -82,36 +110,41 @@ Definition log_entries_disk_synced :=
        let '{| log_addr := a; log_valu := v |} := e in
        a |-> Valuset v nil)%pred.
 
-Fixpoint remaining_entries n : DISK_PRED :=
-  match n with
-  | 0 => emp
-  | Datatypes.S n' => let start_index := (max_entries - n) * 2 in
-                      ($ start_index) |->? * ($ (start_index+1)) |->? *
-                      remaining_entries n'
+Fixpoint remaining_entries entries : DISK_PRED :=
+  match entries with
+  | Empty => (exists a1 v1 a2 v2,
+                 addr1_loc |-> a1 *
+                 valu1_loc |-> v1 *
+                 addr2_loc |-> a2 *
+                 valu2_loc |-> v2)%pred
+  | OneEntry _ => (exists a2 v2,
+                      addr2_loc |-> a2 *
+                      valu2_loc |-> v2)%pred
+  | TwoEntries _ _ => emp
   end.
 
 Definition log_rep st entries : DISK_PRED :=
   match st with
-  | Committed => remaining_entries max_entries *
-                 [[ entries = nil ]]
-  | ActiveTxn => log_entries_unsynced entries 0 *
-                 remaining_entries (max_entries - length entries) *
+  | Committed => remaining_entries entries *
+                 [[ entries = Empty ]]
+  | ActiveTxn => log_entries_unsynced entries *
+                 remaining_entries entries *
                  log_entries_disk_unwritten entries
-  | TxnSynced => log_entries_synced entries 0 *
-                 remaining_entries (max_entries - length entries) *
+  | TxnSynced => log_entries_synced entries *
+                 remaining_entries entries *
                  log_entries_disk_unwritten entries
-  | WrittenUnsynced => log_entries_synced entries 0 *
-                       remaining_entries (max_entries - length entries) *
+  | WrittenUnsynced => log_entries_synced entries *
+                       remaining_entries entries *
                        log_entries_disk_unsynced entries
-  | Synced => log_entries_synced entries 0 *
-              remaining_entries (max_entries - length entries) *
+  | Synced => log_entries_synced entries *
+              remaining_entries entries *
               log_entries_disk_synced entries
   end.
 
 Definition Mcontents : list Set := [nat].
 Definition LogLength : var Mcontents _ := HFirst.
 
-Definition Scontents : list Set := [list entry; log_state].
+Definition Scontents : list Set := [log_entries; log_state].
 Definition S := hlist (fun T:Set => T) Scontents.
 
 Definition GLogEntries : var Scontents _ := HFirst.
@@ -122,7 +155,7 @@ Definition log_inv : Invariant Mcontents S :=
   fun m s d =>
     let entries := get GLogEntries s in
     let st := get LogState s in
-    get LogLength m = length entries /\
+    get LogLength m = entries_length entries /\
     (d |= log_rep st entries)%judgement.
 
 Definition log_relation : ID -> Relation S :=
@@ -140,7 +173,7 @@ Definition logS : transitions Mcontents S :=
 
 Definition log_append {T} a v rx : prog Mcontents S T :=
   l <- Get LogLength;
-  let off := (l+1)*2 in
+  let off := l*2 in
   Write ($ off) (serialize_addr a);;
         Write ($ (off + 1)) v;;
         Assgn LogLength (l+1);;
@@ -156,51 +189,52 @@ Definition write_entry {T} e rx : prog Mcontents S T :=
 
 Definition log_sync {T} rx : prog Mcontents S T :=
   l <- Get LogLength;
-  _ <- For i < l
-| Ghost [ (_:unit) ]
-| Loopvar [ (_:unit) ]
-| Continuation lrx
-| LoopInv (fun d m s0 s => True)
-| OnCrash (fun d => True)
-| Begin
-    Sync ($ (i * 2));;
-    Sync ($ (i * 2 + 1));;
-    lrx (tt, tt)
-| Rof (tt, tt);
-  Write commit_sector ($ l);;
-        Sync commit_sector;;
-        rx tt.
+  match l with
+  | 0 => rx tt
+  | 1 => Sync addr1_loc;;
+              Sync valu1_loc;;
+              Write commit_sector ($ l);;
+              Sync commit_sector;;
+              rx tt
+  | 2 => Sync addr1_loc;;
+              Sync valu1_loc;;
+              Sync addr2_loc;;
+              Sync valu2_loc;;
+              Write commit_sector ($ l);;
+              Sync commit_sector;;
+              rx tt
+  | _ => rx tt
+  end.
 
 Definition log_write {T} rx : prog Mcontents S T :=
   l <- Get LogLength;
-  _ <- For i < l
-| Ghost [ (_:unit) ]
-| Loopvar [ (_:unit) ]
-| Continuation lrx
-| LoopInv (fun d m s0 s => True)
-| OnCrash (fun d => True)
-| Begin
-    e <- read_entry i;
-  write_entry e;;
-              lrx (tt, tt)
-
-| Rof (tt, tt);
-  rx tt.
+  match l with
+  | 0 => rx tt
+  | 1 => e <- read_entry 0;
+      write_entry e;;
+                  rx tt
+  | 2 => e1 <- read_entry 0;
+      write_entry e1;;
+                  e2 <- read_entry 1;
+      write_entry e2;;
+                  rx tt
+  | _ => rx tt
+  end.
 
 Definition log_data_sync {T} rx : prog Mcontents S T :=
   l <- Get LogLength;
-  _ <- For i < l
-| Ghost [ (_:unit) ]
-| Loopvar [ (_:unit) ]
-| Continuation lrx
-| LoopInv (fun d m s0 s => True)
-| OnCrash (fun d => True)
-| Begin
-    e <- read_entry i;
-  Sync (log_addr e);;
-       lrx (tt, tt)
-| Rof (tt, tt);
-  rx tt.
+  match l with
+  | 0 => rx tt
+  | 1 => e <- read_entry 0;
+      Sync (log_addr e);;
+           rx tt
+  | 2 => e1 <- read_entry 0;
+      Sync (log_addr e1);;
+           e2 <- read_entry 1;
+      Sync (log_addr e2);;
+           rx tt
+  | _ => rx tt
+  end.
 
 Definition log_finish_commit {T} rx : prog Mcontents S T :=
   Write commit_sector $0;;
