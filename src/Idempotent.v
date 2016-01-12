f553463f334e1aa22cce42ece29a23874bc76e3d
stephanie-wang <swang93@mit.edu>
2016-01-12 14:57:22 -0500

    Fix Idempotent

--- src/Idempotent.v
+++ src/Idempotent.v
@@ -4,9 +4,9 @@ Require Import Pred PredCrash.
 Require Import SepAuto.
 
 Lemma corr3_from_corr2_failed:
-  forall (TF TR: Type) m mr (p: prog TF) (r: prog TR) out
+  forall (TF TR: Type) m mr hmr (p: prog TF) (r: prog TR) out
          (crash: pred) ppre rpre crashdone_p crashdone_r,
-  exec_recover mr p r out
+  exec_recover mr hmr p r out
   -> TF = TR
   -> possible_crash m mr
   -> crash m
@@ -21,20 +21,20 @@ Proof.
   induction H; intros; try congruence.
   - edestruct H5; eauto.
     apply H3. eapply crash_xform_apply; eauto.
-    destruct H7. destruct H7. destruct H7. congruence.
-    destruct H7. destruct H7. congruence.
+    repeat (destruct H7; try congruence).
+    repeat (destruct H7; try congruence).
   - rewrite H0. eapply IHexec_recover.
     eauto. eauto. eauto. eauto. eauto. eauto.
     edestruct H5; eauto.
     apply H3. eapply crash_xform_apply; eauto.
-    destruct H9. destruct H9. destruct H9. congruence.
-    destruct H9. destruct H9. inversion H9. congruence.
+    repeat (destruct H9; try congruence).
+    repeat (destruct H9; try congruence).
 Qed.
 
 Lemma corr3_from_corr2_finished:
-  forall (TF TR: Type) m mr (p: prog TF) (r: prog TR) out
-         (crash: pred) ppre rpre crashdone_p crashdone_r m' v,
-  exec_recover mr p r out
+  forall (TF TR: Type) m mr hmr (p: prog TF) (r: prog TR) out
+         (crash: pred) ppre rpre crashdone_p crashdone_r m' hm' v,
+  exec_recover mr hmr p r out
   -> TF = TR
   -> possible_crash m mr
   -> crash m
@@ -42,7 +42,7 @@ Lemma corr3_from_corr2_finished:
   -> (crash_xform crash =p=> rpre crashdone_r crash)
   -> {{ ppre }} p
   -> {{ rpre }} r
-  -> out = RFinished TR m' v
+  -> out = RFinished TR m' hm' v
   -> crashdone_p v m'.
 Proof.
   intros.
@@ -51,13 +51,13 @@ Proof.
   - apply H3. eapply crash_xform_apply; eauto.
   - destruct H8. destruct H8. destruct H8.
     inversion H8. congruence.
-  - destruct H8. destruct H8. congruence.
+  - repeat (destruct H8; try congruence).
 Qed.
 
 Lemma corr3_from_corr2_recovered:
-  forall (TF TR: Type) m mr (p: prog TF) (r: prog TR) out
-         (crash: pred) ppre rpre crashdone_p crashdone_r m' v,
-  exec_recover mr p r out
+  forall (TF TR: Type) m mr hmr (p: prog TF) (r: prog TR) out
+         (crash: pred) ppre rpre crashdone_p crashdone_r m' hm' v,
+  exec_recover mr hmr p r out
   -> TF = TR
   -> possible_crash m mr
   -> crash m
@@ -65,24 +65,25 @@ Lemma corr3_from_corr2_recovered:
   -> (crash_xform crash =p=> rpre crashdone_r crash)
   -> {{ ppre }} p
   -> {{ rpre }} r
-  -> out = RRecovered TF m' v
+  -> out = RRecovered TF m' hm' v
   -> crashdone_r v m'.
 Proof.
   intros.
   generalize dependent m.
   induction H; intros; try congruence.
-  - eapply corr3_from_corr2_finished; eauto; try congruence.
+  - eapply corr3_from_corr2_finished; eauto.
     clear IHexec_recover H2.
     edestruct H5; eauto.
     + apply H3. eapply crash_xform_apply; eauto.
-    + destruct H2. destruct H2. destruct H2. congruence.
-    + destruct H2. destruct H2. congruence.
+    + repeat (destruct H2; try congruence).
+    + repeat (destruct H2; try congruence).
+    + instantiate (hm':=hm'). congruence.
   - eapply IHexec_recover; eauto; clear IHexec_recover H2.
     + inversion H7. auto.
     + edestruct H5; eauto.
       * apply H3. eapply crash_xform_apply; eauto.
-      * destruct H2. destruct H2. destruct H2. congruence.
-      * destruct H2. destruct H2. congruence.
+      * repeat (destruct H2; try congruence).
+      * repeat (destruct H2; try congruence).
 Qed.
 
 Theorem corr3_from_corr2: forall TF TR (p: prog TF) (r: prog TR) ppre rpre, {{ ppre }} p
@@ -90,7 +91,7 @@ Theorem corr3_from_corr2: forall TF TR (p: prog TF) (r: prog TR) ppre rpre, {{ p
   -> {{ fun done crashdone => exists crash,
         ppre done crash * [[ crash_xform crash =p=> rpre crashdone crash ]] }} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   destruct H1 as [crash H1].
   destruct_lift H1.
   inversion H2; subst.
@@ -107,12 +108,12 @@ Proof.
   - edestruct H; eauto; repeat deex; try congruence.
     inversion H8; clear H8; subst.
     clear H H1 H2 H3 ppre p m.
-    right. exists m''; exists v. intuition.
+    right. repeat eexists. intuition.
     eapply corr3_from_corr2_finished; eauto.
   - edestruct H; eauto; repeat deex; try congruence.
     inversion H8; clear H8; subst.
     clear H H1 H2 H3 ppre p m.
-    right. exists m''; exists v. intuition.
+    right. repeat eexists. intuition.
     eapply corr3_from_corr2_recovered; eauto.
 Qed.
 
