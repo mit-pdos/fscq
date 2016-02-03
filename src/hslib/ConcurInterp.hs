64edff50643821adb17a142f8600dc12b57c089e
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-02-03 16:48:33 -0500

    run some (boring and invalid) code in parallel

--- src/hslib/ConcurInterp.hs
+++ src/hslib/ConcurInterp.hs
@@ -5,12 +5,12 @@ import Hlist
 import qualified Disk
 
 verbose :: Bool
-verbose = False
+verbose = True
 
-debugmsg :: String -> IO ()
-debugmsg s =
+debugmsg :: Int -> String -> IO ()
+debugmsg tid s =
   if verbose then
-    putStrLn s
+    putStrLn $ "[" ++ (show tid) ++ "] " ++ s
   else
     return ()
 
@@ -19,48 +19,48 @@ hmember_to_int (HFirst _) = 0
 hmember_to_int (HNext _ _ x) = 1 + (hmember_to_int x)
 
 run_dcode :: Disk.DiskState -> Int -> EventCSL.Coq_prog a -> IO a
-run_dcode _ _ (Done r) = do
-  debugmsg $ "Done"
+run_dcode _ tid (Done r) = do
+  debugmsg tid $ "Done"
   return r
 run_dcode ds tid (StartRead a rx) = do
-  debugmsg $ "StartRead " ++ (show a)
+  debugmsg tid $ "StartRead " ++ (show a)
   -- XXX start a read, somehow...
   run_dcode ds tid $ rx ()
 run_dcode ds tid (FinishRead a rx) = do
-  debugmsg $ "FinishRead " ++ (show a)
+  debugmsg tid $ "FinishRead " ++ (show a)
   -- XXX it would be nice if we didn't wait until the last minute to read..
   val <- Disk.read_disk ds a
   run_dcode ds tid $ rx val
 run_dcode ds tid (Write a v rx) = do
-  debugmsg $ "Write " ++ (show a) ++ " " ++ (show v)
+  debugmsg tid $ "Write " ++ (show a) ++ " " ++ (show v)
   Disk.write_disk ds a v
   run_dcode ds tid $ rx ()
 run_dcode ds tid (Sync a rx) = do
-  debugmsg $ "Sync " ++ (show a)
+  debugmsg tid $ "Sync " ++ (show a)
   Disk.sync_disk ds a
   run_dcode ds tid $ rx ()
 run_dcode ds tid (Get a rx) = do
-  debugmsg $ "Get " ++ (show (hmember_to_int a))
+  debugmsg tid $ "Get " ++ (show (hmember_to_int a))
   val <- Disk.get_var ds (hmember_to_int a)
   run_dcode ds tid $ rx val
 run_dcode ds tid (Assgn a v rx) = do
-  debugmsg $ "Assgn " ++ (show (hmember_to_int a))
+  debugmsg tid $ "Assgn " ++ (show (hmember_to_int a))
   Disk.set_var ds (hmember_to_int a) v
   run_dcode ds tid $ rx ()
 run_dcode ds tid (GetTID rx) = do
-  debugmsg $ "GetTID"
+  debugmsg tid $ "GetTID"
   run_dcode ds tid $ rx tid
 run_dcode ds tid (Yield rx) = do
-  debugmsg $ "Yield"
+  debugmsg tid $ "Yield"
   Disk.release_global_lock ds
   -- XXX should we wait for a little bit?
   Disk.acquire_global_lock ds
   run_dcode ds tid $ rx ()
 run_dcode ds tid (GhostUpdate _ rx) = do
-  debugmsg $ "GhostUpdate"
+  debugmsg tid $ "GhostUpdate"
   run_dcode ds tid $ rx ()
 run_dcode ds tid (AcquireLock lockvar xx rx) = do
-  debugmsg $ "AcquireLock"
+  debugmsg tid $ "AcquireLock"
   val <- Disk.get_var ds (hmember_to_int lockvar)
   case (unsafeCoerce val) of
     Open -> do
--- src/hstest.hs
+++ src/hstest.hs
@@ -1,79 +1,55 @@
 module Main where
 
 import Word
-import qualified Interpreter as I
-import qualified System.Directory
-import qualified Testprog
-import qualified FS
-import FSLayout
+import qualified ConcurInterp as I
+import qualified TwoBlockExample
+import qualified EventCSL
 import Disk
-import Errno
+import Control.Concurrent
 
 disk_fn :: String
 disk_fn = "disk.img"
 
--- the_prog :: Log.Coq_xparams -> Prog.Coq_prog ()
--- the_prog xp =
---   _LOG__init xp $ \_ ->
---   _LOG__begin xp $ \_ ->
---   _LOG__read xp (W64 5) $ \v ->
---   _LOG__write xp (W64 6) v $ \_ ->
---   _LOG__commit xp $ \_ ->
---   Prog.Done ()
+waitForChildren :: MVar [MVar ()] -> IO ()
+waitForChildren children = do
+  cs <- takeMVar children
+  case cs of
+    []   -> return ()
+    m:ms -> do
+      putMVar children ms
+      takeMVar m
+      waitForChildren children
 
-repf :: Integer -> t -> s -> (s -> t -> IO (s, t)) -> IO (s, t)
-repf 0 x s _ = return (s, x)
-repf n x s f = do
-  (s, y) <- f s x
-  (s, z) <- repf (n-1) y s f
-  return (s, z)
+forkChild :: MVar [MVar ()] -> IO () -> IO ()
+forkChild children io = do
+  mvar <- newEmptyMVar
+  childlist <- takeMVar children
+  putMVar children (mvar : childlist)
+  _ <- forkFinally io (\_ -> putMVar mvar ())
+  return ()
 
-repf2 :: Integer -> r -> s -> (s -> IO (s, (r, ()))) -> IO (s, (r, ()))
-repf2 0 r s _ = return (s, (r, ()))
-repf2 n _ s f = do
-  (s, (r, ())) <- f s
-  (s, (rr, ())) <- repf2 (n-1) r s f
-  return (s, (rr, ()))
+testprog :: Disk.DiskState -> Int -> IO ()
+testprog ds tid = do
+  _ <- I.run ds tid $
+    \done -> EventCSL.StartRead (natToWord 64 tid) $
+    \_ -> EventCSL.Yield $
+    \_ -> EventCSL.FinishRead (natToWord 64 tid) $
+    \val -> EventCSL.Write (natToWord 64 (tid+1)) val $
+    done
+  return ()
 
 main :: IO ()
 main = do
-  -- This is racy (stat'ing the file first and opening it later)
-  fileExists <- System.Directory.doesFileExist disk_fn
   ds <- init_disk disk_fn
-  (s, fsxp) <- if fileExists
-  then
-    do
-      putStrLn $ "Recovering file system"
-      (s, (fsxp, ())) <- I.run ds $ FS.recover
-      return (s, fsxp)
-  else
-    do
-      putStrLn $ "Initializing file system"
-      res <- I.run ds $ FS.mkfs (W 1) (W 1)
-      case res of
-        Err _ -> error $ "mkfs failed"
-        OK (s, (fsxp, ())) -> do
-          set_nblocks_disk ds $ wordToNat 64 $ coq_FSXPMaxBlock fsxp
-          return (s, fsxp)
-  putStrLn $ "File system mounted, " ++ (show $ coq_FSXPMaxBlock fsxp) ++ " blocks"
-  putStrLn "Running program.."
-  -- r <- I.run ds $ the_prog lxp
-  -- r <- I.run ds $ Testprog.testcopy lxp
-  -- r <- I.run ds $ Testprog.testalloc lxp bxp
+  set_nblocks_disk ds 128
+  putStrLn "Disk initialized.."
 
-  -- (s, r) <- repf 10000 (Just (W 123)) s
-  --     (\s x -> case x of
-  --         Nothing -> return (s, Nothing)
-  --         Just xv -> I.run ds $ Testprog.test_bfile fsxp xv s)
-
-  -- (s, (setok, ())) <- I.run ds $ FS.set_size fsxp (W 3) (W 68) s
-  -- putStrLn $ "set_size: " ++ (show setok)
-  (s, (r, ())) <- repf2 1000 False s $ \s -> I.run ds $ Testprog.test_bfile_bulkwrite fsxp (W 99) (W 64) s
-
-  flushlog <- get_flush_log ds
-  putStrLn $ "Flush log: " ++ (show $ length flushlog)
+  children <- newMVar []
+  forkChild children $ testprog ds 3
+  forkChild children $ testprog ds 5
+  waitForChildren children
 
   stats <- close_disk ds
   print_stats stats
-  putStrLn $ "Done: " ++ (show r)
+  putStrLn "Done."
 
