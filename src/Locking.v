db7a9d851e67e1e9690cd671b2b6d60bb1ab89c5
Tej Chajed <tchajed@mit.edu>
2016-02-04 18:26:18 -0500

    Revert "Start adjusting cache to per-address locks"
    This reverts commit 9b2c73aba212cefabe23fd97360c4cfc84bce415.

--- src/Locking.v
+++ src/Locking.v
@@ -13,57 +13,57 @@ Inductive BusyFlagOwner : Set :=
 
 (** given a lock variable and some other variable v, generate a
 relation for tid that makes the variable read-only for non-owners. *)
-Definition lock_protects (lvar : S Scontents -> BusyFlagOwner)
-           {tv} (v : S Scontents -> tv) tid (s s': S Scontents) :=
+Definition lock_protects (lvar : var Scontents BusyFlagOwner)
+           {tv} (v : var Scontents tv) tid (s s': S Scontents) :=
   forall owner_tid,
-    lvar s = Owned owner_tid ->
+    get lvar s = Owned owner_tid ->
     tid <> owner_tid ->
-    v s' = v s.
+    get v s' = get v s.
 
 Hint Unfold lock_protects : prog.
 
-Inductive lock_protocol (lvar : S Scontents -> BusyFlagOwner) (tid : ID) :
+Inductive lock_protocol (lvar : var Scontents BusyFlagOwner) (tid : ID) :
   S Scontents -> S Scontents -> Prop :=
-| NoChange : forall s s', lvar s  = lvar s' ->
+| NoChange : forall s s', get lvar s  = get lvar s' ->
                      lock_protocol lvar tid s s'
-| OwnerRelease : forall s s', lvar s = Owned tid ->
-                         lvar s' = NoOwner ->
+| OwnerRelease : forall s s', get lvar s = Owned tid ->
+                         get lvar s' = NoOwner ->
                          lock_protocol lvar tid s s'
-| OwnerAcquire : forall s s', lvar s = NoOwner ->
-                         lvar s' = Owned tid ->
+| OwnerAcquire : forall s s', get lvar s = NoOwner ->
+                         get lvar s' = Owned tid ->
                          lock_protocol lvar tid s s'.
 
 Hint Constructors lock_protocol.
 
 Inductive ghost_lock_invariant
-          (lvar : M Mcontents -> BusyFlag)
-          (glvar : S Scontents -> BusyFlagOwner)
+          (lvar : var Mcontents BusyFlag)
+          (glvar : var Scontents BusyFlagOwner)
           (m : M Mcontents) (s : S Scontents) : Prop :=
-| LockOpen : lvar m = Open -> glvar s = NoOwner ->
+| LockOpen : get lvar m = Open -> get glvar s = NoOwner ->
              ghost_lock_invariant lvar glvar m s
-| LockOwned : forall tid, lvar m = Locked -> glvar s = Owned tid ->
+| LockOwned : forall tid, get lvar m = Locked -> get glvar s = Owned tid ->
                      ghost_lock_invariant lvar glvar m s.
 
 Hint Constructors ghost_lock_invariant.
 
 Lemma ghost_lock_owned : forall lvar glvar m s tid,
     ghost_lock_invariant lvar glvar m s ->
-    glvar s = Owned tid ->
-    lvar m = Locked.
+    get glvar s = Owned tid ->
+    get lvar m = Locked.
 Proof.
   inversion 1; congruence.
 Qed.
 
-Theorem ghost_lock_inv_preserved :
-  forall lvar glvar m s m' s',
+Theorem ghost_lock_inv_preserved : forall
+  (lvar: var Mcontents BusyFlag) (glvar: var Scontents BusyFlagOwner) m s m' s',
   ghost_lock_invariant lvar glvar m s ->
-  lvar m = lvar m' ->
-  glvar s = glvar s' ->
+  get lvar m = get lvar m' ->
+  get glvar s = get glvar s' ->
   ghost_lock_invariant lvar glvar m' s'.
 Proof.
   inversion 1; intros;
   repeat match goal with
-       | [ H: ?v ?m = ?x, H': ?v ?m = ?y |- _ ] =>
+       | [ H: get ?v ?m = ?x, H': get ?v ?m = ?y |- _ ] =>
           lazymatch goal with
           | [ Heq: x = y |- _ ] => fail
           | _ => assert (x = y) by congruence
