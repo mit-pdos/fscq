aa692d46c5d154ff93a366c108ff0b37584ab935
Haogang Chen <hchen@csail.mit.edu>
2016-03-17 01:53:55 -0400

    Balloc: prove alloc_ok and free_ok

--- src/Balloc.v
+++ src/Balloc.v
@@ -74,7 +74,167 @@ Module BALLOC (Sig : AllocSig).
     left; apply eq_refl.
   Defined.
 
-  
+  Definition Avail (s : state) : Prop := s = $0.
+  Definition InUse (s : state) : Prop := s = $1.
+
+  Definition is_avail (s : state) := if (state_dec s) then true else false.
+  Definition is_inuse (s : state) := if (state_dec s) then false else true.
+
+  Definition free T lxp xp bn ms rx : prog T :=
+    ms <- Bmp.put lxp xp bn $0 ms;
+    rx ms.
+
+  Definition alloc T lxp xp ms rx : prog T :=
+    let^ (ms, r) <- Bmp.ifind lxp xp is_avail ms;
+    match r with
+    | None =>
+        rx ^(ms, None)
+    | Some (bn, _) =>
+        ms <- Bmp.put lxp xp bn $1 ms;
+        rx ^(ms, Some bn)
+    end.
+
+  Definition freelist_bmap_equiv freelist bmap :=
+    forall a, a < length bmap -> (In a freelist <-> Avail (selN bmap a $0)).
+
+  Definition rep V xp (freelist : list addr) (freepred : @pred _ addr_eq_dec V) :=
+    (exists bmap, Bmp.rep xp bmap *
+     [[ freelist_bmap_equiv freelist bmap ]] *
+     [[ freepred = listpred (fun a => a |->?) freelist ]] )%pred.
+
+
+  Lemma freelist_bmap_equiv_remove_ok : forall bmap freelist a,
+    freelist_bmap_equiv freelist bmap ->
+    is_avail (selN bmap a $0) = true ->
+    a < length bmap ->
+    freelist_bmap_equiv (remove addr_eq_dec a freelist) (updN bmap a $1).
+  Proof.
+    unfold freelist_bmap_equiv; split; intros.
+    destruct (addr_eq_dec a a0); subst.
+    rewrite selN_updN_eq by auto.
+    exfalso; eapply remove_In; eauto.
+    rewrite selN_updN_ne by auto.
+    apply H.
+    erewrite <- length_updN; eauto.
+    eapply remove_still_In; eauto.
+
+    destruct (addr_eq_dec a a0); subst.
+    contradict H3.
+    rewrite selN_updN_eq by auto.
+    discriminate.
+    apply remove_other_In; auto.
+    apply H.
+    erewrite <- length_updN; eauto.
+    erewrite <- selN_updN_ne; eauto.
+  Qed.
+
+  Lemma freelist_bmap_equiv_add_ok : forall bmap freelist a,
+    freelist_bmap_equiv freelist bmap ->
+    a < length bmap ->
+    freelist_bmap_equiv (a :: freelist) (updN bmap a $0).
+  Proof.
+    unfold freelist_bmap_equiv; split; intros.
+    destruct (addr_eq_dec a a0); subst.
+    rewrite selN_updN_eq by auto.
+    unfold Avail; auto.
+    rewrite selN_updN_ne by auto.
+    apply H.
+    erewrite <- length_updN; eauto.
+    simpl in H2; destruct H2; auto; congruence.
+
+    destruct (addr_eq_dec a a0); subst; simpl; auto.
+    right; apply H.
+    erewrite <- length_updN; eauto.
+    erewrite <- selN_updN_ne; eauto.
+  Qed.
+
+  Lemma is_avail_in_freelist : forall a bmap freelist,
+    freelist_bmap_equiv freelist bmap ->
+    is_avail (selN bmap a $0) = true ->
+    a < length bmap ->
+    In a freelist.
+  Proof.
+    unfold freelist_bmap_equiv, is_avail, Avail.
+    intros; apply H; auto.
+    destruct (state_dec (selN bmap a $0)); auto; congruence.
+  Qed.
+
+
+  Lemma bmap_rep_length_ok1 : forall F xp bmap d a,
+    a < length bmap ->
+    (F * Bmp.rep xp bmap)%pred d ->
+    a < BMPLen xp * valulen.
+  Proof.
+    unfold Bmp.rep, Bmp.items_valid; intros.
+    destruct_lift H0.
+    eapply lt_le_trans; eauto.
+    rewrite H6; auto.
+  Qed.
+
+  Lemma bmap_rep_length_ok2 : forall F xp bmap d a,
+    (F * Bmp.rep xp bmap)%pred d ->
+    a < BMPLen xp * valulen ->
+    a < length bmap.
+  Proof.
+    unfold Bmp.rep, Bmp.items_valid; intros.
+    destruct_lift H.
+    eapply lt_le_trans; eauto.
+    rewrite H6; auto.
+  Qed.
+
+
+  Hint Extern 0 (okToUnify (listpred ?prd _ ) (listpred ?prd _)) => constructor : okToUnify.
+
+  Theorem alloc_ok : forall V lxp xp ms,
+    {< F Fm m0 m freelist freepred,
+    PRE   LOG.rep lxp F (LOG.ActiveTxn m0 m) ms *
+          [[[ m ::: (Fm * @rep V xp freelist freepred) ]]]
+    POST RET:^(ms,r)
+          [[ r = None ]] * LOG.rep lxp F (LOG.ActiveTxn m0 m) ms
+       \/ exists bn m' freelist' freepred', 
+          [[ r = Some bn ]] * LOG.rep lxp F (LOG.ActiveTxn m0 m') ms *
+          [[[ m' ::: (Fm * @rep V xp freelist' freepred') ]]] *
+          [[ freepred =p=> freepred' * bn |->? ]] *
+          [[ bn < (BMPLen xp) * valulen ]]
+    CRASH LOG.intact lxp F m0
+    >} alloc lxp xp ms.
+  Proof.
+    unfold alloc, rep.
+    hoare.
+
+    or_r; cancel.
+    eapply freelist_bmap_equiv_remove_ok; eauto.
+    rewrite listpred_remove; try cancel.
+    intros; apply ptsto_conflict.
+    eapply is_avail_in_freelist; eauto.
+    eapply bmap_rep_length_ok1; eauto.
+  Qed.
+
+
+  Theorem free_ok : forall V lxp xp bn ms,
+    {< F Fm m0 m freelist freepred,
+    PRE   LOG.rep lxp F (LOG.ActiveTxn m0 m) ms *
+          [[ bn < (BMPLen xp) * valulen ]] *
+          [[[ m ::: (Fm * @rep V xp freelist freepred) ]]]
+    POST RET:ms exists m' freepred',
+          LOG.rep lxp F (LOG.ActiveTxn m0 m') ms *
+          [[[ m' ::: (Fm * @rep V xp (bn :: freelist) freepred') ]]] *
+          [[ bn |->? * freepred =p=> freepred' ]]
+    CRASH LOG.intact lxp F m0
+    >} free lxp xp bn ms.
+  Proof.
+    unfold free, rep.
+    hoare.
+
+    eapply bmap_rep_length_ok2; eauto.
+    apply freelist_bmap_equiv_add_ok; auto.
+    eapply bmap_rep_length_ok2; eauto.
+  Qed.
+
+
+  Hint Extern 1 ({{_}} progseq (alloc _ _ _) _) => apply alloc_ok : prog.
+  Hint Extern 1 ({{_}} progseq (free _ _ _ _) _) => apply free_ok : prog.
+  Hint Extern 0 (okToUnify (rep _ _ _) (rep _ _ _)) => constructor : okToUnify.
 
 
 
