gsmadhusudan/fscq-impl

Starting to convert Hoare logic (corr2, corr3) to include reasoning about hashmap state

src/Hoare.v
@@ -12,22 +12,32 @@ Set Implicit Arguments.
 
 Definition donecond (T: Type) := T -> @mem addr (@weq addrlen) valuset -> Prop.
 
+Definition corr2' (T: Type) (pre: hashmap -> donecond T -> pred -> pred) (p: prog T) :=
+  forall done crash m hm out, pre hm done crash m
+  -> exec m hm p out
+  -> (exists m' hm' v, out = Finished m' hm' v /\ done v m') \/
+     (exists m' hm', out = Crashed T m' hm' /\ crash m').
+
 Definition corr2 (T: Type) (pre: donecond T -> pred -> pred) (p: prog T) :=
-  forall done crash m out, pre done crash m
-  -> exec m p out
-  -> (exists m' v, out = Finished m' v /\ done v m') \/
-     (exists m', out = Crashed T m' /\ crash m').
+  corr2' (fun hm => pre) p.
+
+Notation "{{{ pre }}} p" := (corr2' pre%pred p)
+  (at level 0, p at level 60).
 
 Notation "{{ pre }} p" := (corr2 pre%pred p)
   (at level 0, p at level 60).
 
 
-Definition corr3 (TF TR: Type) (pre: donecond TF -> donecond TR -> pred)
+Definition corr3' (TF TR: Type) (pre: hashmap -> donecond TF -> donecond TR -> pred)
                  (p1: prog TF) (p2: prog TR) :=
-  forall done crashdone m out, pre done crashdone m
-  -> exec_recover m p1 p2 out
-  -> (exists m' v, out = RFinished TR m' v /\ done v m') \/
-     (exists m' v, out = RRecovered TF m' v /\ crashdone v m').
+  forall done crashdone m hm out, pre hm done crashdone m
+  -> exec_recover m hm p1 p2 out
+  -> (exists m' hm' v, out = RFinished TR m' hm' v /\ done v m') \/
+     (exists m' hm' v, out = RRecovered TF m' hm' v /\ crashdone v m').
+
+Definition corr3 (TF TR: Type) (pre: donecond TF -> donecond TR -> pred)
+                (p1: prog TF) (p2: prog TR) :=
+  corr3' (fun hm => pre ) p1 p2.
 
 Notation "{{ pre }} p1 >> p2" := (corr3 pre%pred p1 p2)
   (at level 0, p1 at level 60, p2 at level 60).
@@ -46,38 +56,70 @@ Notation "'RET' : ^( ra , .. , rb ) post" :=
   )%pred
   (at level 0, post at level 90, ra closed binder, rb closed binder, only parsing).
 
-Notation "{< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >} p1" :=
-  (forall T (rx: _ -> prog T), corr2
-   (fun done_ crash_ =>
+(**
+  * Underlying CHL that allows pre, post, and crash conditions to state
+  * propositions about the hashmap machine state.
+  * TODO: Add crash-condition that the starting hashmap is a subset of
+  * any hashmap after a crash.
+  *)
+Notation "{{< e1 .. e2 , 'PRE' : hm pre 'POST' : hm' post 'CRASH' : hm_crash crash >}} p1" :=
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
     (exis (fun e1 => .. (exis (fun e2 =>
      exists F_,
      F_ * pre *
-     [[ forall r_,
-        {{ fun done'_ crash'_ =>
+     [[ forall r_ ,
+        {{{ fun hm' done'_ crash'_ =>
            post F_ r_ *
            [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
-        }} rx r_ ]] *
-     [[ (F_ * crash)%pred =p=> crash_ ]]
+        }}} rx r_ ]] *
+     [[ forall (hm_crash : hashmap), (F_ * crash)%pred =p=> crash_ ]]
      )) .. ))
    )%pred
    (p1 rx)%pred)
-  (at level 0, p1 at level 60, e1 binder, e2 binder).
+  (at level 0, p1 at level 60,
+    hm at level 0, hm' at level 0, hm_crash at level 0,
+    e1 closed binder, e2 closed binder).
+
+(**
+  * Same as {{< ... >}}, except that the frame F_ can also include
+  * propositions about the hashmap machine state.
+  * TODO: How to reuse the above notation? I want to carry over
+  * the e1 .. e2 as the e1 .. e2 of {{< ... >}}, but not sure how.
+  *)
+Notation "{< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >} p1" :=
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
+    (exis (fun e1 => .. (exis (fun e2 =>
+     exists F_,
+     F_ * pre *
+     [[ forall r_ ,
+        {{{ fun hm' done'_ crash'_ =>
+           post F_ r_ *
+           [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
+        }}} rx r_ ]] *
+     [[ forall (hm_crash : hashmap), (F_ * crash * [[ hm_crash = hm ]])%pred =p=> crash_ ]]
+     )) .. ))
+   )%pred
+   (p1 rx)%pred)
+  (at level 0, p1 at level 60,
+    e1 closed binder, e2 closed binder).
 
 (**
  * The {!< .. >!} notation is the same as above, except it lacks a frame
  * predicate.  This is useful for bootstrapping-style programs.
  *)
 Notation "{!< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >!} p1" :=
-  (forall T (rx: _ -> prog T), corr2
-   (fun done_ crash_ =>
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
     (exis (fun e1 => .. (exis (fun e2 =>
      pre *
      [[ forall r_,
-        {{ fun done'_ crash'_ =>
+        {{ fun hm' done'_ crash'_ =>
            post emp r_ *
            [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
         }} rx r_ ]] *
-     [[ crash =p=> crash_ ]]
+     [[ forall (hm_crash : hashmap), crash =p=> crash_ ]]
      )) .. ))
    )%pred
    (p1 rx)%pred)
@@ -113,6 +155,7 @@ Inductive corr3_result {A B : Type} :=
   | Complete : A -> corr3_result
   | Recover : B -> corr3_result.
 
+(* TODO *)
 Notation "{<<< e1 .. e2 , 'PRE' pre 'POST' post >>>} p1 >> p2" :=
   (forall_helper (fun e1 => .. (forall_helper (fun e2 =>
    exists idemcrash,
@@ -143,7 +186,7 @@ Theorem pimpl_ok2:
   (forall done crash, pre done crash =p=> pre' done crash) ->
   {{pre}} pr.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -155,7 +198,7 @@ Theorem pimpl_ok3:
   (forall done crashdone, pre done crashdone =p=> pre' done crashdone) ->
   {{pre}} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -168,7 +211,7 @@ Theorem pimpl_ok2_cont :
   (forall done crash, pre done crash =p=> exists F, F * [[x = y]]) ->
   {{pre}} k x.
 Proof.
-  unfold corr2, pimpl; intros.
+  unfold corr2, corr2', pimpl; intros.
   edestruct H1; eauto.
   eapply sep_star_lift_l in H4; [|instantiate (1:=([x=y])%pred)].
   unfold lift in *; subst; eauto.
@@ -182,7 +225,7 @@ Theorem pimpl_ok3_cont :
   (forall done crashdone, pre done crashdone =p=> exists F, F * [[x = y]]) ->
   {{pre}} k x >> r.
 Proof.
-  unfold corr3, pimpl; intros.
+  unfold corr3, corr3', pimpl; intros.
   edestruct H1; eauto.
   eapply sep_star_lift_l in H4; [|instantiate (1:=([x=y])%pred)].
   unfold lift in *; subst; eauto.
@@ -195,7 +238,7 @@ Theorem pimpl_pre2:
   -> (forall done crash, pre done crash =p=> pre' done crash done crash)
   -> {{pre}} pr.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -207,7 +250,7 @@ Theorem pimpl_pre3:
   -> (forall done crashdone, pre done crashdone =p=> pre' done crashdone done crashdone)
   -> {{pre}} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -218,7 +261,7 @@ Theorem pre_false2:
   (forall done crash, pre done crash =p=> [False])
   -> {{ pre }} p.
 Proof.
-  unfold corr2; intros; exfalso.
+  unfold corr2, corr2'; intros; exfalso.
   eapply H; eauto.
 Qed.
 
@@ -227,7 +270,7 @@ Theorem pre_false3:
   (forall done crashdone, pre done crashdone =p=> [False])
   -> {{ pre }} p >> r.
 Proof.
-  unfold corr3; intros; exfalso.
+  unfold corr3, corr3'; intros; exfalso.
   eapply H; eauto.
 Qed.
 
@@ -237,7 +280,7 @@ Theorem corr2_exists:
   (forall (a:T), {{ fun done crash => pre done crash a }} p)
   -> {{ fun done crash => exists a:T, pre done crash a }} p.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   destruct H0.
   eapply H; eauto.
 Qed.
@@ -247,7 +290,7 @@ Theorem corr3_exists:
   (forall (a:T), {{ fun done crashdone => pre done crashdone a }} p >> r)
   -> {{ fun done crashdone => exists a:T, pre done crashdone a }} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   destruct H0.
   eapply H; eauto.
 Qed.
@@ -256,7 +299,7 @@ Theorem corr2_forall: forall T R pre (p: prog R),
   {{ fun done crash => exists a:T, pre done crash a }} p
   -> forall (a:T), {{ fun done crash => pre done crash a }} p.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   exists a; eauto.
 Qed.
@@ -265,7 +308,7 @@ Theorem corr3_forall: forall T RF RR pre (p: prog RF) (r: prog RR),
   {{ fun done crashdone => exists a:T, pre done crashdone a }} p >> r
   -> forall (a:T), {{ fun done crashdone => pre done crashdone a }} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   exists a; eauto.
 Qed.

src/Prog.v
@@ -162,7 +162,7 @@ Inductive prog (T: Type) :=
 | Write (a: addr) (v: valu) (rx: unit -> prog T)
 | Sync (a: addr) (rx: unit -> prog T)
 | Trim (a: addr) (rx: unit -> prog T)
-| Hash (sz: nat) (buf: word sz) (hm: hashmap) (rx: (word hashlen * (hashmap * unit)) -> prog T).
+| Hash (sz: nat) (buf: word sz) (rx: word hashlen -> prog T).
 
 Definition progseq (A B:Type) (a:B->A) (b:B) := a b.
 Definition pair_args_helper (A B C:Type) (f: A->B->C) (x: A*B) := f (fst x) (snd x).
@@ -192,35 +192,35 @@ Definition valuset_list (vs : valuset) := fst vs :: snd vs.
 
 Inductive outcome (T: Type) :=
 | Failed
-| Finished (m: @mem addr (@weq addrlen) valuset) (v: T)
-| Crashed (m: @mem addr (@weq addrlen) valuset).
+| Finished (m: @mem addr (@weq addrlen) valuset) (hm: hashmap) (v: T)
+| Crashed (m: @mem addr (@weq addrlen) valuset) (hm: hashmap).
 
 
-Inductive step (T: Type) : @mem _ (@weq addrlen) _ -> prog T ->
-                           @mem _ (@weq addrlen) _ -> prog T -> Prop :=
-| StepRead : forall m a rx v x, m a = Some (v, x) ->
-  step m (Read a rx) m (rx v)
-| StepWrite : forall m a rx v v0 x, m a = Some (v0, x) ->
-  step m (Write a v rx) (upd m a (v, v0 :: x)) (rx tt)
-| StepSync : forall m a rx v l, m a = Some (v, l) ->
-  step m (Sync a rx) (upd m a (v, nil)) (rx tt)
-| StepTrim : forall m a rx vs vs', m a = Some vs ->
-  step m (Trim a rx) (upd m a vs') (rx tt)
+Inductive step (T: Type) : @mem _ (@weq addrlen) _ -> hashmap -> prog T ->
+                           @mem _ (@weq addrlen) _ -> hashmap -> prog T -> Prop :=
+| StepRead : forall m a rx v x hm, m a = Some (v, x) ->
+  step m hm (Read a rx) m hm (rx v)
+| StepWrite : forall m a rx v v0 x hm, m a = Some (v0, x) ->
+  step m hm (Write a v rx) (upd m a (v, v0 :: x)) hm (rx tt)
+| StepSync : forall m a rx v l hm, m a = Some (v, l) ->
+  step m hm (Sync a rx) (upd m a (v, nil)) hm (rx tt)
+| StepTrim : forall m a rx vs vs' hm, m a = Some vs ->
+  step m hm (Trim a rx) (upd m a vs') hm (rx tt)
 | StepHash : forall m sz (buf : word sz) rx h hm,
   hash_safe hm h buf ->
   hash_fwd buf = h ->
-  step m (Hash buf hm rx) m (rx (h, (upd_hashmap' hm h buf, tt))).
+  step m hm (Hash buf rx) m (upd_hashmap' hm h buf) (rx h).
 
-Inductive exec (T: Type) : mem -> prog T -> outcome T -> Prop :=
-| XStep : forall m m' p p' out, step m p m' p' ->
-  exec m' p' out ->
-  exec m p out
-| XFail : forall m p, (~exists m' p', step m p m' p') ->
+Inductive exec (T: Type) : mem -> hashmap -> prog T -> outcome T -> Prop :=
+| XStep : forall m m' hm hm' p p' out, step m hm p m' hm' p' ->
+  exec m' hm' p' out ->
+  exec m hm p out
+| XFail : forall m hm p, (~exists m' hm' p', step m hm p m' hm' p') ->
   (~exists r, p = Done r) ->
-  (~exists sz (buf : word sz) hm rx, p = Hash buf hm rx) ->
-  exec m p (Failed T)
-| XCrash : forall m p, exec m p (Crashed T m)
-| XDone : forall m v, exec m (Done v) (Finished m v).
+  (~exists sz (buf : word sz) rx, p = Hash buf rx) ->
+  exec m hm p (Failed T)
+| XCrash : forall m hm p, exec m hm p (Crashed T m hm)
+| XDone : forall m hm v, exec m hm (Done v) (Finished m hm v).
 
 Hint Constructors exec.
 Hint Constructors step.
@@ -228,8 +228,8 @@ Hint Constructors step.
 
 Inductive recover_outcome (TF TR: Type) :=
 | RFailed
-| RFinished (m: @mem addr (@weq addrlen) valuset) (v: TF)
-| RRecovered (m: @mem addr (@weq addrlen) valuset) (v: TR).
+| RFinished (m: @mem addr (@weq addrlen) valuset) (hm: hashmap) (v: TF)
+| RRecovered (m: @mem addr (@weq addrlen) valuset) (hm: hashmap) (v: TR).
 
 Definition possible_crash {A : Type} {eq : DecEq A} (m m' : @mem A eq valuset) : Prop :=
   forall a,
@@ -237,23 +237,23 @@ Definition possible_crash {A : Type} {eq : DecEq A} (m m' : @mem A eq valuset) :
   (exists vs v', m a = Some vs /\ m' a = Some (v', nil) /\ In v' (valuset_list vs)).
 
 Inductive exec_recover (TF TR: Type)
-  : mem -> prog TF -> prog TR -> recover_outcome TF TR -> Prop :=
-| XRFail : forall m p1 p2, exec m p1 (Failed TF)
-  -> exec_recover m p1 p2 (RFailed TF TR)
-| XRFinished : forall m p1 p2 m' (v: TF), exec m p1 (Finished m' v)
-  -> exec_recover m p1 p2 (RFinished TR m' v)
-| XRCrashedFailed : forall m p1 p2 m' m'r, exec m p1 (Crashed TF m')
+  : mem -> hashmap -> prog TF -> prog TR -> recover_outcome TF TR -> Prop :=
+| XRFail : forall m hm p1 p2, exec m hm p1 (Failed TF)
+  -> exec_recover m hm p1 p2 (RFailed TF TR)
+| XRFinished : forall m hm p1 p2 m' hm' (v: TF), exec m hm p1 (Finished m' hm' v)
+  -> exec_recover m hm p1 p2 (RFinished TR m' hm' v)
+| XRCrashedFailed : forall m hm p1 p2 m' hm' m'r, exec m hm p1 (Crashed TF m' hm')
   -> possible_crash m' m'r
-  -> @exec_recover TR TR m'r p2 p2 (RFailed TR TR)
-  -> exec_recover m p1 p2 (RFailed TF TR)
-| XRCrashedFinished : forall m p1 p2 m' m'r m'' (v: TR), exec m p1 (Crashed TF m')
+  -> @exec_recover TR TR m'r hm' p2 p2 (RFailed TR TR)
+  -> exec_recover m hm p1 p2 (RFailed TF TR)
+| XRCrashedFinished : forall m hm p1 p2 m' hm' m'r m'' hm'' (v: TR), exec m hm p1 (Crashed TF m' hm')
   -> possible_crash m' m'r
-  -> @exec_recover TR TR m'r p2 p2 (RFinished TR m'' v)
-  -> exec_recover m p1 p2 (RRecovered TF m'' v)
-| XRCrashedRecovered : forall m p1 p2 m' m'r m'' (v: TR), exec m p1 (Crashed TF m')
+  -> @exec_recover TR TR m'r hm' p2 p2 (RFinished TR m'' hm'' v)
+  -> exec_recover m hm p1 p2 (RRecovered TF m'' hm'' v)
+| XRCrashedRecovered : forall m hm p1 p2 m' hm' m'r m'' hm'' (v: TR), exec m hm p1 (Crashed TF m' hm')
   -> possible_crash m' m'r
-  -> @exec_recover TR TR m'r p2 p2 (RRecovered TR m'' v)
-  -> exec_recover m p1 p2 (RRecovered TF m'' v).
+  -> @exec_recover TR TR m'r hm' p2 p2 (RRecovered TR m'' hm'' v)
+  -> exec_recover m hm p1 p2 (RRecovered TF m'' hm'' v).
 
 Hint Constructors exec_recover.
 

mit-pdos/fscq

src/Hoare.v
@@ -13,22 +13,32 @@ Set Implicit Arguments.
 
 Definition donecond (T: Type) := T -> rawdisk -> Prop.
 
-Definition corr2 (T: Type) (pre: donecond T -> rawpred -> rawpred) (p: prog T) :=
-  forall done crash m out, pre done crash m
-  -> exec m p out
-  -> (exists m' v, out = Finished m' v /\ done v m') \/
-     (exists m', out = Crashed T m' /\ crash m').
+Definition corr2' (T: Type) (pre: hashmap -> donecond T -> rawpred -> rawpred) (p: prog T) :=
+  forall done crash m hm out, pre hm done crash m
+  -> exec m hm p out
+  -> (exists m' hm' v, out = Finished m' hm' v /\ done v m') \/
+     (exists m' hm', out = Crashed T m' hm' /\ crash m').
+
+Definition corr2 (T: Type) (pre: donecond T -> pred -> pred) (p: prog T) :=
+  corr2' (fun hm => pre) p.
+
+Notation "{{{ pre }}} p" := (corr2' pre%pred p)
+  (at level 0, p at level 60).
 
 Notation "{{ pre }} p" := (corr2 pre%pred p)
   (at level 0, p at level 60).
 
 
-Definition corr3 (TF TR: Type) (pre: donecond TF -> donecond TR -> pred)
+Definition corr3' (TF TR: Type) (pre: hashmap -> donecond TF -> donecond TR -> pred)
                  (p1: prog TF) (p2: prog TR) :=
-  forall done crashdone m out, pre done crashdone m
-  -> exec_recover m p1 p2 out
-  -> (exists m' v, out = RFinished TR m' v /\ done v m') \/
-     (exists m' v, out = RRecovered TF m' v /\ crashdone v m').
+  forall done crashdone m hm out, pre hm done crashdone m
+  -> exec_recover m hm p1 p2 out
+  -> (exists m' hm' v, out = RFinished TR m' hm' v /\ done v m') \/
+     (exists m' hm' v, out = RRecovered TF m' hm' v /\ crashdone v m').
+
+Definition corr3 (TF TR: Type) (pre: donecond TF -> donecond TR -> pred)
+                (p1: prog TF) (p2: prog TR) :=
+  corr3' (fun hm => pre ) p1 p2.
 
 Notation "{{ pre }} p1 >> p2" := (corr3 pre%pred p1 p2)
   (at level 0, p1 at level 60, p2 at level 60).
@@ -47,22 +57,54 @@ Notation "'RET' : ^( ra , .. , rb ) post" :=
   )%pred
   (at level 0, post at level 90, ra closed binder, rb closed binder, only parsing).
 
-Notation "{< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >} p1" :=
-  (forall T (rx: _ -> prog T), corr2
-   (fun done_ crash_ =>
+(**
+  * Underlying CHL that allows pre, post, and crash conditions to state
+  * propositions about the hashmap machine state.
+  * TODO: Add crash-condition that the starting hashmap is a subset of
+  * any hashmap after a crash.
+  *)
+Notation "{{< e1 .. e2 , 'PRE' : hm pre 'POST' : hm' post 'CRASH' : hm_crash crash >}} p1" :=
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
     (exis (fun e1 => .. (exis (fun e2 =>
      exists F_,
      F_ * pre *
-     [[ forall r_,
-        {{ fun done'_ crash'_ =>
+     [[ forall r_ ,
+        {{{ fun hm' done'_ crash'_ =>
            post F_ r_ *
            [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
-        }} rx r_ ]] *
-     [[ (F_ * crash)%pred =p=> crash_ ]]
+        }}} rx r_ ]] *
+     [[ forall (hm_crash : hashmap), (F_ * crash)%pred =p=> crash_ ]]
      )) .. ))
    )%pred
    (p1 rx)%pred)
-  (at level 0, p1 at level 60, e1 binder, e2 binder).
+  (at level 0, p1 at level 60,
+    hm at level 0, hm' at level 0, hm_crash at level 0,
+    e1 closed binder, e2 closed binder).
+
+(**
+  * Same as {{< ... >}}, except that the frame F_ can also include
+  * propositions about the hashmap machine state.
+  * TODO: How to reuse the above notation? I want to carry over
+  * the e1 .. e2 as the e1 .. e2 of {{< ... >}}, but not sure how.
+  *)
+Notation "{< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >} p1" :=
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
+    (exis (fun e1 => .. (exis (fun e2 =>
+     exists F_,
+     F_ * pre *
+     [[ forall r_ ,
+        {{{ fun hm' done'_ crash'_ =>
+           post F_ r_ *
+           [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
+        }}} rx r_ ]] *
+     [[ forall (hm_crash : hashmap), (F_ * crash * [[ hm_crash = hm ]])%pred =p=> crash_ ]]
+     )) .. ))
+   )%pred
+   (p1 rx)%pred)
+  (at level 0, p1 at level 60,
+    e1 closed binder, e2 closed binder).
 
 
 (**
@@ -70,16 +112,16 @@ Notation "{< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >} p1" :=
  * predicate.  This is useful for bootstrapping-style programs.
  *)
 Notation "{!< e1 .. e2 , 'PRE' pre 'POST' post 'CRASH' crash >!} p1" :=
-  (forall T (rx: _ -> prog T), corr2
-   (fun done_ crash_ =>
+  (forall T (rx: _ -> prog T), corr2'
+   (fun hm done_ crash_ =>
     (exis (fun e1 => .. (exis (fun e2 =>
      pre *
      [[ forall r_,
-        {{ fun done'_ crash'_ =>
+        {{ fun hm' done'_ crash'_ =>
            post emp r_ *
            [[ done'_ = done_ ]] * [[ crash'_ = crash_ ]]
         }} rx r_ ]] *
-     [[ crash =p=> crash_ ]]
+     [[ forall (hm_crash : hashmap), crash =p=> crash_ ]]
      )) .. ))
    )%pred
    (p1 rx)%pred)
@@ -139,6 +181,7 @@ Inductive corr3_result {A B : Type} :=
   | Complete : A -> corr3_result
   | Recover : B -> corr3_result.
 
+(* TODO *)
 Notation "{<<< e1 .. e2 , 'PRE' pre 'POST' post >>>} p1 >> p2" :=
   (forall_helper (fun e1 => .. (forall_helper (fun e2 =>
    exists idemcrash,
@@ -169,7 +212,7 @@ Theorem pimpl_ok2:
   (forall done crash, pre done crash =p=> pre' done crash) ->
   {{pre}} pr.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -181,7 +224,7 @@ Theorem pimpl_ok3:
   (forall done crashdone, pre done crashdone =p=> pre' done crashdone) ->
   {{pre}} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -194,7 +237,7 @@ Theorem pimpl_ok2_cont :
   (forall done crash, pre done crash =p=> exists F, F * [[x = y]]) ->
   {{pre}} k x.
 Proof.
-  unfold corr2, pimpl; intros.
+  unfold corr2, corr2', pimpl; intros.
   edestruct H1; eauto.
   eapply sep_star_lift_l in H4; [|instantiate (1:=([x=y])%pred)].
   unfold lift in *; subst; eauto.
@@ -208,7 +251,7 @@ Theorem pimpl_ok3_cont :
   (forall done crashdone, pre done crashdone =p=> exists F, F * [[x = y]]) ->
   {{pre}} k x >> r.
 Proof.
-  unfold corr3, pimpl; intros.
+  unfold corr3, corr3', pimpl; intros.
   edestruct H1; eauto.
   eapply sep_star_lift_l in H4; [|instantiate (1:=([x=y])%pred)].
   unfold lift in *; subst; eauto.
@@ -221,7 +264,7 @@ Theorem pimpl_pre2:
   -> (forall done crash, pre done crash =p=> pre' done crash done crash)
   -> {{pre}} pr.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -233,7 +276,7 @@ Theorem pimpl_pre3:
   -> (forall done crashdone, pre done crashdone =p=> pre' done crashdone done crashdone)
   -> {{pre}} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   eapply H0.
   eauto.
@@ -244,7 +287,7 @@ Theorem pre_false2:
   (forall done crash, pre done crash =p=> [False])
   -> {{ pre }} p.
 Proof.
-  unfold corr2; intros; exfalso.
+  unfold corr2, corr2'; intros; exfalso.
   eapply H; eauto.
 Qed.
 
@@ -253,7 +296,7 @@ Theorem pre_false3:
   (forall done crashdone, pre done crashdone =p=> [False])
   -> {{ pre }} p >> r.
 Proof.
-  unfold corr3; intros; exfalso.
+  unfold corr3, corr3'; intros; exfalso.
   eapply H; eauto.
 Qed.
 
@@ -263,7 +306,7 @@ Theorem corr2_exists:
   (forall (a:T), {{ fun done crash => pre done crash a }} p)
   -> {{ fun done crash => exists a:T, pre done crash a }} p.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   destruct H0.
   eapply H; eauto.
 Qed.
@@ -273,7 +316,7 @@ Theorem corr3_exists:
   (forall (a:T), {{ fun done crashdone => pre done crashdone a }} p >> r)
   -> {{ fun done crashdone => exists a:T, pre done crashdone a }} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   destruct H0.
   eapply H; eauto.
 Qed.
@@ -282,7 +325,7 @@ Theorem corr2_forall: forall T R pre (p: prog R),
   {{ fun done crash => exists a:T, pre done crash a }} p
   -> forall (a:T), {{ fun done crash => pre done crash a }} p.
 Proof.
-  unfold corr2; intros.
+  unfold corr2, corr2'; intros.
   eapply H; eauto.
   exists a; eauto.
 Qed.
@@ -291,7 +334,7 @@ Theorem corr3_forall: forall T RF RR pre (p: prog RF) (r: prog RR),
   {{ fun done crashdone => exists a:T, pre done crashdone a }} p >> r
   -> forall (a:T), {{ fun done crashdone => pre done crashdone a }} p >> r.
 Proof.
-  unfold corr3; intros.
+  unfold corr3, corr3'; intros.
   eapply H; eauto.
   exists a; eauto.
 Qed.

src/Prog.v
@@ -20,44 +20,46 @@ Inductive prog (T : Type):=
   | Write (a: addr) (v: valu) (rx: unit -> prog T)
   | Sync (a: addr) (rx: unit -> prog T)
   | Trim (a: addr) (rx: unit -> prog T)
-  | Hash (sz: nat) (buf: word sz) (hm: hashmap) (rx: (word hashlen * (hashmap * unit)) -> prog T).
+  | Hash (sz: nat) (buf: word sz) (rx: word hashlen -> prog T).
 
 Inductive outcome (T : Type) :=
   | Failed
-  | Finished (m: rawdisk) (v: T)
-  | Crashed (m: rawdisk).
+  | Finished (m: rawdisk) (hm: hashmap) (v: T)
+  | Crashed (m: rawdisk) (hm: hashmap).
 
-Inductive step (T : Type) : rawdisk -> prog T ->
-                           rawdisk -> prog T -> Prop :=
-  | StepRead : forall m a rx v x, m a = Some (v, x) ->
-    step m (Read a rx) m (rx v)
-  | StepWrite : forall m a rx v v0 x, m a = Some (v0, x) ->
-    step m (Write a v rx) (upd m a (v, v0 :: x)) (rx tt)
-  | StepSync : forall m a rx v l, m a = Some (v, l) ->
-    step m (Sync a rx) (upd m a (v, nil)) (rx tt)
-  | StepTrim : forall m a rx vs vs', m a = Some vs ->
-    step m (Trim a rx) (upd m a vs') (rx tt)
+Inductive step (T : Type) : rawdisk -> hashmap -> prog T ->
+                           rawdisk -> hashmap -> prog T -> Prop :=
+  | StepRead : forall m a rx v x hm, m a = Some (v, x) ->
+    step m hm (Read a rx) m hm (rx v)
+  | StepWrite : forall m a rx v v0 x hm, m a = Some (v0, x) ->
+    step m hm (Write a v rx) (upd m a (v, v0 :: x)) hm (rx tt)
+  | StepSync : forall m a rx v l hm, m a = Some (v, l) ->
+    step m hm (Sync a rx) (upd m a (v, nil)) hm (rx tt)
+  | StepTrim : forall m a rx vs vs' hm, m a = Some vs ->
+    step m hm (Trim a rx) (upd m a vs') hm (rx tt)
   | StepHash : forall m sz (buf : word sz) rx h hm,
     hash_safe hm h buf ->
     hash_fwd buf = h ->
-    step m (Hash buf hm rx) m (rx (h, (upd_hashmap' hm h buf, tt))).
+    step m hm (Hash buf rx) m (upd_hashmap' hm h buf) (rx h).
 
-Inductive exec (T : Type) : rawdisk -> prog T -> outcome T -> Prop :=
-  | XStep : forall m m' p p' out, step m p m' p' ->
-    exec m' p' out ->
-    exec m p out
-  | XFail : forall m p, (~exists m' p', step m p m' p') -> (~exists r, p = Done r) ->
-    (~exists sz (buf : word sz) hm rx, p = Hash buf hm rx) ->
-    exec m p (Failed T)
-  | XCrash : forall m p, exec m p (Crashed T m)
-  | XDone : forall m v, exec m (Done v) (Finished m v).
+
+Inductive exec (T : Type) : rawdisk -> hashmap -> prog T -> outcome T -> Prop :=
+  | XStep : forall m m' hm hm' p p' out, step m hm p m' hm' p' ->
+    exec m' hm' p' out ->
+    exec m hm p out
+  | XFail : forall m hm p, (~exists m' hm' p', step m hm p m' hm' p') ->
+    (~exists r, p = Done r) ->
+    (~exists sz (buf : word sz) rx, p = Hash buf rx) ->
+    exec m hm p (Failed T)
+  | XCrash : forall m hm p, exec m hm p (Crashed T m hm)
+  | XDone : forall m hm v, exec m hm (Done v) (Finished m hm v).
 
 
 (** program with recovery *)
 Inductive recover_outcome (TF TR: Type) :=
   | RFailed
-  | RFinished (m: rawdisk) (v: TF)
-  | RRecovered (m: rawdisk) (v: TR).
+  | RFinished (m: rawdisk) (hm: hashmap) (v: TF)
+  | RRecovered (m: rawdisk) (hm: hashmap) (v: TR).
 
 Definition possible_crash (m m' : rawdisk) : Prop :=
   forall a,
@@ -65,23 +67,23 @@ Definition possible_crash (m m' : rawdisk) : Prop :=
   (exists vs v', m a = Some vs /\ m' a = Some (v', nil) /\ In v' (vsmerge vs)).
 
 Inductive exec_recover (TF TR: Type)
-    : rawdisk -> prog TF -> prog TR -> recover_outcome TF TR -> Prop :=
-  | XRFail : forall m p1 p2, exec m p1 (Failed TF)
-    -> exec_recover m p1 p2 (RFailed TF TR)
-  | XRFinished : forall m p1 p2 m' (v: TF), exec m p1 (Finished m' v)
-    -> exec_recover m p1 p2 (RFinished TR m' v)
-  | XRCrashedFailed : forall m p1 p2 m' m'r, exec m p1 (Crashed TF m')
+    : rawdisk -> hashmap -> prog TF -> prog TR -> recover_outcome TF TR -> Prop :=
+  | XRFail : forall m hm p1 p2, exec m hm p1 (Failed TF)
+    -> exec_recover m hm p1 p2 (RFailed TF TR)
+  | XRFinished : forall m hm p1 p2 m' hm' (v: TF), exec m hm p1 (Finished m' hm' v)
+    -> exec_recover m hm p1 p2 (RFinished TR m' hm' v)
+  | XRCrashedFailed : forall m hm p1 p2 m' hm' m'r, exec m hm p1 (Crashed TF m' hm')
     -> possible_crash m' m'r
-    -> @exec_recover TR TR m'r p2 p2 (RFailed TR TR)
-    -> exec_recover m p1 p2 (RFailed TF TR)
-  | XRCrashedFinished : forall m p1 p2 m' m'r m'' (v: TR), exec m p1 (Crashed TF m')
+    -> @exec_recover TR TR m'r hm' p2 p2 (RFailed TR TR)
+    -> exec_recover m hm p1 p2 (RFailed TF TR)
+  | XRCrashedFinished : forall m hm p1 p2 m' hm' m'r m'' hm'' (v: TR), exec m hm p1 (Crashed TF m' hm')
     -> possible_crash m' m'r
-    -> @exec_recover TR TR m'r p2 p2 (RFinished TR m'' v)
-    -> exec_recover m p1 p2 (RRecovered TF m'' v)
-  | XRCrashedRecovered : forall m p1 p2 m' m'r m'' (v: TR), exec m p1 (Crashed TF m')
+    -> @exec_recover TR TR m'r hm' p2 p2 (RFinished TR m'' hm'' v)
+    -> exec_recover m hm p1 p2 (RRecovered TF m'' hm'' v)
+  | XRCrashedRecovered : forall m hm p1 p2 m' hm' m'r m'' hm'' (v: TR), exec m hm p1 (Crashed TF m' hm')
     -> possible_crash m' m'r
-    -> @exec_recover TR TR m'r p2 p2 (RRecovered TR m'' v)
-    -> exec_recover m p1 p2 (RRecovered TF m'' v).
+    -> @exec_recover TR TR m'r hm' p2 p2 (RRecovered TR m'' hm'' v)
+    -> exec_recover m hm p1 p2 (RRecovered TF m'' hm'' v).
 
 Hint Constructors exec.
 Hint Constructors step.