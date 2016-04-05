faf35b6070ee2e067af3cb28808ee797679adaf2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-04-05 14:48:04 +0100

    start on ocaml extraction
    "make fstest" tries to build the ocaml code, though a number of things are broken

---
+++ src/ExtractOcaml.v
@@ -0,0 +1,20 @@
+Require Import ExtrOcamlBasic.
+Require Import FS.
+Require Import Testprog.
+
+Extraction Language Ocaml.
+
+(* Optimize away some noop-like wrappers. *)
+Extraction Inline Prog.progseq.
+Extraction Inline Prog.pair_args_helper.
+
+(* Hook up our untrusted replacement policy. *)
+Extract Inlined Constant Cache.eviction_state  => "unit".
+Extract Inlined Constant Cache.eviction_init   => "()".
+Extract Inlined Constant Cache.eviction_update => "fun state addr -> state".
+Extract Inlined Constant Cache.eviction_choose => "fun state -> (0, state)".
+Extract Constant FS.cachesize => "10000".
+
+Cd "../codegen".
+Recursive Extraction Library FS.
+Recursive Extraction Library Testprog.
