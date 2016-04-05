9ab818efea037674956576632f50da9fec610bd7
stephanie-wang <swang93@mit.edu>
2016-04-05 19:08:55 -0400

    hash_list takes in an initial hash value, which must match some list already hashed

--- src/HashmapProg.v
+++ src/HashmapProg.v
@@ -19,33 +19,33 @@ Require Import Omega.
 Set Implicit Arguments.
 
 
-Definition hash_list T values rx : prog T :=
-  hash <- Hash (combine_entry default_entry);
+Definition hash_list T h values rx : prog T :=
   let^ (hash) <- ForN i < length values
   Hashmap hm'
-  Ghost [ crash ]
+  Ghost [ l crash ]
   Loopvar [ hash ]
   Continuation lrx
   Invariant
-    [[ hash_list_rep (rev (firstn i values)) hash hm' ]]
+    [[ hash_list_rep (rev (firstn i values) ++ l) hash hm' ]]
   OnCrash crash
   Begin
     hash <- Hash (Word.combine (combine_entry (selN values i default_entry)) hash);
     lrx ^(hash)
-  Rof ^(hash);
+  Rof ^(h);
   rx hash.
 
 
-Theorem hash_list_ok : forall values,
-  {< (_ : unit) ,
+Theorem hash_list_ok : forall h values,
+  {< l,
   PRE:hm
     emp * [[ goodSize addrlen (length values) ]]
         * [[ Forall (fun e => goodSize addrlen (fst e)) values ]]
-  POST:hm' RET:hash
-    emp * [[ hash_list_rep (rev values) hash hm' ]]
+        * [[ hash_list_rep l h hm ]]
+  POST:hm' RET:h'
+    emp * [[ hash_list_rep (rev values ++ l) h' hm' ]]
   CRASH:hm'
-    emp * [[ exists i hash, hash_list_rep (rev (firstn i values)) hash hm' ]]
-  >} hash_list values.
+    emp * [[ exists i hash, hash_list_rep (rev (firstn i values) ++ l) hash hm' ]]
+  >} hash_list h values.
 Proof.
   unfold hash_list.
   step.
@@ -57,13 +57,12 @@ Proof.
     rewrite min_l; omega.
 
   step.
-  step.
 
   (* Loop invariant. *)
   - destruct (rev (firstn (m + 1) values)) eqn:Hrev_values.
     + simpl in Hlength. inversion Hlength.
     + destruct values.
-      inversion H0.
+      cbn in *. omega.
 
       assert (Hvalues: rev (p0 :: firstn m values) = selN (p0 :: values) m default_entry :: rev (firstn m (p0 :: values))).
         rewrite <- rev_unit.
@@ -76,14 +75,14 @@ Proof.
       rewrite Hvalues.
       solve_hash_list_rep.
       solve_hash_list_rep.
-      eapply Forall_forall in H4; eauto.
+      eapply Forall_forall in H5; eauto.
       apply in_selN; auto.
       auto.
       apply upd_hashmap'_eq.
       intuition.
       unfold hash_safe in *.
-      rewrite H8 in H17.
-      inversion H17 as [ Hdef | Hdef ];
+      rewrite H8 in H16.
+      inversion H16 as [ Hdef | Hdef ];
       contradict_hashmap_get_default Hdef hm0.
 
   (* Loop invariant implies post-condition. *)
@@ -91,8 +90,7 @@ Proof.
     rewrite firstn_oob in H9; try omega.
     auto.
 
-  - exists 0; eexists. econstructor. eauto.
-  - exists 0; eexists. econstructor. eauto.
+  - exists 0; eexists. simpl. solve_hash_list_rep.
 
   Grab Existential Variables.
   all: econstructor.
