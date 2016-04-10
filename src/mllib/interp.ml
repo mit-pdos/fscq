99b26a3967dbd31042188d1fbe51793ad02d0829
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-04-10 19:04:19 -0400

    ocaml extraction: switch to using zarith

--- src/mllib/interp.ml
+++ src/mllib/interp.ml
@@ -1,10 +1,18 @@
 let addrlen = Big.of_int 64
-let blocksize = Big.of_int 32768
+let blockbytes = 4096
+let blockbits = Big.of_int (blockbytes*8)
 let disk_fd = ref Unix.stderr   (* just some Unix.file_descr object *)
 let disk_in = ref stdin
 let disk_out = ref stdout
 
 let addr_to_int a = Big.to_int (Word.wordToNat addrlen a)
+
+let word_to_block (Word.W w) =
+  let s = Z.to_bits w in
+  let nbytes = String.length s in
+  String.concat "" [ s ; String.make (blockbytes-nbytes) (Char.chr 0) ]
+
+let block_to_word b = Word.W (Z.of_bits b)
 
 let init_disk fn =
   let fd = Unix.openfile fn [ Unix.O_RDWR ; Unix.O_CREAT ] 0o666 in
@@ -17,14 +25,15 @@ let close_disk =
 
 let read_disk b =
   let ic = !disk_in in
-  seek_in ic b;
-  let v = input_byte ic in
-  Word.natToWord blocksize (Big.of_int v)
+  seek_in ic (b*blockbytes);
+  let s = really_input_string ic blockbytes in
+  block_to_word s
 
 let write_disk b v =
   let oc = !disk_out in
-  seek_out oc b;
-  output_byte oc (Big.to_int (Word.wordToNat blocksize v))
+  seek_out oc (b*blockbytes);
+  let s = word_to_block v in
+  output_string oc s
 
 let sync_disk b =
   let fd = !disk_fd in
