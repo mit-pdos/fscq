2c28e1d1b23ae156848cd38f5cf946bd2cf34a4e
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-04-10 18:38:36 -0400

    ocaml mkfs at least starts running..
    but quickly hits an exception because the bigint handling isn't quite correct yet

--- src/mllib/word.ml
+++ src/mllib/word.ml
@@ -15,6 +15,12 @@ let wlt_dec sz (W a) (W b) = a < b
 let wplus sz (W a) (W b) = W (wrap sz (Big.add a b))
 let wminus sz (W a) (W b) = W (wrap sz (Big.sub a b))
 let wmult sz (W a) (W b) = W (wrap sz (Big.mult a b))
+let wdiv sz (W a) (W b) = W (Big.div a b)
+let wmod sz (W a) (W b) = W (Big.modulo a b)
+
+let wbit sz _ (W n) = W (wrap sz (Big_int.power_int_positive_big_int 2 n))
+let wor _ (W a) (W b) = W (Big_int.or_big_int a b)
+let wand _ (W a) (W b) = W (Big_int.and_big_int a b)
 
 let combine sz1 (W a) sz2 (W b) = W (Big.add a (Big_int.shift_left_big_int b (Big.to_int sz1)))
 let split1 sz1 sz2 (W a) = W (wrap sz1 a)
