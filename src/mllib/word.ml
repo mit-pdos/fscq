da3a058e9a316abe73a44361247e115294fb5c38
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-04-10 19:04:19 -0400

    ocaml extraction: switch to using zarith

--- src/mllib/word.ml
+++ src/mllib/word.ml
@@ -2,7 +2,7 @@ type word =
   W of Big.big_int
 
 let wrap sz n =
-  Big.modulo n (Big_int.power_int_positive_big_int 2 sz)
+  Big.modulo n (Big_int_Z.power_int_positive_big_int 2 sz)
 
 let natToWord sz n = W (wrap sz n)
 let wordToNat sz (W n) = n
@@ -18,11 +18,11 @@ let wmult sz (W a) (W b) = W (wrap sz (Big.mult a b))
 let wdiv sz (W a) (W b) = W (Big.div a b)
 let wmod sz (W a) (W b) = W (Big.modulo a b)
 
-let wbit sz _ (W n) = W (wrap sz (Big_int.power_int_positive_big_int 2 n))
-let wor _ (W a) (W b) = W (Big_int.or_big_int a b)
-let wand _ (W a) (W b) = W (Big_int.and_big_int a b)
+let wbit sz _ (W n) = W (wrap sz (Big_int_Z.power_int_positive_big_int 2 n))
+let wor _ (W a) (W b) = W (Big_int_Z.or_big_int a b)
+let wand _ (W a) (W b) = W (Big_int_Z.and_big_int a b)
 
-let combine sz1 (W a) sz2 (W b) = W (Big.add a (Big_int.shift_left_big_int b (Big.to_int sz1)))
+let combine sz1 (W a) sz2 (W b) = W (Big.add a (Big_int_Z.shift_left_big_int b (Big.to_int sz1)))
 let split1 sz1 sz2 (W a) = W (wrap sz1 a)
-let split2 sz1 sz2 (W a) = W (Big_int.shift_right_big_int a (Big.to_int sz1))
+let split2 sz1 sz2 (W a) = W (Big_int_Z.shift_right_big_int a (Big.to_int sz1))
 let splice _ _ _ _ _ _ = raise Not_found
