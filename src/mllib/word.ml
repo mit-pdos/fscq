39d8c37fa17d8b98e731e87a6b4cadf99fe19c41
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-04-12 22:20:34 -0400

    some slight improvements to ocaml word extraction
    mailbench 70 msgs/sec

--- src/mllib/word.ml
+++ src/mllib/word.ml
@@ -5,6 +5,8 @@ let wrap sz n =
   let isz = Z.to_int sz in
   if isz == 0 then Z.zero else Z.extract n 0 isz
 
+let two_to_n n = Z.shift_left Z.one (Z.to_int n)
+
 let natToWord sz n = W (wrap sz n)
 let wordToNat sz (W n) = n
 let wzero sz = W Big.zero
@@ -14,12 +16,16 @@ let weq sz (W a) (W b) = Big.eq a b
 let wlt_dec sz (W a) (W b) = a < b
 
 let wplus sz (W a) (W b) = W (wrap sz (Big.add a b))
-let wminus sz (W a) (W b) = W (wrap sz (Big.sub a b))
+let wminus sz (W a) (W b) = W (wrap sz (Big.sub (Big.add (two_to_n sz) a) b))
 let wmult sz (W a) (W b) = W (wrap sz (Big.mult a b))
 let wdiv sz (W a) (W b) = W (Big.div a b)
 let wmod sz (W a) (W b) = W (Big.modulo a b)
 
-let wbit sz _ (W n) = W (wrap sz (Big_int_Z.power_int_positive_big_int 2 n))
+let wbit sz _ (W n) =
+  let int_n = Z.to_int n in
+  if int_n >= (Z.to_int sz)
+    then W Z.zero
+    else W (Z.shift_left Z.one int_n)
 let wor _ (W a) (W b) = W (Big_int_Z.or_big_int a b)
 let wand _ (W a) (W b) = W (Big_int_Z.and_big_int a b)
 
