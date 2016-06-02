8f18343c5932637f412d6f7124ed4a1840930c19
Tej Chajed <tchajed@mit.edu>
2016-06-02 08:31:43 -0400

    Insert cache_abort appropriately in compiler
    Need to abort before yielding (which includes cache_fill) in the error
    cases, so that other threads see a clean pre-syscall state.

--- src/ConcurrentBridge.v
+++ src/ConcurrentBridge.v
@@ -22,14 +22,16 @@ Fixpoint compiler {T} (error_rx: prog Sigma) (p: Prog.prog T) : prog Sigma :=
   | Prog.Read a rx => opt_v <- cache_maybe_read a;
                        match opt_v with
                        | Some v => compiler error_rx (rx (valu_conv v))
-                       | None => v <- cache_fill a;
+                       | None => _ <- cache_abort;
+                                  v <- cache_fill a;
                                   error_rx
                        end
   | Prog.Write a v rx => ok <- cache_write a (valu_conv' v);
                           if ok then
                             compiler error_rx (rx tt)
                           else
-                            _ <- Yield a;
+                            _ <- cache_abort;
+                          _ <- Yield a;
                           error_rx
   | Prog.Sync a rx => _ <- cache_writeback a;
                        (* current concurrent disk model has no
