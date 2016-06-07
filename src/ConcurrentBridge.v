100bb1e212a9615585551aa5b6187f084b0b3e8e
Tej Chajed <tchajed@mit.edu>
2016-06-07 13:17:30 -0400

    Convert between structured specs
    Now stating the master compiler correctness theorem is even harder - it
    seems to require having a monadic prog (specifically, separating the
    constructors for a program from the manner of combining them).

--- src/ConcurrentBridge.v
+++ src/ConcurrentBridge.v
@@ -75,18 +75,19 @@ Abort.
 
 Notation prog_valuset := (word Prog.Valulen.valulen * list (word Prog.Valulen.valulen))%type.
 
-Inductive SeqHoareSpec A R :=
-| SeqSpec (pre: A -> @pred (word Prog.addrlen) _ (const prog_valuset))
+Inductive SeqHoareSpec R :=
+| SeqSpec A
+          (pre: A -> @pred (word Prog.addrlen) _ (const prog_valuset))
           (post: A -> R -> @pred (word Prog.addrlen) _ (const prog_valuset))
           (crash: A -> @pred (word Prog.addrlen) _ (const prog_valuset)).
 
-Definition seq_hoare_double A R (spec: SeqHoareSpec A R)
+Definition seq_hoare_double R (spec: SeqHoareSpec R)
            (p: forall T, (R -> Prog.prog T) -> Prog.prog T) :=
-  let '(SeqSpec pre post crash) := spec in
+  let 'SeqSpec pre post crash := spec in
   forall T (rx : _ -> Prog.prog T),
     Hoare.corr2
       (fun done_ crash_ =>
-         exists (a:A) F_,
+         exists a F_,
            F_ * pre a *
            [[ forall r:R, Hoare.corr2
                        (fun done'_ crash'_ =>
@@ -117,17 +118,18 @@ Section ExampleSeqSpec.
   Qed.
 End ExampleSeqSpec.
 
-Inductive ConcurHoareSpec A R :=
-| ConcurSpec (pre: TID -> A -> DISK -> memory Sigma -> abstraction Sigma -> abstraction Sigma -> Prop)
+Inductive ConcurHoareSpec R :=
+| ConcurSpec A
+             (pre: TID -> A -> DISK -> memory Sigma -> abstraction Sigma -> abstraction Sigma -> Prop)
              (post: TID -> A -> R -> DISK -> memory Sigma -> abstraction Sigma -> abstraction Sigma -> Prop).
 
-Definition concur_hoare_double A R (spec: ConcurHoareSpec A R)
+Definition concur_hoare_double R (spec: ConcurHoareSpec R)
            (p: (R -> prog Sigma) -> prog Sigma) :=
-  let '(ConcurSpec pre post) := spec in
+  let 'ConcurSpec pre post := spec in
   forall (rx: _ -> prog Sigma) (tid:TID),
     valid delta tid
           (fun done d m s_i s =>
-             exists (a:A),
+             exists a,
                pre tid a d m s_i s /\
                (forall ret_,
                    valid delta tid
@@ -161,43 +163,39 @@ Proof.
   exact (valu_conv w, nil).
 Qed.
 
-(* The idea of valid_for_corr2 is to compute a concurrent precondition
-(as part of a Hoare double) corresponding to sequential precondition,
-capturing the same spec on top of the abstraction exported by the
-cache.
+(* The idea of concurrent_spec is to compute a concurrent spec
+corresponding to sequential spec, capturing the same spec on top of
+the abstraction exported by the cache.
 
 Several things are important and currently broken with this
 definition:
 
-- the abstraction from the cache is some combination of vDisk0 and
+- The abstraction from the cache is some combination of vDisk0 and
   vdisk: vDisk0 is for between system calls and vdisk is for a given
-  system call. [project_disk] currently just uses vDisk0, which is
-  definitely wrong.
-- the precondition needs to copy over the sequential precondition but
-  also add the global invariant and probably state that we aren't in
-  the middle of a system call. This latter condition should be
-  generalized so we can prove valid_for_corr2 about the compiled
-  program by induction.
+  system call. [project_disk] currently just uses vDisk0, which seems
+  wrong.
+- The precondition needs to state that we aren't in the middle of a
+  system call. This condition should be generalized so we can prove
+  concurrent_spec about the compiled program by induction.
 *)
-Definition valid_for_corr2 T (pre: Hoare.donecond T ->
-                                   @pred (word Prog.addrlen) _ (const prog_valuset) ->
-                                   @pred (word Prog.addrlen) _ (const prog_valuset)) :
-  (donecond ->
-   DISK -> memory Sigma -> abstraction Sigma -> abstraction Sigma -> Prop) :=
-  fun done d m s0 s =>
-    pre (fun _ d => done (lift_disk d)) (fun _ => False) (project_disk s).
+Definition concurrent_spec R (spec: SeqHoareSpec R) : ConcurHoareSpec R :=
+  let 'SeqSpec pre post _ := spec in
+  ConcurSpec
+    (fun tid a d m s_i s =>
+       invariant delta d m s /\
+       pre a (project_disk s) /\
+       guar delta tid s_i s)
+    (fun tid a r d' m' s_i' s' =>
+       invariant delta d' m' s' /\
+       post a r (project_disk s') /\
+       guar delta tid s_i' s').
 
 (* The master theorem: convert a sequential program into a concurrent
 program via [compiler], convert its spec to a concurrent spec via
-[valid_for_corr2], and prove the resulting concurrent Hoare double.
+[concurrent_spec], and prove the resulting concurrent Hoare double.
 
-Note that a correct [valid_for_corr2] is crucial for starting this
-proof: one nice property is that valid_for_corr2 can separately be
-validated by proving compiler_correct for some specific example
-programs p manually.
+It's not actually clear how to even state this in a way that type
+checks using SeqHoareSpec and ConcurHoareSpec; the most natural way
+requires compiling a program that takes a continuation, which is
+impossible since that would be a function (R -> prog) -> prog.
 *)
-Theorem compiler_correct : forall T pre R p erx (rx: R -> Prog.prog T) tid,
-    Hoare.corr2 pre (p rx) ->
-    valid delta tid (@valid_for_corr2 T pre) (compiler erx (p rx)).
-Proof.
-Abort.
