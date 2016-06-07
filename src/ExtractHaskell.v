abdb50d74a63ed4f4461b5d98482b9cdf80c58c3
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-06-07 16:33:15 -0400

    fix one build error (though others remain)

--- src/ExtractHaskell.v
+++ src/ExtractHaskell.v
@@ -11,10 +11,10 @@ Extraction Inline Prog.pair_args_helper.
 (* Extract Inlined Constant Prog.progseq => "Profile.progseq __FILE__ __LINE__". *)
 
 (* Hook up our untrusted replacement policy. *)
-Extract Inlined Constant ReadCache.eviction_state  => "Evict.EvictionState".
-Extract Inlined Constant ReadCache.eviction_init   => "Evict.eviction_init".
-Extract Inlined Constant ReadCache.eviction_update => "Evict.eviction_update".
-Extract Inlined Constant ReadCache.eviction_choose => "Evict.eviction_choose".
+Extract Inlined Constant UniCache.eviction_state  => "Evict.EvictionState".
+Extract Inlined Constant UniCache.eviction_init   => "Evict.eviction_init".
+Extract Inlined Constant UniCache.eviction_update => "Evict.eviction_update".
+Extract Inlined Constant UniCache.eviction_choose => "Evict.eviction_choose".
 Extract Constant AsyncFS.AFS.cachesize => "10000".
 
 Cd "../codegen".
