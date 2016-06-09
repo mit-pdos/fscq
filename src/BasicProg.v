689d7225013824aa13a539e3dfd55071051735f1
Tej Chajed <tchajed@mit.edu>
2016-06-09 10:13:14 -0400

    Prove monad laws

--- src/BasicProg.v
+++ src/BasicProg.v
@@ -53,15 +53,23 @@ Ltac inv_crash_step :=
     inversion H; subst; clear H
   end.
 
-Ltac inv_exec :=
-  match goal with
-  | [ H: exec _ _ _ _ |- _ ] =>
+Ltac inv_exec' H :=
     inversion H;
     repeat inj_existT; subst;
     try inv_step;
     try inv_fail_step;
     try inv_crash_step;
-    clear H
+    subst;
+    clear H.
+
+Ltac inv_exec :=
+  lazymatch goal with
+  | [ H: exec _ _ (Ret _) _ |- _ ] =>
+    inv_exec' H
+  | [ H: exec _ _ (Bind _ _) _ |- _ ] =>
+    inv_exec' H
+  | [ H: exec _ _ _ _ |- _ ] =>
+    inv_exec' H
   end.
 
 Lemma sync_invariant_possible_sync : forall (F: rawpred) (m: rawdisk),
@@ -189,24 +197,72 @@ Qed.
 
 Hint Extern 1 ({{_}} Bind (Hash _) _) => apply hash_ok : prog.
 
-Theorem ret_ok :
-  forall T (v:T),
-    {< (_:unit),
-     PRE:hm    emp
-     POST: hm' RET:r emp * [[ r = v ]] * [[ hm' = hm ]]
-     CRASH: hm' emp * [[ hm' = hm ]]
-    >} Ret v.
-Proof.
-  unfold corr2; intros.
-  destruct_lift H.
-  inv_exec.
-  inv_exec.
-  eapply H4; eauto.
-  pred_apply; cancel.
-  eauto.
-Qed.
+(** program equivalence and monad laws *)
 
-Hint Extern 1 ({{_}} Bind (Ret _) _) => apply ret_ok : prog.
+Section MonadLaws.
+
+  Definition prog_equiv T : prog T -> prog T -> Prop :=
+    fun p1 p2 => forall m hm out,
+        exec m hm p1 out <-> exec m hm p2 out.
+
+  Arguments prog_equiv {T} _ _.
+
+  Infix "~=" := prog_equiv (at level 50, left associativity).
+
+  Theorem bind_left_id : forall T T' v (p: T -> prog T'),
+      Bind (Ret v) p ~= p v.
+  Proof.
+    split; intros.
+    - inv_exec.
+      inv_exec; eauto.
+    - eauto.
+  Qed.
+
+  Theorem bind_right_id : forall T (p: prog T),
+      Bind p Ret ~= p.
+  Proof.
+    split; intros.
+    - inv_exec; eauto.
+      inv_exec; eauto.
+    - destruct out; eauto.
+  Qed.
+
+  Theorem bind_assoc : forall T T' T'' (p1: prog T) (p2: T -> prog T') (p3: T' -> prog T''),
+      Bind (Bind p1 p2) p3 ~= Bind p1 (fun x => Bind (p2 x) p3).
+  Proof.
+    split; intros.
+    - inv_exec; eauto;
+        inv_exec; eauto.
+    - inv_exec; eauto.
+      inv_exec; eauto.
+  Qed.
+
+  Theorem corr2_equivalence : forall T (p p': prog T) pre,
+      corr2 pre p' ->
+      prog_equiv p p' ->
+      corr2 pre p.
+  Proof.
+    unfold corr2; intros.
+    match goal with
+    | [ H: _ ~= _ |- _ ] =>
+      edestruct H; eauto
+    end.
+  Qed.
+
+End MonadLaws.
+
+Ltac monad_simpl :=
+  repeat match goal with
+         | [ |- {{_}} Bind (Ret _) _ ] =>
+           eapply corr2_equivalence;
+           [ | apply bind_left_id ]
+         | [ |- {{_}} Bind _ Ret ] =>
+           eapply corr2_equivalence;
+           [ | apply bind_right_id ]
+         | [ |- {{_}} Bind (Bind _ _) _ ] =>
+           eapply corr2_equivalence;
+           [ | apply bind_assoc ]
+         end.
 
 Definition If_ T P Q (b : {P} + {Q}) (p1 p2 : prog T) :=
   if b then p1 else p2.
