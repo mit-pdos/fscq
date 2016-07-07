172b45a47c9e0acb916855db612a730cb6cdb76b
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-07-07 17:09:49 -0400

    delete old directory separation logic plan

--- src/DirSep.v
+++ src/DirSep.v
@@ -13,51 +13,12 @@ Import ListNotations.
 
 Set Implicit Arguments.
 
-Fixpoint dirents2mem (ents : list (string * dirtree)) : @mem _ string_dec _ :=
-  match ents with
-  | nil => empty_mem
-  | (name, subtree) :: rest =>
-    Mem.insert (dirents2mem rest) name subtree
-  end.
-
-Definition dir2mem (t : dirtree) :=
-  dirents2mem (dirtree_dirents t).
-
-
-Section FLAT.
-
-  Variable AT : Type.
-  Variable AEQ : forall (a b : AT), {a=b} + {a<>b}.
-  Variable V : Type.
-  Variable mk_mem : string -> dirtree -> @mem AT AEQ V.
-  Fixpoint dirlist_mem (dirlist : list (string * dirtree)) :=
-    match dirlist with
-    | nil => empty_mem
-    | (name, e) :: dirlist' => mem_union (mk_mem name e) (dirlist_mem dirlist')
-    end.
-
-End FLAT.
-
-
-Fixpoint dir2flatmem (prefix : list string) (d : dirtree) : @mem _ (list_eq_dec string_dec) _ :=
-  match d with
-  | TreeFile inum f => Mem.insert empty_mem prefix (inum, f)
-  | TreeDir _ ents => dirlist_mem (fun name subtree => dir2flatmem (prefix ++ [name]) subtree) ents
-  end.
-
 Definition dir2flatmem2 (d : dirtree) : @mem _ (list_eq_dec string_dec) _ :=
   fun pn => match DIRTREE.find_subtree pn d with
   | Some (TreeFile inum f) => Some (Some (inum, f))
   | Some (TreeDir _ _) => Some None
   | None => Some None
   end.
-
-Lemma dir2flatmem_find_subtree : forall fnlist tree inum f,
-  tree_names_distinct tree ->
-  dir2flatmem [] tree fnlist = Some (inum, f) ->
-  find_subtree fnlist tree = Some (TreeFile inum f).
-Proof.
-Admitted.
 
 Lemma dir2flatmem2_find_subtree : forall fnlist tree inum f,
   tree_names_distinct tree ->
@@ -75,16 +36,6 @@ Qed.
 (** This should be useful for satisfying preconditions of most AsyncFS functions
  ** that take as an argument an inode number of an existing file in a tree.
  *)
-Lemma dir2flatmem_find_subtree_ptsto : forall fnlist tree inum f F,
-  tree_names_distinct tree ->
-  (F * fnlist |-> (inum, f))%pred (dir2flatmem [] tree) ->
-  find_subtree fnlist tree = Some (TreeFile inum f).
-Proof.
-  intros.
-  eapply ptsto_valid' in H0.
-  eapply dir2flatmem_find_subtree; eauto.
-Qed.
-
 Lemma dir2flatmem2_find_subtree_ptsto : forall fnlist tree inum f F,
   tree_names_distinct tree ->
   (F * fnlist |-> Some (inum, f))%pred (dir2flatmem2 tree) ->
@@ -93,15 +44,6 @@ Proof.
   intros.
   eapply ptsto_valid' in H0.
   eapply dir2flatmem2_find_subtree; eauto.
-Qed.
-
-Lemma dir2flatmem_find_name : forall fnlist tree inum f,
-  tree_names_distinct tree ->
-  dir2flatmem [] tree fnlist = Some (inum, f) ->
-  find_name fnlist tree = Some (inum, false).
-Proof.
-  unfold find_name; intros.
-  erewrite dir2flatmem_find_subtree; eauto.
 Qed.
 
 Lemma dir2flatmem2_find_name : forall fnlist tree inum f,
@@ -115,16 +57,6 @@ Qed.
 
 (** This should be useful for satisfying the precondition of [lookup_ok].
  *)
-Lemma dir2flatmem_find_name_ptsto : forall fnlist tree inum f F,
-  tree_names_distinct tree ->
-  (F * fnlist |-> (inum, f))%pred (dir2flatmem [] tree) ->
-  find_name fnlist tree = Some (inum, false).
-Proof.
-  intros.
-  eapply ptsto_valid' in H0.
-  eapply dir2flatmem_find_name; eauto.
-Qed.
-
 Lemma dir2flatmem2_find_name_ptsto : forall fnlist tree inum f F,
   tree_names_distinct tree ->
   (F * fnlist |-> Some (inum, f))%pred (dir2flatmem2 tree) ->
@@ -134,14 +66,6 @@ Proof.
   eapply ptsto_valid' in H0.
   eapply dir2flatmem2_find_name; eauto.
 Qed.
-
-Lemma dir2flatmem_update_subtree_upd : forall fnlist tree inum f f',
-  tree_names_distinct tree ->
-  find_subtree fnlist tree = Some (TreeFile inum f) ->
-  dir2flatmem [] (update_subtree fnlist (TreeFile inum f') tree) =
-  upd (dir2flatmem [] tree) fnlist (inum, f').
-Proof.
-Admitted.
 
 Lemma dir2flatmem2_update_subtree_upd : forall fnlist tree inum f f',
   tree_names_distinct tree ->
@@ -164,17 +88,6 @@ Qed.
  ** mutation functions (and, indirectly through various lemmas, in the
  ** postcondition of [update_fblock_d_ok] as well).
  *)
-Lemma dir2flatmem_update_subtree : forall fnlist tree inum f f' F,
-  tree_names_distinct tree ->
-  (F * fnlist |-> (inum, f))%pred  (dir2flatmem [] tree) ->
-  (F * fnlist |-> (inum, f'))%pred (dir2flatmem [] (update_subtree fnlist (TreeFile inum f') tree)).
-Proof.
-  intros.
-  erewrite dir2flatmem_update_subtree_upd; eauto.
-  eapply ptsto_upd'; eauto.
-  eapply dir2flatmem_find_subtree_ptsto; eauto.
-Qed.
-
 Lemma dir2flatmem2_update_subtree : forall fnlist tree inum f f' F,
   tree_names_distinct tree ->
   (F * fnlist |-> Some (inum, f))%pred  (dir2flatmem2 tree) ->
@@ -187,25 +100,6 @@ Proof.
 Qed.
 
 
-Theorem dirents2mem_not_in_none : forall name l,
-  (~ In name (map fst l)) ->
-  dirents2mem l name = None.
-Proof.
-  induction l; simpl; intros.
-  - firstorder.
-  - destruct a.
-    rewrite insert_ne; eauto.
-Qed.
-
-
-Lemma dir2flatmem_graft_upd : forall tree inum inum' f f' basenum basedents base name,
-  tree_names_distinct tree ->
-  find_subtree (base++[name]) tree = Some (TreeFile inum f) ->
-  dir2flatmem [] (tree_graft basenum basedents base name (TreeFile inum' f') tree) =
-  upd (dir2flatmem [] tree) (base++[name]) (inum', f').
-Proof.
-Admitted.
-
 Lemma dir2flatmem2_graft_upd : forall tree inum inum' f f' basenum basedents base name,
   tree_names_distinct tree ->
   find_subtree (base++[name]) tree = Some (TreeFile inum f) ->
@@ -215,18 +109,6 @@ Proof.
 Admitted.
 
 (* if name exists then the graft is the same as an update but with new inum too *)
-Theorem dirents2mem_graft_file' : forall (F: @pred (list string) (@list_eq_dec string string_dec) (addr * BFILE.bfile))
-      tree name inum f inum' f' basenum basedents base,
-  tree_names_distinct tree ->
-  (F * (base ++ [name]) |-> (inum', f'))%pred (dir2flatmem [] tree) -> 
-  (F * (base ++ [name]) |-> (inum, f))%pred (dir2flatmem [] (tree_graft basenum basedents base name (TreeFile inum f) tree)).
-Proof.
-  intros.
-  erewrite dir2flatmem_graft_upd; eauto.
-  eapply ptsto_upd'; eauto.
-  eapply dir2flatmem_find_subtree_ptsto; eauto.
-Qed.
-
 Theorem dirents2mem2_graft_file' : forall (F: @pred _ (@list_eq_dec string string_dec) _)
       tree name inum f inum' f' basenum basedents base,
   tree_names_distinct tree ->
@@ -239,14 +121,6 @@ Proof.
   eapply dir2flatmem2_find_subtree_ptsto; eauto.
 Qed.
 
-Lemma dir2flatmem_prune_delete : forall tree inum f basenum basedents base name,
-  tree_names_distinct tree ->
-  find_subtree (base++[name]) tree = Some (TreeFile inum f) ->
-  dir2flatmem [] (tree_prune basenum basedents base name tree) =
-  mem_except (dir2flatmem [] tree) (base++[name]).
-Proof.
-Admitted.
-
 Lemma dir2flatmem2_prune_delete : forall tree inum f basenum basedents base name,
   tree_names_distinct tree ->
   find_subtree (base++[name]) tree = Some (TreeFile inum f) ->
@@ -254,20 +128,6 @@ Lemma dir2flatmem2_prune_delete : forall tree inum f basenum basedents base name
   upd (dir2flatmem2 tree) (base++[name]) None.
 Proof.
 Admitted.
-
-Theorem dirents2mem_prune_file : forall (F: @pred (list string) (@list_eq_dec string string_dec) (addr * BFILE.bfile))
-      tree name inum f basenum basedents base,
-  tree_names_distinct tree ->
-  (F * (base ++ [name]) |-> (inum, f))%pred (dir2flatmem [] tree) ->
-  F (dir2flatmem [] (tree_prune basenum basedents base name tree)).
-Proof.
-  intros.
-  erewrite dir2flatmem_prune_delete; eauto.
-  eapply ptsto_mem_except; eauto.
-  pred_apply.
-  cancel.
-  eapply dir2flatmem_find_subtree_ptsto; eauto.
-Qed.
 
 Theorem dirents2mem2_prune_file : forall (F: @pred _ (@list_eq_dec string string_dec) _)
       tree name inum f basenum basedents base,
@@ -281,14 +141,6 @@ Proof.
   eapply dir2flatmem2_find_subtree_ptsto; eauto.
 Qed.
 
-Lemma dir2flatmem_update_delete : forall tree basenum basedents base name,
-  tree_names_distinct tree ->
-  find_subtree base tree = Some (TreeDir basenum basedents) ->
-  dir2flatmem [] (update_subtree base (TreeDir basenum (delete_from_list name basedents)) tree) =
-  mem_except (dir2flatmem [] tree) (base++[name]).
-Proof.
-Admitted.
-
 (* XXX require that [name] refer to a TreeFile, as opposed to a TreeDir *)
 Lemma dir2flatmem2_update_delete : forall tree basenum basedents base name,
   tree_names_distinct tree ->
@@ -297,20 +149,6 @@ Lemma dir2flatmem2_update_delete : forall tree basenum basedents base name,
   upd (dir2flatmem2 tree) (base++[name]) None.
 Proof.
 Admitted.
-
-Lemma dir2flatmem_delete_file: forall (F: @pred (list string) (@list_eq_dec string string_dec) (addr * BFILE.bfile))
-     tree name inum f basenum basedents base,
-  tree_names_distinct tree ->
-  DIRTREE.find_subtree base tree = Some (DIRTREE.TreeDir basenum basedents) ->
-  (F * (base++[name])%list |-> (inum, f))%pred (dir2flatmem [] tree) ->
-  F (dir2flatmem [] (update_subtree base (TreeDir basenum (delete_from_list name basedents)) tree)).
-Proof.
-  intros.
-  erewrite dir2flatmem_update_delete; eauto.
-  eapply ptsto_mem_except; eauto.
-  pred_apply.
-  cancel.
-Qed.
 
 Lemma dir2flatmem2_delete_file: forall (F: @pred _ (@list_eq_dec string string_dec) _)
      tree name inum f basenum basedents base,
@@ -325,4 +163,4 @@ Proof.
 Qed.
 
 
-Global Opaque dir2flatmem.
+Global Opaque dir2flatmem2.
