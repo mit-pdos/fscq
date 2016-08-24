033cf922f3e34ba110a5802c0626b957c3f63223
Tej Chajed <tchajed@mit.edu>
2016-08-24 10:00:41 -0400

    More refined thoughts on covalid

--- src/CoValid.v
+++ src/CoValid.v
@@ -7,6 +7,10 @@ Inductive TookStep Sigma T : prog Sigma T -> prog Sigma T -> Prop :=
     TookStep p' p'' ->
     TookStep p p''.
 
+Transparent valid.
+
+Notation "{{{ p ; p' }}}" := (valid _ _ _ (Bind p p')).
+
 Theorem covalid : forall Sigma (delta: Protocol Sigma) tid T (p: prog Sigma T)
                     (pre: donecond T -> DISK -> memory _ -> abstraction _ -> abstraction _ -> Prop)
                     p' pre',
@@ -19,17 +23,21 @@ Theorem covalid : forall Sigma (delta: Protocol Sigma) tid T (p: prog Sigma T)
 Proof.
   induction 1; intros.
 
-  - apply H.
-    eapply pimpl_ok.
-    apply H0.
-    intros.
-    split; eauto.
-    (* back where we started *)
+  - unfold valid; intros.
+    remember (x <- p1; p x).
+    induction H2; try congruence;
+      subst;
+      try solve [ inversion H2 ].
+    (* how do we deal with p0 = ... obligations in the inductive hypotheses?
+    those make it impossible to apply them *)
     admit.
-
-  - (* induction on TookStep can't be right, it just produces more TookStep
-    programs, and this wouldn't even be fixed by changing TookStep_Trans to
-    require a right-assocative Bind structure *)
+    inversion Heqp0.
+    pose proof (EqdepFacts.eq_sigT_fst H5); subst.
+    repeat sigT_eq; cleanup.
+    admit.
+  - (* perhaps induction on TookStep should occur within the induction on exec?
+    or maybe we need an induction on exec that allows skipping the binding
+    structure captured in TookStep? *)
     admit.
 Abort.
 
