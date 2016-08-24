2a721aadf41edaaa6fee697f9bf0ed8171d62b2a
Tej Chajed <tchajed@mit.edu>
2016-08-24 17:05:38 -0400

    TookStep need only talk about a single Bind
    Can rewrite the program to an equivalent one with a bind in the right
    place, as long as the corecursive call is in the next step (which it
    must be for the cofix to be guarded).

--- src/CoValid.v
+++ src/CoValid.v
@@ -1,14 +1,12 @@
 Require Import CoopConcur.
 
 Inductive TookStep Sigma T : prog Sigma T -> prog Sigma T -> Prop :=
-| TookStep_Bind : forall T' (p1: prog Sigma T') p v, TookStep (Bind p1 p) (p v)
-| TookStep_Trans : forall p p' p'',
-    TookStep p p' ->
-    TookStep p' p'' ->
-    TookStep p p''.
+| TookStep_Bind : forall T' (p1: prog Sigma T') p v, TookStep (Bind p1 p) (p v).
 
 Transparent valid.
 
+(* for debugging purposes, override {{p; _}} notation to one that shows the
+entire program *)
 Notation "{{{ p ; p' }}}" := (valid _ _ _ (Bind p p')).
 
 Theorem covalid : forall Sigma (delta: Protocol Sigma) tid T (p: prog Sigma T)
@@ -21,24 +19,23 @@ Theorem covalid : forall Sigma (delta: Protocol Sigma) tid T (p: prog Sigma T)
                    valid delta tid pre p) p' ->
     valid delta tid pre p.
 Proof.
-  induction 1; intros.
+  inversion 1; subst.
+  rename p0 into p.
 
-  - unfold valid; intros.
-    remember (x <- p1; p x).
-    induction H2; try congruence;
-      subst;
-      try solve [ inversion H2 ].
-    (* how do we deal with p0 = ... obligations in the inductive hypotheses?
+  unfold valid; intros.
+  remember (x <- p1; p x).
+  induction H3; try congruence;
+    subst;
+    try solve [ inversion H3 ].
+  (* how do we deal with p0 = ... obligations in the inductive hypotheses?
     those make it impossible to apply them *)
-    admit.
-    inversion Heqp0.
-    pose proof (EqdepFacts.eq_sigT_fst H5); subst.
-    repeat sigT_eq; cleanup.
-    admit.
-  - (* perhaps induction on TookStep should occur within the induction on exec?
-    or maybe we need an induction on exec that allows skipping the binding
-    structure captured in TookStep? *)
-    admit.
+  admit.
+  inversion Heqp0.
+  match goal with
+  | [ H: existT _ _ _ = existT _ _ _ |- _ ] =>
+    pose proof (EqdepFacts.eq_sigT_fst H); subst
+  end; repeat sigT_eq; cleanup.
+  admit.
 Abort.
 
 (* Local Variables: *)
