64de66c777c45f962f7bfa3689d53124dc0ea756
Alex Konradi <alexkonradi@gmail.com>
2016-09-16 17:22:59 -0400

    Prove some more map facts

--- src/MoreMapFacts.v
+++ src/MoreMapFacts.v
@@ -8,6 +8,12 @@ Module MoreFacts_fun (E:DecidableType)(Import M:WSfun E).
   Module Import MapFacts := FMapFacts.WFacts_fun(E)(M).
   Import M.
 
+  Ltac smash_equal :=
+    unfold Equal; intros;
+    repeat rewrite ?remove_o, ?add_o;
+    repeat destruct eq_dec; auto;
+    try solve [exfalso; eauto].
+
   Lemma Forall_elements_add : forall V P k (v : V) m,
                                 Forall P (elements (add k v m)) <->
                                 P (k, v) /\ Forall P (elements (remove k m)).
@@ -22,33 +28,45 @@ Module MoreFacts_fun (E:DecidableType)(Import M:WSfun E).
   Hint Resolve Forall_elements_equal : mapfacts.
 
   Lemma add_remove_comm : forall V k1 k2 (v : V) m,
-                            k1 <> k2 ->
+                            ~E.eq k1 k2 ->
                             Equal (add k2 v (remove k1 m)) (remove k1 (add k2 v m)).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma add_remove_comm' : forall V k1 k2 (v : V) m,
-                             k1 <> k2 ->
+                             ~E.eq k1 k2 ->
                              Equal (remove k1 (add k2 v m)) (add k2 v (remove k1 m)).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma add_remove_same : forall V k (v : V) m,
                             Equal (remove k (add k v m)) (remove k m).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma add_add_comm : forall V k1 k2 v1 v2 (m : t V),
-                         k1 <> k2 ->
+                         ~E.eq k1 k2 ->
                          Equal (add k2 v2 (add k1 v1 m))
                                          (add k1 v1 (add k2 v2 m)).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma add_same : forall V k (v v0 : V) m,
                             Equal (add k v (add k v0 m)) (add k v m).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma remove_remove_comm : forall V k1 k2 (m : t V),
-                               k1 <> k2 ->
+                               ~E.eq k1 k2 ->
                                Equal (remove k2 (remove k1 m)) (remove k1 (remove k2 m)).
-  Admitted.
+  Proof.
+    smash_equal.
+  Qed.
 
   Lemma Forall_elements_remove_weaken : forall V P k (m : t V),
                                           Forall P (elements m) ->
