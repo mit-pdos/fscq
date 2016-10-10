99939a1c4e829b8a6691eb9c7906ed4da5e781d9
Alex Konradi <alexkonradi@gmail.com>
2016-10-10 15:37:06 -0400

    Prove sep_star_comm and sep_star_assoc_1 in GoSep
    Add lots of lemmas about properties of disjoint maps and use those to
    prove the theorems. The ehlper lemmas should be moved elsewhere, but
    that's something that can be done later.

--- src/GoSep.v
+++ src/GoSep.v
@@ -40,6 +40,7 @@ Definition ptsto var val : pred := Some (VarMap.add var val (VarMap.empty _)).
 
 Module VarMapProperties := FMapFacts.WProperties_fun(Nat_as_OT)(VarMap).
 
+(* TODO move this and its associated theorems somewhere else *)
 Definition maps_disjoint T (m1 m2 : VarMap.t T) := 
   VarMapProperties.for_all (fun k1 _ => negb (VarMap.mem k1 m2)) m1.
 
@@ -53,7 +54,7 @@ Proof.
   firstorder using VarMapProperties.for_all_iff.
 Qed.
 
-Lemma not_for_all : forall T (m : VarMap.t T) f,
+Lemma not_for_all_if : forall T (m : VarMap.t T) f,
   VarMapProperties.for_all f m = false ->
   exists k v, VarMap.MapsTo k v m /\ f k v = false.
 Proof.
@@ -79,13 +80,34 @@ Proof.
   repeat eexists; intuition.
 Qed.
 
+Lemma not_for_all_rev : forall T (t : VarMap.t T) P,
+  (exists k v, VarMap.MapsTo k v t /\ P k v = false) ->
+  VarMapProperties.for_all P t = false.
+Proof.
+  intros.
+  repeat deex.
+  apply Bool.not_true_is_false.
+  intro.
+  rewrite VarMapProperties.for_all_iff in * by congruence.
+  rewrite H in H1 by auto. inversion H1.
+Qed.
+
+Lemma not_for_all_iff : forall T (t : VarMap.t T) P,
+  (exists k v, VarMap.MapsTo k v t /\ P k v = false) <->
+  VarMapProperties.for_all P t = false.
+Proof.
+  split.
+  apply not_for_all_rev.
+  apply not_for_all_if.
+Qed.
+
 Lemma for_all_mem_disagree : forall T (m1 m2 : VarMap.t T),
   VarMapProperties.for_all (fun k _ => negb (VarMap.mem k m1)) m2 = false ->
   VarMapProperties.for_all (fun k _ => negb (VarMap.mem k m2)) m1 = true ->
   False.
 Proof.
   intros.
-  apply not_for_all in H.
+  apply not_for_all_iff in H.
   destruct H as [k [v [H H']]].
   rewrite VarMapProperties.for_all_iff in H0; try congruence.
   rewrite Bool.negb_false_iff in *.
@@ -115,6 +137,64 @@ Proof.
     | [ |- context[VarMapProperties.for_all ?p m2] ] => case_eq (VarMapProperties.for_all p m2)
   end; try congruence;
     intros; exfalso; eapply for_all_mem_disagree; eauto.
+Qed.
+
+Lemma mapsto_in : forall T k v (t : VarMap.t T),
+  VarMap.MapsTo k v t -> VarMap.In k t.
+Proof.
+  intros.
+  apply VarMapProperties.F.in_find_iff.
+  find_eapply_lem_hyp VarMap.find_1.
+  intro. rewrite H in *. inversion H0.
+Qed.
+
+Hint Resolve mapsto_in.
+
+Lemma maps_disjoint_in' : forall T k (t0 t1 : VarMap.t T),
+  maps_disjoint t0 t1 = true ->
+  VarMap.In k t0 -> ~VarMap.In k t1.
+Proof.
+  unfold maps_disjoint.
+  intros.
+  find_eapply_lem_hyp VarMapProperties.F.in_find_iff.
+  destruct VarMap.find eqn:H'.
+  find_eapply_lem_hyp for_all_if; try congruence.
+  rewrite Bool.negb_true_iff in *.
+  apply VarMapProperties.F.not_mem_in_iff. eauto.
+  apply VarMap.find_2. eauto.
+  contradiction H0; auto.
+Qed.
+
+Lemma maps_disjoint_in : forall T k (t0 t1 : VarMap.t T),
+  maps_disjoint t0 t1 = true ->
+  VarMap.In k t0 -> VarMap.In k t1 -> False.
+Proof.
+  intros.
+  find_eapply_lem_hyp maps_disjoint_in'; eauto.
+  rewrite maps_disjoint_comm. auto.
+Qed.
+
+Hint Resolve maps_disjoint_in.
+
+Lemma maps_disjoint_assoc : forall T (t0 t1 t2 : VarMap.t T),
+  maps_disjoint t0 (VarMapProperties.update t1 t2) = true ->
+  maps_disjoint t1 t2 = true ->
+  maps_disjoint t2 (VarMapProperties.update t1 t0) = true.
+Proof.
+  intros.
+  rewrite maps_disjoint_comm.
+  pose proof H.
+  unfold maps_disjoint in H. unfold maps_disjoint.
+  rewrite VarMapProperties.for_all_iff in *; try congruence.
+  intros. apply Bool.negb_true_iff.
+  setoid_rewrite Bool.negb_true_iff in H.
+  find_eapply_lem_hyp VarMapProperties.update_mapsto_iff.
+  apply VarMapProperties.F.not_mem_in_iff.
+  intuition; eauto.
+  find_eapply_lem_hyp H.
+  find_eapply_lem_hyp VarMapProperties.F.not_mem_in_iff; auto.
+  rewrite VarMapProperties.update_in_iff in *.
+  intuition.
 Qed.
 
 Definition sep_star_impl (p1 p2 : pred) : pred :=
@@ -167,17 +247,89 @@ Proof.
   rewrite maps_disjoint_comm.
   case_eq (maps_disjoint t t0); intro He; rewrite ?He in *; auto; intros.
   destruct item.
-  eapply VarMap.find_1.
+  specialize (H k (SItem v)). simpl in *.
   find_eapply_lem_hyp VarMap.find_2.
-  pose proof (VarMapProperties.update_mapsto_iff t t0 k (SItem v)).
-  pose proof (VarMapProperties.update_mapsto_iff t0 t k (SItem v)).
-Admitted.
+  destruct (VarMapProperties.update_mapsto_iff t t0 k (SItem v));
+  destruct (VarMapProperties.update_mapsto_iff t0 t k (SItem v)).
+  intuition; apply H, VarMap.find_1; intuition; eauto.
+Qed.
+
+Theorem maps_disjoint_update_eq' : forall T (t0 t1 : VarMap.t T),
+  maps_disjoint t0 t1 = true ->
+  VarMap.Equal (VarMapProperties.update t0 t1) (VarMapProperties.update t1 t0).
+Proof.
+  intros.
+  unfold VarMap.Equal; intros.
+  destruct VarMap.find eqn:H'; symmetry.
+  rewrite <- VarMapProperties.F.find_mapsto_iff in *.
+  rewrite VarMapProperties.update_mapsto_iff in *.
+  intuition. right; eauto.
+  rewrite <- VarMapProperties.F.not_find_in_iff in *.
+  intuition.
+  rewrite VarMapProperties.update_in_iff in *.
+  firstorder.
+Qed.
+
+Theorem maps_disjoint_update_eq : forall T (t t0 t1 : VarMap.t T),
+  maps_disjoint t (VarMapProperties.update t0 t1) =
+  maps_disjoint t (VarMapProperties.update t1 t0).
+Proof.
+  intros.
+  destruct maps_disjoint eqn:H; symmetry; unfold maps_disjoint in *.
+  rewrite VarMapProperties.for_all_iff in * by congruence.
+  intros.
+  find_eapply_lem_hyp H.
+  rewrite Bool.negb_true_iff in *.
+  rewrite <- VarMapProperties.F.not_mem_in_iff in *.
+  rewrite VarMapProperties.update_in_iff in *; intuition.
+  rewrite <- not_for_all_iff in *.
+  repeat deex; repeat eexists;
+    rewrite ?Bool.negb_false_iff in *; eauto.
+  rewrite <- VarMapProperties.F.mem_in_iff in *.
+  rewrite VarMapProperties.update_in_iff in *; intuition.
+Qed.
+
+Theorem maps_not_disjoint : forall T (t0 t1 : VarMap.t T),
+  maps_disjoint t0 t1 = false ->
+  exists k, VarMap.In k t0 /\ VarMap.In k t1.
+Proof.
+  unfold maps_disjoint.
+  intros.
+  find_eapply_lem_hyp not_for_all_iff.
+  repeat deex.
+  rewrite Bool.negb_false_iff in *.
+  eexists; split; eauto.
+  apply VarMapProperties.F.mem_in_iff; auto.
+Qed.
 
 Theorem sep_star_assoc_1 :
   forall p1 p2 p3,
     p1 * p2 * p3 =p=> p1 * (p2 * p3).
 Proof.
-Admitted.
+  unfold pimpl, pred_matches.
+  unfold sep_star.
+  intros.
+  destruct p1, p2, p3;
+    rewrite sep_star_is in *; simpl in *;
+    repeat (match goal with
+    | [H : context [if ?X then _ else _] |- _ ] => let H := fresh in destruct X eqn:H
+    | [|- context [if ?X then _ else _]] => let H := fresh in destruct X eqn:H
+    end; simpl in *); intros; auto; intuition.
+
+  intros.
+  apply H.
+  rewrite <- VarMapProperties.F.find_mapsto_iff in *.
+  repeat rewrite VarMapProperties.update_mapsto_iff in *.
+  rewrite VarMapProperties.update_in_iff in *.
+  intuition.
+  apply Bool.not_true_iff_false in H3; contradiction H3.
+  rewrite maps_disjoint_comm in H1.
+  eapply maps_disjoint_assoc; [rewrite maps_disjoint_update_eq |]; auto.
+  rewrite maps_disjoint_comm; auto.
+  find_eapply_lem_hyp maps_not_disjoint. deex.
+  eapply maps_disjoint_in; eauto.
+  apply VarMapProperties.update_in_iff; auto.
+Qed.
 
 Theorem sep_star_assoc_2 :
   forall p1 p2 p3,
