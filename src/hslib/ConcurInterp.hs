bfcfc0111073fefeccad6f20141309b1a7281992
Tej Chajed <tchajed@mit.edu>
2016-12-07 11:48:18 -0500

    Fix compilation of concurrent interpreter

--- src/hslib/ConcurInterp.hs
+++ src/hslib/ConcurInterp.hs
@@ -2,9 +2,15 @@ module ConcurInterp where
 
 import CoopConcur
 import Hlist
+import Variables
 import qualified Disk
+import Word
 import Control.Exception as E
 import Control.Concurrent.MVar
+import Data.Map
+import GHC.Prim
+import Data.IORef
+import qualified Crypto.Hash.SHA256 as SHA256
 
 verbose :: Bool
 verbose = True
@@ -20,61 +26,90 @@ hmember_to_int :: Hlist.Coq_member a -> Int
 hmember_to_int (HFirst _) = 0
 hmember_to_int (HNext _ _ x) = 1 + (hmember_to_int x)
 
-run_dcode :: Disk.DiskState -> Int -> CoopConcur.Coq_prog a -> IO a
-run_dcode _ tid (Done r) = do
+-- hlist, represented as a mapping from variable indices to values
+type VMap = Data.Map.Map Int GHC.Prim.Any
+
+get_var :: VMap -> Variables.Coq_var a -> a
+get_var vm a =
+  case Data.Map.lookup (hmember_to_int a) vm of
+    Just x -> unsafeCoerce x
+    Nothing -> error $ "get of unset variable " ++ (show (hmember_to_int a))
+
+set_var :: VMap -> Variables.Coq_var a -> a -> VMap
+  -- TODO: implement
+set_var m a v = Data.Map.insert (hmember_to_int a) (unsafeCoerce v) m
+
+data ConcurrentState =
+  -- CS vm lock
+  CS !(IORef VMap) !(MVar ())
+
+acquire_global_lock :: ConcurrentState -> IO ()
+acquire_global_lock (CS _ lock) = takeMVar lock
+
+release_global_lock :: ConcurrentState -> IO ()
+release_global_lock (CS _ lock) = putMVar lock ()
+
+type ProgramState = (Disk.DiskState, ConcurrentState)
+
+run_dcode :: ProgramState -> Int -> CoopConcur.Coq_prog a -> IO a
+run_dcode _ tid (Ret r) = do
   debugmsg tid $ "Done"
-  return r
-run_dcode ds tid (StartRead a rx) = do
+  return . unsafeCoerce $ r
+run_dcode _ tid (StartRead a) = do
   debugmsg tid $ "StartRead " ++ (show a)
   -- XXX start a read, somehow...
-  run_dcode ds tid $ rx ()
-run_dcode ds tid (FinishRead a rx) = do
+  return . unsafeCoerce $ ()
+run_dcode (ds, _) tid (FinishRead a) = do
   debugmsg tid $ "FinishRead " ++ (show a)
   -- XXX it would be nice if we didn't wait until the last minute to read..
   val <- Disk.read_disk ds a
-  run_dcode ds tid $ rx val
-run_dcode ds tid (Write a v rx) = do
+  return . unsafeCoerce $ val
+run_dcode (ds, _) tid (Write a v) = do
   debugmsg tid $ "Write " ++ (show a) ++ " " ++ (show v)
   Disk.write_disk ds a v
-  run_dcode ds tid $ rx ()
-run_dcode ds tid (Sync a rx) = do
-  debugmsg tid $ "Sync " ++ (show a)
-  Disk.sync_disk ds a
-  run_dcode ds tid $ rx ()
-run_dcode ds tid (Get a rx) = do
-  debugmsg tid $ "Get " ++ (show (hmember_to_int a))
-  val <- Disk.get_var ds (hmember_to_int a)
-  run_dcode ds tid $ rx val
-run_dcode ds tid (Assgn a v rx) = do
+  return . unsafeCoerce $ ()
+run_dcode (_, CS vm _) tid (Get a) = do
+    debugmsg tid $ "Get " ++ (show (hmember_to_int a))
+    m <- readIORef vm
+    return . unsafeCoerce $ get_var m a
+run_dcode (_, CS vm _) tid (Assgn a v) = do
   debugmsg tid $ "Assgn " ++ (show (hmember_to_int a))
-  Disk.set_var ds (hmember_to_int a) v
-  run_dcode ds tid $ rx ()
-run_dcode ds tid (GetTID rx) = do
+  modifyIORef vm (\m -> set_var m a v)
+  return . unsafeCoerce $ ()
+run_dcode _ tid (GetTID) = do
   debugmsg tid $ "GetTID"
-  run_dcode ds tid $ rx tid
-run_dcode ds tid (Yield wchan rx) = do
+  return . unsafeCoerce $ tid
+run_dcode (_, cs) tid (Yield wchan) = do
   debugmsg tid $ "Yield " ++ (show wchan)
-  waiter <- newEmptyMVar
-  Disk.register_waiter ds wchan waiter
-  Disk.release_global_lock ds
-  _ <- takeMVar waiter
-  Disk.acquire_global_lock ds
-  run_dcode ds tid $ rx ()
-run_dcode ds tid (Wakeup wchan rx) = do
+  -- ignore wchan for now
+  release_global_lock cs
+  acquire_global_lock cs
+  return . unsafeCoerce $ ()
+run_dcode _ tid (Wakeup wchan) = do
   debugmsg tid $ "Wakeup " ++ (show wchan)
-  waiters <- Disk.get_waiters ds wchan
-  _ <- mapM (\waiter -> putMVar waiter ()) waiters
-  run_dcode ds tid $ rx ()
-
-run_dcode ds tid (GhostUpdate _ rx) = do
+  return . unsafeCoerce $ ()
+run_dcode _ tid (GhostUpdate _) = do
   debugmsg tid $ "GhostUpdate"
-  run_dcode ds tid $ rx ()
+  return . unsafeCoerce $ ()
+run_dcode ps tid (Hash sz (W64 w)) =
+  run_dcode ps tid (Hash sz (W $ fromIntegral w))
+run_dcode _ tid (Hash sz (W w)) = do
+  debugmsg tid $ "Hash " ++ (show sz) ++ " " ++ (show w)
+  wbs <- Disk.i2bs w $ fromIntegral $ (sz + 7) `div` 8
+  h <- return $ SHA256.hash wbs
+  ih <- Disk.bs2i h
+  return $ unsafeCoerce $ W ih
+run_dcode ps tid (Bind p1 p2) = do
+  debugmsg tid $ "Bind"
+  r1 <- run_dcode ps tid p1
+  r2 <- run_dcode ps tid (p2 r1)
+  return . unsafeCoerce $ r2
 
-run_e :: Disk.DiskState -> Int -> ((a -> CoopConcur.Coq_prog a) -> CoopConcur.Coq_prog a) -> IO a
-run_e ds tid p = do
-  Disk.acquire_global_lock ds
-  ret <- run_dcode ds tid $ p (\x -> CoopConcur.Done x)
-  Disk.release_global_lock ds
+run_e :: ProgramState -> Int -> CoopConcur.Coq_prog a -> IO a
+run_e (ds, cs) tid p = do
+  acquire_global_lock cs
+  ret <- run_dcode (ds, cs) tid p
+  release_global_lock cs
   return ret
 
 spin_forever :: IO a
@@ -86,5 +121,5 @@ print_exception tid e = do
   putStrLn $ "[" ++ (show tid) ++ "] Exception: " ++ (show e)
   spin_forever
 
-run :: Disk.DiskState -> Int -> ((a -> CoopConcur.Coq_prog a) -> CoopConcur.Coq_prog a) -> IO a
-run ds tid p = E.catch (run_e ds tid p) (print_exception tid)
+run :: ProgramState -> Int -> CoopConcur.Coq_prog a -> IO a
+run ps tid p = E.catch (run_e ps tid p) (print_exception tid)
