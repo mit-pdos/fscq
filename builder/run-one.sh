0eb2bca031f1ff2e7e6782cc28a4e23386d8233c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-12-08 16:47:37 -0500

    time how long it takes to do each of the nightly build steps (build and checkproofs)

--- builder/run-one.sh
+++ builder/run-one.sh
@@ -18,8 +18,8 @@ cd ~/builder/runs && ( ls | head -n -20 | xargs rm -rf )
 cd $D
 git clone -b $BRANCH git://g.csail.mit.edu/fscq-impl fscq
 cd fscq/src
-script $D/make-out.txt -c 'make'
-script $D/checkproofs-out.txt -c 'make checkproofs J=24'
+script $D/make-out.txt -c 'time make'
+script $D/checkproofs-out.txt -c 'time make checkproofs J=24'
 cat $D/checkproofs-out.txt | grep -v '^Checking task ' > $D/checkproofs-errors.txt
 cd $D
 python3 ~/builder/parse-errors.py $BUILDNAME
