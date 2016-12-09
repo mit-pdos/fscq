02e6e7372cf7d6c3406f448473530bd40c5d3397
Tej Chajed <tchajed@mit.edu>
2016-12-09 12:36:26 -0500

    Add concurrent micro benchmark
    Large copy in parallel with lots of small reads

---
+++ bench/concurrent/prepare.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+code=$DIR/../../src
+img=$code/init-disk.img
+mnt=/tmp/fscq
+
+mkdir -p $mnt
+
+$code/mkfs $img
+$code/fscq $img -f $mnt &
+sleep 1
+
+dd if=/dev/urandom of=$mnt/large-10m bs=1k count=10000
+dd if=/dev/urandom of=$mnt/small-4k bs=1k count=4
+
+sudo umount $mnt
+# fscq binary should terminate
+wait
---
+++ bench/concurrent/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+code=$DIR/../../src
+img=$code/disk.img
+mnt=/tmp/fscq
+
+cp $code/init-disk.img $img
+
+$code/cfscq +RTS -N4 -RTS $img -f $mnt &
+sleep 1
+
+{ time cp $mnt/large-10m $mnt/large-10m-copy;
+ echo "done with large copy"; } &
+large_pid=$!
+
+{ time for i in $(seq 2500); do
+    cat $mnt/small-4k > /dev/null;
+done;
+ echo "done with small copies"; } &
+small_pid=$!
+
+wait $small_pid
+wait $large_pid
+
+sudo umount $mnt
+wait
