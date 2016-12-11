6d07ec459ff2d7e31f93fb275982e9b04dfe7509
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-12-11 05:27:08 -0500

    coq-trunk and coq-v86 seem to be producing identical results

--- builder/run.sh
+++ builder/run.sh
@@ -6,9 +6,9 @@ lockfile -r 0 $L || exit 0
 THISDIR=$(dirname $0)
 
 $THISDIR/run-one.sh /usr/local/bin master master-coq85
-$THISDIR/run-one.sh /usr/local/coq-v86/bin master master-coq86
+# $THISDIR/run-one.sh /usr/local/coq-v86/bin master master-coq86
 $THISDIR/run-one.sh /usr/local/coq-trunk/bin master master-coqtrunk
 $THISDIR/run-one.sh /usr/local/coq-v86/bin io-concur io-concur-coq86
-$THISDIR/run-one.sh /usr/local/coq-trunk/bin io-concur io-concur-coqtrunk
+# $THISDIR/run-one.sh /usr/local/coq-trunk/bin io-concur io-concur-coqtrunk
 
 rm -f $L
