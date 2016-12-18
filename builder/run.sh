f62d84315d060b84ce488c386830cb9d8baf89c8
Nickolai Zeldovich <nickolai@csail.mit.edu>
2016-12-17 20:45:32 -0500

    don't build master with coq-v85
    coq-trunk seems to be almost fine now (only one bug remaining, in BlockPtr.v),
    and maintaining compatibility with coq-v85 is a bit annoying (e.g., some proofs
    in ListUtils that work in coq-trunk are broken with coq-v85).  so let's just
    switch to v86/trunk.

--- builder/run.sh
+++ builder/run.sh
@@ -5,7 +5,7 @@ lockfile -r 0 $L || exit 0
 
 THISDIR=$(dirname $0)
 
-$THISDIR/run-one.sh /usr/local/bin master master-coq85
+# $THISDIR/run-one.sh /usr/local/bin master master-coq85
 # $THISDIR/run-one.sh /usr/local/coq-v86/bin master master-coq86
 $THISDIR/run-one.sh /usr/local/coq-trunk/bin master master-coqtrunk
 $THISDIR/run-one.sh /usr/local/coq-v86/bin io-concur io-concur-coq86
