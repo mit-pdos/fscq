e65ca94614c6903d226682af6d53ddb600297297
Tej Chajed <tchajed@mit.edu>
2016-12-29 13:54:11 -0500

    Prove file_truncate and retry

--- src/DirectoryIsolatedFS.v
+++ src/DirectoryIsolatedFS.v
@@ -526,6 +526,398 @@ Definition lookup_root fnlist :=
                        CFS.lookup fsxp (FSLayout.FSXPRootInum fsxp) fnlist mscs)
                     (fun tree => tree).
 
+Definition dirtree_alter_file inum (up:BFILE.bfile -> BFILE.bfile) :
+  DIRTREE.dirtree -> DIRTREE.dirtree :=
+  DIRTREE.alter_inum inum
+                     (fun subtree =>
+                        match subtree with
+                        | DIRTREE.TreeFile inum' f =>
+                          DIRTREE.TreeFile inum' (up f)
+                        | DIRTREE.TreeDir _ _ => subtree
+                        end).
+
+Definition file_truncate1 inum sz :=
+  fsxp <- Get mFsxp;
+    mscs <- Get mMscs;
+    r <- CFS.file_truncate fsxp inum sz mscs;
+    match r with
+    | Some r =>
+      let '(mscs', (r, _)) := r in
+      _ <- Assgn mMscs mscs';
+        _ <- ConcurrentCache.cache_commit;
+        _ <- if r
+            then var_update
+                   vDirTree
+                   (dirtree_alter_file
+                      inum
+                      (fun f => let 'BFILE.mk_bfile d attr := f in
+                             let d' := ListUtils.setlen d sz ($0, nil) in
+                             BFILE.mk_bfile d' attr))
+
+            else Ret tt;
+        Ret (value r)
+    | None =>
+      _ <- ConcurrentCache.cache_abort;
+        Ret None
+    end.
+
+Definition file_truncate inum sz :=
+  fuel_retry (file_truncate1 inum sz).
+
+Ltac member_index_ne := match goal with
+                        | |- member_index ?v1 <> member_index ?v2 =>
+                          try unfold v1; try unfold v2;
+                          simpl;
+                          rewrite ?get_next, ?get_first;
+                          simpl;
+                          Omega.omega
+                        end.
+
+Lemma pred_lift_or : forall A AEQ V (p q q': @pred A AEQ V) (P Q:Prop) m,
+    (p * (([[ P ]] * q) \/ ([[ Q ]] * q')))%pred m ->
+    (P /\ (p * q)%pred m) \/
+    (Q /\ (p * q')%pred m).
+Proof.
+  intros.
+  apply sep_star_or_distr in H.
+  unfold or in H.
+  destruct H; [ left | right ].
+  assert ((p * q * [[ P ]])%pred m).
+  pred_apply; cancel.
+  apply sep_star_lift_apply in H0; intuition.
+  assert ((p * q' * [[ Q ]])%pred m).
+  pred_apply; cancel.
+  apply sep_star_lift_apply in H0; intuition.
+Qed.
+
+Lemma update_subtree_helper_already_found : forall inum rec l a d,
+    DIRTREE.tree_names_distinct (DIRTREE.TreeDir inum ((a, d) :: l)) ->
+    List.map (DIRTREE.update_subtree_helper
+                rec a) l = l.
+Proof.
+  intros.
+  inversion H; subst; simpl in *.
+  inversion H3; subst.
+  clear H H3.
+  induction l; simpl in *; intros; auto.
+  unfold DIRTREE.update_subtree_helper at 1.
+  destruct a0; simpl in *.
+  destruct (string_dec s a); subst; eauto.
+  - exfalso; eauto.
+  - f_equal.
+    apply IHl; eauto.
+    rewrite List.Forall_forall; simpl; intros.
+    rewrite List.Forall_forall in H2.
+    apply H2; simpl.
+    intuition eauto.
+Qed.
+
+Lemma dirtree_alter_to_update : forall pathname subtree up tree,
+    DIRTREE.tree_names_distinct tree ->
+    DIRTREE.find_subtree pathname tree = Some subtree ->
+    DIRTREE.alter_subtree pathname up tree =
+    DIRTREE.update_subtree pathname (up subtree) tree.
+Proof.
+  induction pathname; simpl; intros.
+  inversion H0; subst; auto.
+  destruct tree; try congruence.
+  induction l; simpl in *; try congruence.
+  unfold DIRTREE.find_subtree_helper in H0 at 1.
+  destruct a0; simpl.
+  destruct (string_dec s a); subst; eauto.
+  f_equal.
+  f_equal.
+  f_equal; eauto.
+
+  erewrite ?update_subtree_helper_already_found by eauto; auto.
+
+  f_equal.
+  f_equal.
+
+  repeat specialize (IHl ltac:(eauto)).
+  inversion IHl; eauto.
+Qed.
+
+Lemma dirtree_rep_tree_names_distinct : forall fsxp F tree ilist frees m,
+    DIRTREE.rep fsxp F tree ilist frees m ->
+    DIRTREE.tree_names_distinct tree.
+Proof.
+  intros.
+  eapply DIRTREE.rep_tree_names_distinct.
+  pred_apply' H; cancel.
+Qed.
+
+Lemma dirtree_rep_tree_inodes_distinct : forall fsxp F tree ilist frees m,
+    DIRTREE.rep fsxp F tree ilist frees m ->
+    DIRTREE.tree_inodes_distinct tree.
+Proof.
+  intros.
+  eapply DIRTREE.rep_tree_inodes_distinct.
+  pred_apply' H; cancel.
+Qed.
+
+Hint Resolve dirtree_rep_tree_names_distinct
+     dirtree_rep_tree_inodes_distinct.
+
+Theorem file_truncate1_ok : forall inum sz,
+      SPEC App.delta, tid |-
+              {{ pathname f,
+               | PRE d hm m s_i s:
+                   let tree := get vDirTree s in
+                   invariant App.delta d hm m s /\
+                   DIRTREE.find_subtree pathname tree = Some (DIRTREE.TreeFile inum f) /\
+                   path_owner (get vPathOwner s) pathname = Owned tid /\
+                   guar App.delta tid s_i s
+               | POST d' hm' m' s_i' s' r:
+                   let tree' := get vDirTree s' in
+                   invariant App.delta d' hm' m' s' /\
+                   match r with
+                   | Some (OK _) =>
+                     let d' := ListUtils.setlen (BFILE.BFData f) sz ($0, nil) in
+                     let f' := BFILE.mk_bfile d' (BFILE.BFAttr f) in
+                     tree' = DIRTREE.update_subtree
+                               pathname (DIRTREE.TreeFile inum f') (get vDirTree s)
+                   | _ => tree' = get vDirTree s
+                   end /\
+                   guar App.delta tid s s' /\
+                   hashmap_le hm hm' /\
+                   guar App.delta tid s_i' s'
+              }} file_truncate1 inum sz.
+Proof.
+  unfold file_truncate1, wrap_syscall; intros.
+  step.
+  step.
+  step.
+
+  match goal with
+  | [ H: invariant App.delta _ _ _ _ |- _ ] =>
+    simpl in H
+  end.
+  match goal with
+  | [ H: guar App.delta _ _ _ |- _ ] =>
+    simpl in H
+  end.
+  destruct_ands; repeat deex.
+  (* exists_tuple breaks apart ds *)
+  destruct ds.
+
+  unfold project_disk.
+  repeat eapply exists_tuple; eexists; simpl.
+  intuition eauto.
+
+  replace (get vdisk s).
+  pred_apply; cancel; eauto.
+
+  step.
+  destruct matches; subst.
+  - step.
+    step.
+    unfold cacheI in *; simpl_get_set_all; intuition eauto.
+    step.
+    step.
+    unfold cacheI in *; simpl_get_set_all;
+      rewrite ?get_set_other in * by member_index_ne;
+      intuition eauto.
+
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+
+    match goal with
+    | [ H: (emp * _)%pred (lower_disk (get vdisk s0)) |- _ ] =>
+      apply star_emp_pimpl in H;
+        apply pred_lift_or in H
+    end.
+    intuition auto; try congruence.
+    match goal with
+    | [ H: ([[ _ ]] * _)%pred (lower_disk (get vdisk s0)) |- _ ] =>
+      apply sep_star_comm in H;
+        apply sep_star_lift_apply in H
+    end.
+    intuition idtac.
+
+    descend; intuition idtac.
+    pred_apply; cancel.
+    pred_apply; cancel.
+
+    match goal with
+    | [ H: ([[ _ ]] * _)%pred (lower_disk (get vdisk s0)) |- _ ] =>
+      apply sep_star_comm in H;
+        apply sep_star_lift_apply in H
+    end.
+    unfold exis in *; intuition idtac; repeat deex.
+    repeat split_lifted_prop; subst.
+
+    unfold dirtree_alter_file.
+    erewrite DIRTREE.alter_inum_to_alter_path by eauto.
+    erewrite dirtree_alter_to_update by eauto.
+    destruct f; simpl.
+    descend; intuition idtac.
+    pred_apply; cancel.
+    pred_apply; cancel.
+
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+
+    unfold dirtree_alter_file.
+    erewrite DIRTREE.alter_inum_to_alter_path by eauto.
+    erewrite dirtree_alter_to_update by eauto.
+    destruct f; auto.
+
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+    unfold cacheR; rewrite ?get_set_other by member_index_ne; try reflexivity.
+
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+    replace (get vPathOwner s_i).
+    unfold dirtree_alter_file.
+    erewrite DIRTREE.alter_inum_to_alter_path by eauto.
+    erewrite dirtree_alter_to_update by eauto.
+    destruct f.
+
+    eapply allowed_subtree_update_file; eauto.
+    simpl in *.
+    replace (path_owner (get vPathOwner s) pathname).
+    reflexivity.
+
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+    unfold cacheR; rewrite ?get_set_other by member_index_ne; try reflexivity.
+
+    eapply allowed_preorder; eauto.
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+    replace (get vPathOwner s_i).
+    unfold dirtree_alter_file.
+    erewrite DIRTREE.alter_inum_to_alter_path by eauto.
+    erewrite dirtree_alter_to_update by eauto.
+    destruct f.
+    eapply allowed_subtree_update_file; eauto.
+    simpl in *.
+    replace (path_owner (get vPathOwner s) pathname).
+    reflexivity.
+  - step.
+    step.
+
+    unfold cacheI; simpl_get_set_all; intuition eauto.
+
+    step.
+
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+    descend; intuition eauto.
+    pred_apply; cancel.
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+
+    etransitivity; eauto.
+    replace (get vDirTree s).
+    replace (get vDirTree s0).
+    reflexivity.
+  - step.
+    step.
+
+    repeat match goal with
+           | [ H: get _ _ = get _ _ |- _ ] => rewrite H
+           end.
+    descend; intuition eauto.
+
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+    eapply cacheR_preorder; eauto.
+
+    etransitivity; eauto.
+    replace (get vDirTree s).
+    replace (get vDirTree s0).
+    reflexivity.
+
+    Unshelve.
+    all: eauto.
+Qed.
+
+Hint Extern 1 {{file_truncate1 _ _; _}} => apply file_truncate1_ok.
+
+Theorem file_truncate_ok : forall inum sz n,
+      SPEC App.delta, tid |-
+              {{ pathname f,
+               | PRE d hm m s_i s:
+                   let tree := get vDirTree s in
+                   invariant App.delta d hm m s /\
+                   DIRTREE.find_subtree pathname tree = Some (DIRTREE.TreeFile inum f) /\
+                   path_owner (get vPathOwner s) pathname = Owned tid /\
+                   guar App.delta tid s_i s
+               | POST d'' hm'' m'' s_i' s'' r:
+                   invariant App.delta d'' hm'' m'' s'' /\
+                   (exists d' hm' m' s',
+                     rely App.delta tid s s' /\
+                     invariant App.delta d' hm' m' s' /\
+                     guar App.delta tid s' s'' /\
+                     let tree' := get vDirTree s'' in
+                     match r with
+                     | Some (OK _) =>
+                       let d' := ListUtils.setlen (BFILE.BFData f) sz ($0, nil) in
+                       let f' := BFILE.mk_bfile d' (BFILE.BFAttr f) in
+                       tree' = DIRTREE.update_subtree
+                                 pathname (DIRTREE.TreeFile inum f') (get vDirTree s')
+                     | _ => True
+                     end) /\
+                   hashmap_le hm hm'' /\
+                   guar App.delta tid s_i' s''
+              }} file_truncate inum sz n.
+Proof.
+  unfold file_truncate.
+  induction n; simpl; intros.
+  - eapply pimpl_ok; [ apply ret_ok | ]; intros; repeat deex.
+    exists tt; intuition eauto.
+    eapply pimpl_ok; [ apply H0 | ]; intros; intuition subst; eauto.
+
+    descend; intuition eauto.
+    constructor.
+    reflexivity.
+  - step.
+    descend; intuition eauto.
+    step.
+    step.
+    exists d, hm, m, s.
+    intuition eauto.
+    constructor.
+    destruct matches; subst; eauto.
+
+    step.
+    step.
+    descend; intuition eauto.
+    eapply find_subtree_preserved_rely with (s:=s0); eauto.
+    simpl in *.
+    replace (get vDirTree s0); eauto.
+    simpl in *; congruence.
+
+    eapply find_subtree_preserved_rely with (s:=s0); eauto.
+    simpl in *.
+    replace (get vDirTree s0); eauto.
+    simpl in *; congruence.
+
+    reflexivity.
+
+    step.
+
+    exists d', hm', m', s'.
+    intuition eauto.
+    etransitivity; eauto.
+    etransitivity; eauto.
+
+    apply rely_guar_const_dirtree; simpl in *; intuition eauto.
+
+    etransitivity; eauto.
+    etransitivity; eauto.
+Qed.
+
 Theorem lookup1_ok : forall dnum fnlist,
       SPEC App.delta, tid |-
               {{ (_:unit),
@@ -947,16 +1339,6 @@ Proof.
     reflexivity.
 Qed.
 
-Definition dirtree_alter_file inum (up:BFILE.bfile -> BFILE.bfile) :
-  DIRTREE.dirtree -> DIRTREE.dirtree :=
-  DIRTREE.alter_inum inum
-                     (fun subtree =>
-                        match subtree with
-                        | DIRTREE.TreeFile inum' f =>
-                          DIRTREE.TreeFile inum' (up f)
-                        | DIRTREE.TreeDir _ _ => subtree
-                        end).
-
 Definition file_set_attr1 inum attr :=
   fsxp <- Get mFsxp;
     mscs <- Get mMscs;
@@ -982,101 +1364,6 @@ Definition file_set_attr1 inum attr :=
 
 Definition file_set_attr inum attr :=
   fuel_retry (file_set_attr1 inum attr).
-
-Ltac member_index_ne := match goal with
-                        | |- member_index ?v1 <> member_index ?v2 =>
-                          try unfold v1; try unfold v2;
-                          simpl;
-                          rewrite ?get_next, ?get_first;
-                          simpl;
-                          Omega.omega
-                        end.
-
-Lemma pred_lift_or : forall A AEQ V (p q q': @pred A AEQ V) (P Q:Prop) m,
-    (p * (([[ P ]] * q) \/ ([[ Q ]] * q')))%pred m ->
-    (P /\ (p * q)%pred m) \/
-    (Q /\ (p * q')%pred m).
-Proof.
-  intros.
-  apply sep_star_or_distr in H.
-  unfold or in H.
-  destruct H; [ left | right ].
-  assert ((p * q * [[ P ]])%pred m).
-  pred_apply; cancel.
-  apply sep_star_lift_apply in H0; intuition.
-  assert ((p * q' * [[ Q ]])%pred m).
-  pred_apply; cancel.
-  apply sep_star_lift_apply in H0; intuition.
-Qed.
-
-Lemma update_subtree_helper_already_found : forall inum rec l a d,
-    DIRTREE.tree_names_distinct (DIRTREE.TreeDir inum ((a, d) :: l)) ->
-    List.map (DIRTREE.update_subtree_helper
-                rec a) l = l.
-Proof.
-  intros.
-  inversion H; subst; simpl in *.
-  inversion H3; subst.
-  clear H H3.
-  induction l; simpl in *; intros; auto.
-  unfold DIRTREE.update_subtree_helper at 1.
-  destruct a0; simpl in *.
-  destruct (string_dec s a); subst; eauto.
-  - exfalso; eauto.
-  - f_equal.
-    apply IHl; eauto.
-    rewrite List.Forall_forall; simpl; intros.
-    rewrite List.Forall_forall in H2.
-    apply H2; simpl.
-    intuition eauto.
-Qed.
-
-Lemma dirtree_alter_to_update : forall pathname subtree up tree,
-    DIRTREE.tree_names_distinct tree ->
-    DIRTREE.find_subtree pathname tree = Some subtree ->
-    DIRTREE.alter_subtree pathname up tree =
-    DIRTREE.update_subtree pathname (up subtree) tree.
-Proof.
-  induction pathname; simpl; intros.
-  inversion H0; subst; auto.
-  destruct tree; try congruence.
-  induction l; simpl in *; try congruence.
-  unfold DIRTREE.find_subtree_helper in H0 at 1.
-  destruct a0; simpl.
-  destruct (string_dec s a); subst; eauto.
-  f_equal.
-  f_equal.
-  f_equal; eauto.
-
-  erewrite ?update_subtree_helper_already_found by eauto; auto.
-
-  f_equal.
-  f_equal.
-
-  repeat specialize (IHl ltac:(eauto)).
-  inversion IHl; eauto.
-Qed.
-
-Lemma dirtree_rep_tree_names_distinct : forall fsxp F tree ilist frees m,
-    DIRTREE.rep fsxp F tree ilist frees m ->
-    DIRTREE.tree_names_distinct tree.
-Proof.
-  intros.
-  eapply DIRTREE.rep_tree_names_distinct.
-  pred_apply' H; cancel.
-Qed.
-
-Lemma dirtree_rep_tree_inodes_distinct : forall fsxp F tree ilist frees m,
-    DIRTREE.rep fsxp F tree ilist frees m ->
-    DIRTREE.tree_inodes_distinct tree.
-Proof.
-  intros.
-  eapply DIRTREE.rep_tree_inodes_distinct.
-  pred_apply' H; cancel.
-Qed.
-
-Hint Resolve dirtree_rep_tree_names_distinct
-     dirtree_rep_tree_inodes_distinct.
 
 Theorem file_set_attr1_ok : forall inum attr,
       SPEC App.delta, tid |-
@@ -1316,11 +1603,6 @@ Proof.
     etransitivity; eauto.
     etransitivity; eauto.
 Qed.
-
-Definition file_truncate inum sz :=
-  wrap_syscall_loop (fun fsxp mscs =>
-                       CFS.file_truncate fsxp inum sz mscs)
-                    (fun tree => tree).
 
 Definition update_fblock_d inum off v :=
   wrap_syscall'_loop (fun fsxp mscs =>
