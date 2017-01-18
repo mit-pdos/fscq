69dfd61faa9461ae68a0aa330d66cb5c4494f84a
Alex Konradi <alexkonradi@gmail.com>
2017-01-17 23:55:17 -0500

    Add dummy fscq.go for testing
    This will eventually be the main executable for FSCQ but for now it will
    serve as a way of running the extracted library code.

---
+++ src/golib/fscq.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"gofscq"
+)
+
+// Just for testing currently
+
+func main() {
+	a := fscq.New_Num()
+	b := fscq.New_pair_pair_AddrMap_pair_DiskBlock_bool_Num_Empty()
+	fscq.Writeback(a, b)
+}
