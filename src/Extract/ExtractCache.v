b33499712781f3df9f6676145b29529218ff60cc
Alex Konradi <alexkonradi@gmail.com>
2017-01-21 02:05:38 -0500

    Fix extraction lemmas in ExtractCache.v

--- src/Extract/ExtractCache.v
+++ src/Extract/ExtractCache.v
@@ -6,6 +6,7 @@ Import ListNotations.
 
 Import Go.
 Open Scope pred_scope.
+Open Scope type_scope.
 
 Require Import Cache.
 
@@ -37,46 +38,40 @@ Defined.
 
 Local Open Scope string_scope.
 
-(*
-Example compile_writeback : forall env, sigT (fun p => forall a cs,
+Example compile_writeback : sigT (fun p => source_stmt p /\
+  forall env a cs,
   EXTRACT BUFCACHE.writeback a cs
-  {{ 0 ~> a * 1 ~> cs }}
+  {{ 0 ~> a * 1 ~> cs * 2 ~>? cachestate }}
     p
-  {{ fun ret => 0 ~>? MapUtils.AddrMap.Map.key * 1 ~> ret }} // env
-  /\ source_stmt p).
+  {{ fun ret => 0 ~>? MapUtils.AddrMap.Map.key * 1 ~>? cachestate * 2 ~> ret }} // env).
 Proof.
   unfold BUFCACHE.writeback.
-  intros.
   compile.
 Defined.
 
-Eval lazy in (projT1 (compile_writeback (StringMap.empty _))).
-*)
+Eval lazy in (projT1 compile_writeback).
 
-Example compile_evict : forall env, sigT (fun p => forall a cs,
+Example compile_evict : sigT (fun p => source_stmt p /\ forall env a cs,
   prog_func_call_lemma {| FArgs := [with_wrapper addr; with_wrapper cachestate]; FRet := with_wrapper cachestate |} "writeback" BUFCACHE.writeback env ->
   EXTRACT BUFCACHE.evict a cs
-  {{ 0 ~> a * 1 ~> cs }}
+  {{ 0 ~> a * 1 ~> cs * 2 ~>? cachestate }}
     p
-  {{ fun ret => 0 |->? * 1 ~> ret }} // env
-  /\ source_stmt p).
+  {{ fun ret => 0 ~>? W * 1 ~>? cachestate * 2 ~> ret }} // env).
 Proof.
   unfold BUFCACHE.evict.
-  intros.
   compile.
 Defined.
-Eval lazy in (projT1 (compile_evict (StringMap.empty _))).
 
-(*
-Example compile_maybe_evict : forall env, sigT (fun p => forall cs,
-  func2_val_ref "evict" BUFCACHE.evict env ->
+Eval lazy in (projT1 compile_evict).
+
+Example compile_maybe_evict : sigT (fun p => source_stmt p /\ forall env cs,
+  prog_func_call_lemma {| FArgs := [with_wrapper addr; with_wrapper cachestate]; FRet := with_wrapper cachestate |} "evict" BUFCACHE.evict env ->
   EXTRACT BUFCACHE.maybe_evict cs
-  {{ 0 ~> cs }}
+  {{ 0 ~> cs * 1 ~>? cachestate }}
     p
-  {{ fun ret => 0 ~> ret }} // env).
+  {{ fun ret => 0 ~>? cachestate * 1 ~> ret }} // env).
 Proof.
   unfold BUFCACHE.maybe_evict.
-  intros.
   compile_step.
   compile_step.
   compile_step.
@@ -89,7 +84,7 @@ Proof.
   compile_step.
   compile_step.
   eapply hoare_weaken.
-  eapply CompileMapCardinal with (var0 := pair_vec_nthl 0 0 vars) (mvar := pair_vec_nthl 0 1 vars).
+  eapply CompileMapCardinal with (var0 := nth_var 0 vars) (mvar := nth_var 1 vars).
   cancel_go.
   cancel_go.
   compile_step.
@@ -97,6 +92,7 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
+  cancel_go.
   simpl decls_pre. (* TODO: most of the [simpl]s in GoExtraction.v right now are in the wrong spot -- could add a declarationn in another goal *)
   compile_step.
   compile_step.
@@ -116,6 +112,8 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
+  compile_step.
+  cancel_go.
   Ltac pattern_prog pat :=
     match goal with
     | [ |- ProgMonad.prog_equiv _ ?pr ] =>
@@ -131,7 +129,7 @@ Proof.
   compile_step.
   compile_step.
   eapply hoare_weaken.
-  eapply CompileMapElements with (mvar := pair_vec_nthl 0 1 vars) (var0 := pair_vec_nthl 0 14 vars).
+  eapply CompileMapElements with (mvar := nth_var 1 vars) (var0 := nth_var 15 vars).
   cancel_go.
   cancel_go.
   simpl decls_pre.
@@ -141,36 +139,65 @@ Proof.
   simpl decls_pre.
   do_declare (list (nat * (word AsyncDisk.Valulen.valulen * bool))%type) ltac:(fun xsvar => idtac xsvar).
   eapply hoare_weaken.
-  apply CompileUncons with (lvar := pair_vec_nthl 0 14 vars)
-                             (cvar := pair_vec_nthl 0 15 vars)
-                             (xvar := pair_vec_nthl 0 16 vars)
-                             (xsvar := pair_vec_nthl 0 17 vars).
+  apply CompileUncons with (lvar := nth_var 15 vars)
+                             (cvar := nth_var 16 vars)
+                             (xvar := nth_var 17 vars)
+                             (xsvar := nth_var 18 vars).
   3: cancel_go.
   3: cancel_go.
   compile_step.
   compile_step.
   compile_step.
   compile_step.
+  cancel_go.
   intros.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  repeat compile_step.
+  compile_step.
+  compile_step.
+  repeat compile_step.
+  compile_step.
+  cancel_go.
+  Unshelve. all: compile.
+Defined.
+
+Definition eviction_update' a s := Ret (eviction_update s a).
+
+Instance WrapByTransforming_evictionstate : WrapByTransforming eviction_state.
+  refine {|
+    transform := fun s => tt;
+  |}.
+  simpl; intros. destruct t1, t2; f_equal; auto.
+Defined.
+
+Example compile_eviction_update' : sigT (fun p => source_stmt p /\ forall env a s,
+  EXTRACT eviction_update' a s
+  {{ 0 ~> a * 1 ~> s * 2 ~>? eviction_state }}
+    p
+  {{ fun ret => 0 |->? * 1 |->? * 2 ~> ret }} // env).
+Proof.
+  unfold eviction_update', eviction_update.
   compile.
 Defined.
 
+Eval lazy in (projT1 compile_eviction_update').
 
 Transparent BUFCACHE.read.
 
-
-(* TODO *)
-Definition eviction_update' a s := Ret (eviction_update s a).
-
-Example compile_read : forall env, sigT (fun p => forall a cs,
-  func1_ref "maybe_evict" BUFCACHE.maybe_evict env ->
-  func2_val_ref "eviction_update" eviction_update' env ->
+Example compile_read : sigT (fun p => source_stmt p /\ forall env a cs,
+  prog_func_call_lemma {| FArgs := [with_wrapper cachestate]; FRet := with_wrapper cachestate |}
+    "maybe_evict" BUFCACHE.maybe_evict env ->
+  prog_func_call_lemma {| FArgs := [with_wrapper W; with_wrapper eviction_state]; FRet := with_wrapper eviction_state |}
+    "eviction_update" eviction_update' env ->
   EXTRACT BUFCACHE.read a cs
   {{ 0 ~> a * 1 ~> cs * 2 ~>? (cachestate * (valu * unit)) }}
     p
   {{ fun ret => 0 |->? * 1 ~>? cachestate * 2 ~> ret }} // env).
 Proof.
-  intros. unfold BUFCACHE.read.
+  unfold BUFCACHE.read.
   compile_step.
   compile_step.
   compile_step.
@@ -213,12 +240,15 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
-  (* TODO: copy here automatically. This is *the* standard mostly unavoidable copy *)
-  do_declare valu ltac:(fun ka' =>
-                          eapply CompileBefore; [ 
-                            eapply CompileRet with (v := a1) (var0 := ka');
-                            eapply hoare_weaken; [
-                              eapply CompileDup with (var0 := pair_vec_nthl 0 14 vars) (var' := ka') | cancel_go .. ] | ]).
+  match goal with
+  |- context [?k ~> a1] =>
+    (* TODO: copy here automatically. This is *the* standard mostly unavoidable copy *)
+    do_declare valu ltac:(fun ka' =>
+                            eapply CompileBefore; [
+                              eapply CompileRet with (v := a1) (var0 := ka');
+                              eapply hoare_weaken; [
+                                eapply CompileDup with (var0 := k) (var' := ka') | cancel_go .. ] | ])
+  end.
   compile_step.
   compile_step.
   compile_step.
@@ -234,22 +264,27 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
+  compile_step.
+  compile_step.
   cancel_go.
 
   Unshelve.
-  all: econstructor.
+  all: compile.
 Defined.
 
+
 Transparent BUFCACHE.write.
-Example compile_write : forall env, sigT (fun p => forall a v cs,
-  func1_ref "maybe_evict" BUFCACHE.maybe_evict env ->
-  func2_val_ref "eviction_update" eviction_update' env ->
+Example compile_write : sigT (fun p => source_stmt p /\ forall env a v cs,
+  prog_func_call_lemma {| FArgs := [with_wrapper cachestate]; FRet := with_wrapper cachestate |}
+    "maybe_evict" BUFCACHE.maybe_evict env ->
+  prog_func_call_lemma {| FArgs := [with_wrapper W; with_wrapper eviction_state]; FRet := with_wrapper eviction_state |}
+    "eviction_update" eviction_update' env ->
   EXTRACT BUFCACHE.write a v cs
-  {{ 0 ~> a * 1 ~> v * 2 ~> cs }}
+  {{ 0 ~> a * 1 ~> v * 2 ~> cs * 3 ~>? cachestate }}
     p
-  {{ fun ret => 0 ~>? addr * 1 ~>? valu * 2 ~> ret }} // env).
+  {{ fun ret => 0 ~>? addr * 1 ~>? valu * 2 ~>? cachestate * 3 ~> ret }} // env).
 Proof.
-  intros. unfold BUFCACHE.write.
+  unfold BUFCACHE.write.
   compile_step.
   compile_step.
   compile_step.
@@ -283,65 +318,78 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
+  compile_step.
+  compile_step.
+  cancel_go.
+  cancel_fast_normr.
+  rewrite sep_star_comm.
+  apply pimpl_sep_star. cancel.
+  cancel_go.
 
   Unshelve.
-  all: econstructor.
+  all: compile.
 Defined.
 
 Transparent BUFCACHE.begin_sync.
-Example compile_begin_sync : forall env, sigT (fun p => forall cs,
+Example compile_begin_sync : sigT (fun p => source_stmt p /\ forall env cs,
   EXTRACT BUFCACHE.begin_sync cs
-  {{ 0 ~> cs }}
+  {{ 0 ~> cs * 1 ~>? cachestate }}
     p
-  {{ fun ret => 0 ~> ret }} // env).
+  {{ fun ret => 0 |->? * 1 ~> ret }} // env).
 Proof.
   intros. unfold BUFCACHE.begin_sync.
   (* TODO: make this not split & rejoin :P *)
   compile.
 Defined.
 
-Eval lazy in projT1 (compile_begin_sync _).
-*)
+Eval lazy in projT1 (compile_begin_sync).
 
 Transparent BUFCACHE.sync.
 
-Example compile_sync : forall env, sigT (fun p => forall a cs,
+Example compile_sync : sigT (fun p => source_stmt p /\ forall env a cs,
   prog_func_call_lemma {| FRet := with_wrapper cachestate; FArgs := [with_wrapper addr; with_wrapper cachestate] |} "writeback" BUFCACHE.writeback env ->
   EXTRACT BUFCACHE.sync a cs
-  {{ 0 ~> a * 1 ~> cs }}
+  {{ 0 ~> a * 1 ~> cs * 2 ~>? cachestate }}
     p
-  {{ fun ret => 0 ~>? addr * 1 ~> ret }} // env).
+  {{ fun ret => 0 ~>? addr * 1 ~>? cachestate * 2 ~> ret }} // env).
 Proof.
-  intros. unfold BUFCACHE.sync.
-  compile.
+  unfold BUFCACHE.sync.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  compile_step.
+  cancel. cancel_go.
+  Unshelve.
+  all : compile.
 Defined.
-Eval lazy in projT1 (compile_sync _).
+Eval lazy in projT1 (compile_sync).
 
 
 Transparent BUFCACHE.end_sync.
-Example compile_end_sync : forall env, sigT (fun p => forall cs,
+Example compile_end_sync : sigT (fun p => source_stmt p /\ forall env cs,
   EXTRACT BUFCACHE.end_sync cs
   {{ 0 ~> cs }}
     p
   {{ fun ret => 0 ~> ret }} // env).
 Proof.
-  intros. unfold BUFCACHE.end_sync.
+  unfold BUFCACHE.end_sync.
   compile.
 Defined.
 
 
 Transparent BUFCACHE.init.
-Example compile_init : forall env, sigT (fun p => forall n,
+Example compile_init : sigT (fun p => source_stmt p /\ forall env n,
   EXTRACT BUFCACHE.init n
   {{ 0 ~> n * 1 ~>? cachestate }}
     p
   {{ fun ret => 0 ~>? nat * 1 ~> ret }} // env).
 Proof.
-  intros. unfold BUFCACHE.init.
-  repeat compile_step.
-  cancel_go.
-  Unshelve.
-  exact nil.
+  unfold BUFCACHE.init.
+  compile.
 Defined.
 
 Local Open Scope string_scope.
@@ -358,27 +406,29 @@ Ltac argtuple pre var :=
   | _ => constr:(pair 0 tt)
   end.
 
-(*
 Ltac add_to_env name P env :=
   match type of P with
-  | EXTRACT _ {{ ?PRE }} ?body_ {{ fun ret : ?R => ?POST }} // _ =>
-    lazymatch constr:(fun ret : mark_ret R => (_:find_ret POST)) with
-    | (fun ret => ?rvar) =>
-      match PRE with
-      | context [?x ~> ?y] =>
-        match (argtuple PRE 0) with
-        | (?nargs, ?args) =>
-          let x_ := fresh in
-          set (body := body_);
-          set (op := {|
-            NumParamVars := nargs;
-            ParamVars := args;
-            Body := body;
-            body_source := ltac:(eauto);
-          |});
-          set (env' := StringMap.add name op env);
-          simpl in *; subst body; subst env;
-          rename env' into env; subst op
+  | context [EXTRACT _ {{ ?PRE }} ?body_ {{ ?POST }} // _] =>
+    match POST with
+    | (fun ret : ?R => ?POST) =>
+      lazymatch constr:(fun ret : mark_ret R => (_:find_ret POST)) with
+      | (fun ret => ?rvar) =>
+        match PRE with
+        | context [?x ~> ?y] =>
+          match (argtuple PRE 0) with
+          | (?nargs, ?args) =>
+            let x_ := fresh in
+            set (body := body_);
+            set (op := {|
+              NumParamVars := nargs;
+              ParamVars := args;
+              Body := body;
+              body_source := ltac:(eauto);
+            |});
+            set (env' := StringMap.add name op env);
+            simpl in *; subst body; subst env;
+            rename env' into env; subst op
+          end
         end
       end
     end
@@ -388,100 +438,46 @@ Ltac add_compiled_program name compiled env :=
   let P := fresh in
   let e_ := fresh in
   let H := fresh in
-  destruct (compiled env) as [e_  P];
+  let S := fresh "src" in
+  destruct compiled as [e_ P];
+  match type of P with
+  | Logic.and (source_stmt _) _ =>
+    apply proj1 in P as S;
+    apply proj2 in P
+  | Logic.and _ (source_stmt _) =>
+    apply proj2 in P as S;
+    apply proj1 in P
+  end;
   repeat match type of P with
   | forall x : ?X, ?y =>
       let x_ := fresh "v" in
       cut X; [intro x_; specialize (P x_) | solve [abstract (repeat econstructor)] ]
-  | func2_val_ref ?a ?b ?c -> ?y =>
-      let x_ := fresh "v" in
-    cut (func2_val_ref a b c); [intro x_; specialize (P x_) | clear P]
-  | Logic.and _ _ => destruct P
-  end ;
-  add_to_env name P env ;
+  end;
+  try (add_to_env name P env;
   (* Remove unnecessary variables *)
+  clear P;
   repeat match goal with
   | [env := ?X, v : _ |- _] =>
-    clear v
-  end;
-  (* preserve proof for solving func2_ref_val *)
-  pose proof compiled.
+    lazymatch type of v with
+    | context [ProgOk] => fail
+    | _ => clear v
+    end
+  end).
 
-Definition prog_of := projT1.
 Definition extract_env : Env.
   pose (env := StringMap.empty FunctionSpec).
   add_compiled_program "writeback" compile_writeback env.
+  add_compiled_program "evict" compile_evict env.
+  add_compiled_program "maybe_evict" compile_maybe_evict env.
+  add_compiled_program "eviction_update" compile_eviction_update' env.
+  add_compiled_program "read" compile_read env.
+  add_compiled_program "write" compile_write env.
+  add_compiled_program "sync" compile_sync env.
+  add_compiled_program "compile_end_sync" compile_end_sync env.
+  add_compiled_program "begin_sync" compile_begin_sync env.
+  add_compiled_program "end_sync" compile_end_sync env.
+  add_compiled_program "init" compile_init env.
+  (* TODO add more programs here *)
+
   exact env.
 Defined.
-
-
- Ltac add_compiled_program name compiled env ::=
-  let P := fresh in
-  let e_ := fresh in
-  let H := fresh in
-  destruct (compiled env) as [e_  P];
-  repeat match type of P with
-  | forall x : ?X, ?y =>
-      let x_ := fresh "v" in
-      cut X; [intro x_; specialize (P x_) | solve [abstract (repeat econstructor)] ]
-  | func2_val_ref ?a ?b ?c -> ?y =>
-      let x_ := fresh "v" in
-    cut (func2_val_ref a b c); [intro x_; specialize (P x_) | clear P]
-  | Logic.and _ _ => destruct P
-  end.
-  add_compiled_program "evict" compile_evict env.
-  Focus 2.
-  
-  
-  Ltac func2_val_ref := match goal with
-  | [H : context [ProgOk] |- func2_val_ref _ ?p ?env] =>
-    match type of H with
-    | context [sigT] =>
-      destruct (H env); clear H
-    | context [?q] => (is_evar q; fail 1) ||
-      unify p q;
-      eapply extract_func2_val_ref_call;
-      [ eapply H ||
-        (let H1 := fresh in
-        edestruct H as [H1 ]; eapply H1) |
-      ]
-    end
-  end.
-  Print func2_val_ref.
-  Print func2_ref_val.
-  func2_val_ref.
-  func2_val_ref.
-  Check extract_func2_val_ref_call.
-  subst_definitions.
-  Ltac map_find_step :=
-    match goal with
-    |- context [StringMap.find ?k (StringMap.add ?k' _ _)] =>
-      destruct (StringMap.E.eq_dec k k');
-      [rewrite StringMapFacts.add_eq_o by auto |
-       rewrite StringMapFacts.add_neq_o by auto];
-       try congruence
-    end.
-  map_find_step.
-  simpl.
-  congruence.
-  
-  
-      
-  eval_expr.
-  
-  
-  intros. edestruct a. eauto.
-  edestruct X.
-  eapply extract_func2_val_ref_call.
-  intros.
-  edestruct a.
-  eauto.
-  
-  apply p.
-  clear p.
-  subst env.
-  
-Search func2_val_ref.
-  unfold func2_val_ref.
-  (* TODO add more programs here *)
-  *)
