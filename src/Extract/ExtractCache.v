e845012b9607f534c7a53034367b1a1a93f6e361
Alex Konradi <alexkonradi@gmail.com>
2017-01-24 15:42:53 -0500

    Fix argument detection in ExtractCache.v

--- src/Extract/ExtractCache.v
+++ src/Extract/ExtractCache.v
@@ -357,16 +357,25 @@ Local Open Scope string_scope.
 Local Open Scope list_scope.
 Local Open Scope pred_scope.
 
-Ltac argtuple pre var :=
+Ltac argtuple' pre var :=
   match pre with
-  | context [var |-> @wrap ?T ?V ?val] =>
-    let X := argtuple pre (S var) in
-    let P := constr:(@wrap_type _ V) in
+  | context [var |-> (Val ?V _)] =>
+    let X := argtuple' pre (S var) in
+    match X with
+    | (?count, ?X)  =>
+      constr:(pair (S count) (pair V X))
+    end
+  | context [var |-> @wrap _ ?Wr _] =>
+    let X := argtuple' pre (S var) in
+    let P := constr:(@wrap_type _ Wr) in
     match X with
     | (?count, ?X)  => constr:(pair (S count) (pair P X))
     end
   | _ => constr:(pair 0 tt)
   end.
+
+Ltac argtuple PRE :=
+  argtuple' PRE 0.
 
 Ltac add_to_env name P env :=
   match type of P with
@@ -375,26 +384,32 @@ Ltac add_to_env name P env :=
     | (fun ret : ?R => ?POST) =>
       lazymatch constr:(fun ret : mark_ret R => (_:find_ret POST)) with
       | (fun ret => ?rvar) =>
-        match PRE with
-        | context [?x ~> ?y] =>
-          match (argtuple PRE 0) with
-          | (?nargs, ?args) =>
-            let x_ := fresh in
-            set (body := body_);
-            set (op := {|
-              NumParamVars := nargs;
-              ParamVars := args;
-              Body := body;
-              body_source := ltac:(eauto);
-            |});
-            set (env' := StringMap.add name op env);
-            simpl in *; subst body; subst env;
-            rename env' into env; subst op
-          end
+        match (argtuple PRE) with
+        | (?nargs, ?args) =>
+          let x_ := fresh in
+          set (body := body_);
+          set (op := {|
+            NumParamVars := nargs;
+            ParamVars := args;
+            Body := body;
+            body_source := ltac:(eauto);
+          |});
+          set (env' := StringMap.add name op env);
+          simpl in *; subst body; subst env;
+          rename env' into env; subst op
         end
       end
     end
   end.
+
+Ltac make_instance_step :=
+  match goal with
+  | [|- word ?x] => apply wzero
+  | _ => econstructor
+  end.
+
+Ltac make_instance :=
+  abstract (repeat make_instance_step).
 
 Ltac add_compiled_program name compiled env :=
   let P := fresh in
@@ -413,9 +428,9 @@ Ltac add_compiled_program name compiled env :=
   repeat match type of P with
   | forall x : ?X, ?y =>
       let x_ := fresh "v" in
-      cut X; [intro x_; specialize (P x_) | solve [abstract (repeat econstructor)] ]
+      cut X; [intro x_; specialize (P x_) | make_instance ]
   end;
-  try (add_to_env name P env;
+  (add_to_env name P env;
   (* Remove unnecessary variables *)
   clear P;
   repeat match goal with
