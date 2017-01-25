bea01adfcb465ed68912343a98fbf50d2e7ee6d2
Alex Konradi <alexkonradi@gmail.com>
2017-01-25 13:43:10 -0500

    Fix Go compilation in makefile
    It looks like everything builds, though running the fscq binary is still
    pretty unexciting.

--- src/golib/fscq.go
+++ src/golib/fscq.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"gofscq"
+	"fmt"
 )
 
 // Just for testing currently
 
 func main() {
 	a := fscq.New_Num()
-	b := fscq.New_pair_pair_AddrMap_pair_DiskBlock_bool_Num_Empty()
-	fscq.Writeback(a, b)
+	b := fscq.New_pair_pair_AddrMap_pair_Buffer_Bool_Num_Empty()
+	c := fscq.New_pair_pair_AddrMap_pair_Buffer_Bool_Num_Empty()
+	fmt.Println("Running fscq.Writeback(%v, %v, %v)", a, b, c)
+	fscq.Writeback(&c, &a, &b)
+	fmt.Println("Finished running fscq.Writeback")
 }
