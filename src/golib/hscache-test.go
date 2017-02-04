04e362958eb505e6dfafa5db1dd327fb021bceb2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2017-02-03 20:29:31 -0500

    get go-hscache-test to compile again

--- src/golib/hscache-test.go
+++ src/golib/hscache-test.go
@@ -12,30 +12,30 @@ const cachesize = 100000
 
 var valulen = fscq.Num_of_i64(4096)
 
-func do_read(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty,
-	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty {
-	ret := fscq.New_Pair_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty_Pair_Buffer_Empty()
+func do_read(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_,
+	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_ {
+	ret := fscq.New_Pair_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct__Pair_Buffer_Struct_()
 	fscq.Read(&ret, &addr, &cs)
 	return ret.Fst
 }
 
-func do_write(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty,
-	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty {
-	new_cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty()
+func do_write(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_,
+	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_ {
+	new_cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_()
 	buf := fscq.New_Buffer(valulen)
 	fscq.Write(&new_cs, &addr, &buf, &cs)
 	return new_cs
 }
 
-func do_sync(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty,
-	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty {
-	new_cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty()
+func do_sync(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_,
+	addr fscq.Num) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_ {
+	new_cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_()
 	fscq.Sync(&new_cs, &addr, &cs)
 	return new_cs
 }
 
-func exec_line(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty,
-	line string) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty {
+func exec_line(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_,
+	line string) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_ {
 	split := strings.Split(line, " ")
 	switch split[0] {
 	case "write":
@@ -51,7 +51,7 @@ func exec_line(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty,
 	return cs
 }
 
-func exec_input(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty {
+func exec_input(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_) fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_ {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -62,7 +62,7 @@ func exec_input(cs fscq.Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty) fscq.Pair_
 
 func run_test(disk string) {
 	fscq.Init_disk(disk)
-	cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Empty()
+	cs := fscq.New_Pair_Pair_AddrMap_Pair_Buffer_Bool_Num_Struct_()
 	cache_size := fscq.Num_of_i64(cachesize)
 	fscq.Init(&cs, &cache_size)
 	cs = exec_input(cs)
