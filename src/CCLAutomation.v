b97c3905179ce6415227e004637b6b92c661a86e
Tej Chajed <tchajed@mit.edu>
2017-02-04 14:30:38 -0500

    Get basic proofs working

---
+++ src/CCLAutomation.v
@@ -0,0 +1,48 @@
+Require Export CCL CCLPrimitives.
+Require Pred.
+
+Ltac destruct_st :=
+  match goal with
+  | [ st: Sigma _ * Sigma _, H: context[let '(a, b) := ?st in _] |- _ ] =>
+    destruct st as [a b]; cbn [precondition postcondition] in *
+  end.
+
+(* unfold ReflectDouble into primitive Hoare double statement *)
+Ltac unfold_double :=
+  match goal with
+  | [ H: ReflectDouble _ _ _ _ _ _ |- _ ] =>
+    unfold ReflectDouble in H; simpl;
+    repeat Pred.deex
+  | [ |- ReflectDouble _ _ _ _ _ _ ] =>
+    unfold ReflectDouble; simpl
+  end.
+
+Ltac simplify :=
+  intros; repeat Pred.deex;
+  repeat unfold_double;
+  repeat destruct_st;
+  repeat match goal with
+         | [ H: _ /\ _ |- _ ] => destruct H
+         | _ => subst
+         end.
+
+Ltac monad_simpl :=
+  let rewrite_equiv H := eapply cprog_ok_respects_exec_equiv;
+                         [ solve [ apply H ] | ] in
+  repeat match goal with
+         | _ => rewrite_equiv monad_right_id
+         | _ => rewrite_equiv monad_left_id
+         | _ => rewrite_equiv monad_assoc
+         end.
+
+Ltac step :=
+  match goal with
+  | [ |- cprog_ok _ _ _ _ ] =>
+    eapply cprog_ok_weaken; [
+      monad_simpl; (solve [ auto with prog ] ||
+                    match goal with
+                    | [ |- cprog_ok _ _ _ (Bind ?p _) ] =>
+                      fail "no spec for" p
+                    end) | ];
+    simplify
+  end.
---
+++ src/SyncRead.v
@@ -0,0 +1,55 @@
+Require Import CCLAutomation.
+Require Import AsyncDisk.
+Require Import FunctionalExtensionality.
+
+Section SyncRead.
+
+  Variable St:StateTypes.
+  Variable G: TID -> Sigma St -> Sigma St -> Prop.
+
+  Definition SyncRead a : @cprog St _ :=
+    _ <- BeginRead a;
+      v <- WaitForRead a;
+      Ret v.
+
+  Theorem SyncRead_ok : forall tid a,
+      cprog_triple G tid
+                   (fun v0 '(sigma_i, sigma) =>
+                      {| precondition :=
+                           Sigma.disk sigma a = Some (v0, NoReader);
+                         postcondition :=
+                           fun '(sigma_i', sigma') r =>
+                             sigma_i' = sigma_i /\
+                             sigma' = sigma /\
+                             r = v0; |})
+                   (SyncRead a).
+  Proof.
+    unfold cprog_triple.
+    unfold SyncRead; intros.
+    step.
+
+    eexists; intuition eauto.
+
+    step.
+
+    intuition eauto; subst.
+    eexists; intuition eauto.
+
+    (* need better way to simplify nested updates on Sigma *)
+    destruct sigma; simpl in *.
+    autorewrite with upd; eauto.
+
+    step.
+    intuition auto; simplify.
+
+    destruct sigma; simpl in *.
+
+    f_equal.
+    extensionality a'.
+    destruct (addr_eq_dec a a'); subst;
+      autorewrite with upd; auto.
+  Qed.
+
+End SyncRead.
+
+Hint Extern 0 {{ SyncRead _; _ }} => apply SyncRead_ok : prog.
