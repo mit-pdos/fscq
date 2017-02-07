5e0dc93a323903ee000994d94dc69d7bb3555937
Tej Chajed <tchajed@mit.edu>
2017-02-06 19:31:15 -0500

    Define a directory protocol for ConcurrentFS
    A section variable gives a global guarantee condition, and a section
    variable assumes that it implies the file-system guarantee.

---
+++ src/HomeDirProtocol.v
@@ -0,0 +1,26 @@
+Require Import DirTree.
+Require Export String.
+Require Import RelationClasses.
+Require Import CCLProg.
+
+Export DirTreeDef.
+
+Definition thread_homes := TID -> list string.
+
+Definition homedir_rely tid (homes: thread_homes) (tree tree':dirtree) :=
+  find_subtree (homes tid) tree' = find_subtree (homes tid) tree.
+
+Definition homedir_guarantee tid (homes: thread_homes) (tree tree':dirtree) :=
+  forall tid', tid' <> tid ->
+          homedir_rely tid homes tree tree'.
+
+Instance homedir_guar_preorder tid homes : PreOrder (homedir_guarantee tid homes).
+Proof.
+  unfold homedir_guarantee, homedir_rely.
+  constructor; hnf; intros.
+  auto.
+
+  specialize (H tid').
+  specialize (H0 tid').
+  intuition congruence.
+Defined.
