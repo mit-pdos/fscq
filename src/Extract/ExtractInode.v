918ecffe907e35d98abf9593e0079142d5620fd6
Daniel Ziegler <dmz@mit.edu>
2017-02-14 22:33:50 -0500

    progress on [IRec.get]

--- src/Extract/ExtractInode.v
+++ src/Extract/ExtractInode.v
@@ -166,7 +166,121 @@ Proof.
   compile_step.
   compile_step.
   compile_step.
-  (* TODO: selN_val2block *)
+  unfold INODE.IRec.Defs.selN_val2block.
+  Import Rec.
+  cbv [Rec.of_word Rec.len INODE.IRecSig.itemtype INODE.irectype INODE.iattrtype INODE.NDirect
+             Rec.len Rec.data Rec.field_type string_dec string_rec string_rect Ascii.ascii_dec Ascii.ascii_rec Ascii.ascii_rect
+             plus minus mult
+             addrlen hashlen wtl whd
+             sumbool_rec sumbool_rect Bool.bool_dec bool_rec bool_rect eq_rec_r eq_rec eq_rect eq_sym eq_ind_r eq_ind] in *.
+  match goal with
+  | |- context[@Rec.word_selN' ?ft ?l ?i ?w] => pattern_prog (@Rec.word_selN' ft l i w)
+  end.
+  Ltac do_declare T cont ::=
+  lazymatch goal with
+  | |- EXTRACT _
+       {{ ?pre }}
+          _
+       {{ _ }} // _ =>
+         lazymatch goal with
+         | |- EXTRACT _
+              {{ ?pre }}
+                 _
+              {{ _ }} // _ =>
+           (* no simpl *)
+               lazymatch pre with
+               | context [ decls_pre ?decls ?vars ?m ] =>
+                   let decls' := fresh "decls" in
+                   evar ( decls' : list Declaration ); unify decls (Decl T :: decls'); subst decls';
+                    cont (nth_var m vars)
+               end
+         end
+  end.
+  do_declare (immut_word 1024) ltac:(fun var => idtac var).
+  eapply hoare_weaken; [ eapply CompileBindRet with (HA := GoWrapper_immut_word 1024) (vara := nth_var 20 vars) | cancel_go.. ].
+  unfold Rec.word_selN'.
+  rewrite <- Rec.word_selN_shift_equiv.
+  unfold Rec.word_selN.
+  break_match.
+  cbv [Rec.len plus mult]. fold Nat.add Nat.mul.
+  Ltac make_value_exist v_ :=
+    let T := type of v_ in
+    eapply CompileBefore; [
+      do_declare T ltac:(fun var => idtac var v_;
+                           eapply CompileRet with (var0 := var) (v := v_)); repeat compile_step | ].
+  lazymatch goal with
+  | [ |- EXTRACT (Ret (?f ?a ?b ?c ?d)) {{ _ }} _ {{ _ }} // _ ] =>
+    make_value_exist a
+  end.
+  lazymatch goal with
+  | [ |- EXTRACT (Ret (?f ?a ?b ?c ?d)) {{ _ }} _ {{ _ }} // _ ] =>
+    make_value_exist (a + b)
+  end.
+  (* Freeze the buffer *)
+  pattern_prog (fst (snd a)).
+  do_declare (immut_word valulen) ltac:(fun var => idtac var).
+  eapply hoare_weaken.
+  eapply CompileBindRet with (A := immut_word valulen) (vara := nth_var 23 vars) (a := fst (snd a)).
+  3: cancel_go.
+  3: cancel_go.
+
+  eapply hoare_weaken.
+  apply CompileFreeze with (svar := nth_var 16 vars) (dvar := nth_var 23 vars).
+  Import PeanoNat.
+  Hint Extern 2 (Nat.divide 8 (S (S ?n2))) =>
+    (exists ((S (S n2)) / 8); reflexivity) : divide.
+  Hint Resolve Nat.divide_sub_r : divide.
+  Lemma valulen_divide_8 : Nat.divide 8 valulen.
+  Proof.
+    rewrite valulen_is. exists (valulen_real / 8). reflexivity.
+  Qed.
+  Hint Resolve valulen_divide_8 : divide.
+  divisibility.
+  cancel_go.
+  cancel_go.
+  eapply hoare_weaken.
+  eapply (@CompileMiddle _ _ _ _ env (nth_var 20 vars) (nth_var 23 vars) (nth_var 21 vars) (nth_var 22 vars)).
+  divisibility.
+  divisibility.
+  divisibility.
+  cancel_go.
+  norm.
+  do 26 delay_one.
+  eapply cancel_one.
+  eapply PickFirst.
+  match goal with
+  | |- okToCancel (nth_var _ vars |-> ?a) (nth_var _ vars |-> ?b) => let H := fresh in assert (a = b) as H; [ | rewrite H; reflexivity ]
+  end.
+  cbv [wrap wrap' wrap_type GoWrapper_immut_word].
+  f_equal.
+  unfold INODE.IRec.Defs.val2word.
+  unfold eq_rec.
+  Import EqNotations.
+Theorem eq_rect_double : forall A P (a b c : A) (x : P a) (ab : a = b) (bc : b = c),
+  rew bc in rew ab in x = rew eq_trans ab bc in x.
+Proof.
+  intros.
+  destruct ab.
+  destruct bc.
+  simpl.
+  auto.
+Qed.
+  rewrite eq_rect_double.
+  match goal with
+  | |- context[rew ?He in _] => let H := fresh in let Te := type of He in assert Te as H; [ | generalize He; rewrite <- H ]
+  end.
+  rewrite INODE.IRecSig.blocksz_ok; simpl.
+  rewrite (Rec.word_selN_helper 1024 l) at 1.
+  reflexivity.
+  intros.
+  f_equal.
+  Require Import Eqdep.
+  rewrite UIP_refl with (p := e).
+  reflexivity.
+  cancel'.
+  cancel_go.
+  admit. (* TODO: do equivalent rewrites as above in order to rewrite away references to variables
+     which [?Y] does not have in scope *)
 Admitted.
 
 Definition extract_env : Env.
