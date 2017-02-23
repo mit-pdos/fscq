9b9707aaffc86d6c9abba9c39b453c99645a67ec
Tej Chajed <tchajed@mit.edu>
2017-02-22 20:53:42 -0500

    Some benchmark running conveniences

--- bench/concurrent/run-all.sh
+++ bench/concurrent/run-all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# Usage: ./run-all.sh | tee data.tsv
+
 echo -e "fs\toperation\tmissing\tattr_cache\tname_cache\tneg_name_cache\tparallel\tkiters\ttime\tspeedup\ttimePerOp"
 
-for kiters in 5 10 100; do
-  for op in stat open; do
-    for fs in hfuse cfuse fusexmp native; do
-      for missing in "false" "true"; do
-        for attrcache in "false" "true"; do
-          for namecache in "false" "true"; do
-            for negnamecache in "false" "true"; do
-              fsbench -op=$op -missing=$missing -parallel=true -kiters=$kiters -attr-cache=$attrcache -name-cache=$namecache -neg-cache=$negnamecache $fs
-            done
+for kiters in 10 15 100; do
+  for cache1 in "false" "true"; do
+    for cache2 in "false" "true"; do
+      for op in stat open; do
+        for fs in hfuse cfuse hello fusexmp native; do
+          for missing in "false" "true"; do
+            fsbench -op=$op -missing=$missing -parallel=true -kiters=$kiters -attr-cache=$cache1 -name-cache=$cache1 -neg-cache=$cache2 $fs 2>&1
           done
         done
       done
---
+++ bench/concurrent/setup.fish
@@ -0,0 +1,5 @@
+set -gx GOPATH $PWD/fsbench
+set PATH $GOPATH/bin $PATH
+set PATH ~/hfuse/.cabal-sandbox/bin ~/c-hellofs $PATH
+set PATH ~/fscq-impl/src ~/fscq-impl/bench/concurrent $PATH
+set PATH ~/libfuse/build/example $PATH
