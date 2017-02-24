81c2504395148d7d80903c53252d74349b178665
Tej Chajed <tchajed@mit.edu>
2017-02-24 10:23:07 -0500

    Remove c-fuse

--- bench/concurrent/run-all.sh
+++ bench/concurrent/run-all.sh
@@ -16,7 +16,7 @@ for kernelcache in "false"; do
     for cache2 in "false"; do
       info "caches: {attr,name}=$cache1\tneg=$cache2\tkernel=$kernelcache"
       for op in stat open; do
-        #for fs in hfuse cfuse c-hello fusexmp native; do
+        #for fs in hfuse c-hello fusexmp native; do
         for fs in c-hello fusexmp native; do
           for exists in "true" "false"; do
             fsbench -target-ms=1000 -op=$op -exists=$exists -parallel=true -attr-cache=$cache1 -name-cache=$cache1 -neg-cache=$cache2 -kernel-cache=$kernelcache $fs
