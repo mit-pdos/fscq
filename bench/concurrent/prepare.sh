1e8ae0c338fd0afcfd299ecbd181536384d93b79
Tej Chajed <tchajed@mit.edu>
2017-03-01 17:42:00 -0500

    Add script to create disk image

---
+++ bench/concurrent/prepare.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+img="$1"
+mnt=/tmp/fscq
+
+if [ -z "$img" ]; then
+  echo "Usage: $0 <disk.img>"
+  exit 1
+fi
+
+mkfs $img
+fscq $img "$mnt"
+
+dd if=/dev/urandom of="$mnt/small-4k" bs=4k count=1
+mkdir "$mnt/dir1"
+mkdir "$mnt/dir2"
+
+touch "$mnt/dir1/file1"
+touch "$mnt/dir1/file2"
+touch "$mnt/dir2/file1"
+touch "$mnt/dir2/file2"
+
+path1="a/b/c/d/e/f"
+path2="a____/b____/c____/d____/e____/f____"
+mkdir -p "$mnt/$path1"
+mkdir -p "$mnt/$path2"
+touch "$mnt/$path1/file"
+touch "$mnt/$path2/file"
+
+for file in $mnt/**; do
+  sync $file
+done
+
+fusermount -u /tmp/fscq
