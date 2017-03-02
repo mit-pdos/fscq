cdddc29f3a213333abcca2ecf8ba4c6adb9abea4
Tej Chajed <tchajed@mit.edu>
2017-03-02 16:42:52 -0500

    Compile more constructs

--- src/ConcurCompile.v
+++ src/ConcurCompile.v
@@ -118,6 +118,59 @@ Section ConcurCompile.
     reflexivity.
   Defined.
 
+  Fixpoint cForN_ (L : Type)
+           (f : nat -> L -> WriteBuffer -> cprog (Result L * WriteBuffer))
+           (i n: nat) (l: L)
+    : WriteBuffer -> cprog (St:=St OtherSt) (Result L * WriteBuffer) :=
+    fun wb =>
+      match n with
+      | O => Ret (Success l, wb)
+      | S n' =>
+        ret <- f i l wb;
+          let '(r, wb') := ret in
+          match r with
+          | Success l' => cForN_ f (S i) n' l' wb'
+          | Failure e => Ret (Failure e, wb')
+          end
+      end.
+
+  Lemma translate_ForN : forall L G p i n
+                           nocrash crashed l ls,
+      translate OtherSt (@ForN_ L G p i n nocrash crashed l) ls =
+      cForN_ (fun i l => translate OtherSt (p i l) ls) i n l.
+  Proof.
+    intros.
+    extensionality wb.
+    generalize dependent l.
+    generalize dependent wb.
+    generalize dependent i.
+    induction n; simpl; intros; eauto.
+    f_equal.
+    extensionality r.
+    destruct r as [ [? |] ?]; eauto.
+  Qed.
+
+  Lemma compile_forN L p :
+    (forall i l wb, Compiled (p i l wb)) ->
+    (forall i n l wb, Compiled (@cForN_ L p i n l wb)).
+  Proof.
+    intros.
+    unshelve refine (ExtractionOf (@cForN_ L _ i n l wb) _);
+      intros.
+    destruct (X H X0 X1).
+    exact p'.
+    generalize dependent i.
+    generalize dependent l.
+    generalize dependent wb.
+    induction n; intros; simpl.
+    reflexivity.
+    destruct (X i l wb); simpl.
+    eapply exec_equiv_bind; intros; eauto.
+    destruct v.
+    destruct r; eauto.
+    reflexivity.
+  Defined.
+
   Lemma compile_equiv T (p p': cprog T) :
     exec_equiv G p p' ->
     forall (cp': Compiled p'),
@@ -191,6 +244,29 @@ Section ConcurCompile.
     destruct r; eauto.
   Qed.
 
+  Lemma translate_match_sumbool : forall P Q T' (p1: prog T') (p2: prog T') (r:{P}+{Q}),
+      translate OtherSt (match r with
+                         | left _ => p1
+                         | right _ => p2
+                         end) =
+      match r with
+      | left _ => translate OtherSt p1
+      | right _ => translate OtherSt p2
+      end.
+  Proof.
+    intros.
+    destruct r; eauto.
+  Qed.
+
+  Lemma translate_destruct_prod : forall A B T' (p: A -> B -> prog T') (r:A*B),
+      translate OtherSt (let (a, b) := r in p a b) =
+      let (a, b) := r in
+      translate OtherSt (p a b).
+  Proof.
+    intros.
+    destruct r; eauto.
+  Qed.
+
   Ltac destruct_compiled :=
     match goal with
     | [ |- context[compiled_prog ?c] ] =>
@@ -235,6 +311,25 @@ Section ConcurCompile.
       eauto.
   Defined.
 
+  Lemma compile_match_sumbool P Q T' (p1:  _ -> _ -> cprog T') p2
+        (r: {P}+{Q}) (ls: LockState) (wb: WriteBuffer) :
+    (forall ls wb, Compiled (p1 ls wb)) ->
+    (forall ls wb, Compiled (p2 ls wb)) ->
+    Compiled (match r with
+              | left _ => p1
+              | right _ => p2
+              end ls wb).
+  Proof.
+    intros.
+    refine (ExtractionOf (match r with
+                          | left _ => fun ls wb => compiled_prog (X ls wb)
+                          | right _ => fun ls wb => compiled_prog (X0 ls wb)
+                          end ls wb) _).
+    destruct r;
+      destruct_compiled;
+      eauto.
+  Defined.
+
   Lemma compile_match_cT : forall T T' (p1: T -> WriteBuffer -> cprog T') p2 r,
       (forall v wb, Compiled (p1 v wb)) ->
       (forall e wb, Compiled (p2 e wb)) ->
@@ -254,59 +349,70 @@ Section ConcurCompile.
     destruct (X0 e w); simpl; eauto.
   Defined.
 
+  Lemma compile_destruct_prod : forall A B T' (p: A -> B -> cprog T') (r:A*B),
+      (forall a b, Compiled (p a b)) ->
+      Compiled (let (a, b) := r in p a b).
+  Proof.
+    intros.
+    refine (ExtractionOf (let (a, b) := r in
+                          compiled_prog (X a b)) _).
+    destruct r.
+    destruct (X a b); eauto.
+  Defined.
+
+  Ltac compile_hook := fail.
+
   Ltac compile :=
     match goal with
+
+      (* monad laws *)
     | [ |- Compiled (Bind (Ret _) _) ] =>
       eapply compile_equiv; [ solve [ apply monad_left_id ] | ]
     | [ |- Compiled (Bind (Bind _ _) _) ] =>
       eapply compile_equiv; [ solve [ apply monad_assoc ] | ]
     | [ |- Compiled (Bind _ _) ] =>
       apply compile_bind; intros
+
+    (* push translate inside functions *)
     | [ |- Compiled (translate _ (ForEach_ _ _ _ _ _) _ _) ] =>
       rewrite translate_ForEach
-    | [ |- Compiled (translate _ (match _ with
-                                 | OK _ => _
-                                 | Err _ => _
-                                 end) _ _) ] =>
+    | [ |- Compiled (translate _ (ForN_ _ _ _ _ _ _) _ _) ] =>
+      rewrite translate_ForN
+    | [ |- Compiled (translate _ (match _ with | _ => _ end) _ _) ] =>
       rewrite translate_match_res
-    | [ |- Compiled (translate _ (match _ with
-                                 | Some _ => _
-                                 | None => _
-                                 end) _ _) ] =>
+    | [ |- Compiled (translate _ (match _ with | _ => _ end) _ _) ] =>
       rewrite translate_match_opt
+    | [ |- Compiled (translate _ (match _ with | _ => _ end) _ _) ] =>
+      rewrite translate_match_sumbool
+    | [ |- Compiled (translate _ (let (_, _) := _ in _) _ _) ] =>
+      rewrite translate_destruct_prod
+
+    (* compile specific constructs *)
     | [ |- Compiled (cForEach_ _ _ _ _) ] =>
       apply compile_forEach; intros
-    | [ |- Compiled (match _ with
-                    | OK _ => _
-                    | Err _ => _
-                    end _ _) ] =>
+    | [ |- Compiled (cForN_ _ _ _ _ _) ] =>
+      apply compile_forN; intros
+    | [ |- Compiled (match _ with | _ => _ end _ _) ] =>
       apply compile_match_res; intros; eauto
-    | [ |- Compiled (match _ with
-                    | Some _ => _
-                    | None => _
-                    end _ _) ] =>
+    | [ |- Compiled (match _ with | _ => _ end _ _) ] =>
       apply compile_match_opt; intros; eauto
-    | [ |- Compiled (match _ with
-                    | _ => _
-                    end) ] =>
+    | [ |- Compiled (match _ with | _ => _ end _ _) ] =>
+      apply compile_match_sumbool; intros; eauto
+    | [ |- Compiled (match _ with | _ => _ end) ] =>
       apply compile_match_cT; intros; eauto
+    | [ |- Compiled (let _ := (_, _) in _) ] =>
+      apply compile_destruct_prod; intros
+
+    (* terminating programs that cannot be improved *)
     | [ |- Compiled (Ret _)] =>
       apply compile_refl
     | [ |- Compiled (CacheRead _ _ _)] =>
       apply compile_refl
-    | _ => progress (unfold pair_args_helper; simpl)
+
+    | _ => progress (unfold pair_args_helper, If_; simpl)
+
+    | _ => compile_hook
     end.
-
-  Definition CompiledLookup fsxp dnum names ams ls wb :
-    Compiled (OptFS.lookup OtherSt fsxp dnum names ams ls wb).
-  Proof.
-    unfold OptFS.lookup; simpl.
-
-    repeat compile.
-
-    (* TODO: if statements *)
-    apply compile_refl.
-  Defined.
 
   Ltac head_symbol e :=
     match e with
@@ -327,18 +433,39 @@ Section ConcurCompile.
       unfold h
     end.
 
+  Ltac compile_hook ::= comp_unfold ||
+       match goal with
+       | [ |- context[let (_, _) := ?p in _] ] =>
+         destruct p
+       end.
+
+  Transparent DirName.SDIR.lookup.
   Transparent BUFCACHE.read_array.
   Transparent BUFCACHE.read.
+
+  Lemma destruct_fun : forall T U A B (f: T -> U) (p: A * B) x,
+      (let (a, b) := p in f) x =
+      let (a, b) := p in f x.
+  Proof.
+    intros.
+    destruct p; auto.
+  Qed.
+
+  Definition CompiledLookup fsxp dnum names ams ls wb :
+    Compiled (OptFS.lookup OtherSt fsxp dnum names ams ls wb).
+  Proof.
+    unfold OptFS.lookup.
+
+    repeat compile;
+      apply compile_refl.
+  Defined.
 
   Definition CompiledGetAttr fsxp inum ams ls wb :
     Compiled (OptFS.file_get_attr OtherSt fsxp inum ams ls wb).
   Proof.
     unfold OptFS.file_get_attr; simpl.
-    repeat (compile || comp_unfold).
-
-    apply compile_refl.
-    apply compile_refl.
-    apply compile_refl.
+    repeat compile;
+      apply compile_refl.
   Defined.
 
 End ConcurCompile.
