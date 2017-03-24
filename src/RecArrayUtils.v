ffc6d8c5761e6d95fb8712afe0f90c4c3fcb892e
Nickolai Zeldovich <nickolai@csail.mit.edu>
2017-03-24 13:56:07 -0400

    optimize writing out log descriptor blocks
    the log descriptor blocks contain 512 64-bit addresses, but most are
    zero (when flushing a small set of changes).  we used to flush the
    block by padding it out with zero elements, and pickling all of them.
    
    this change removes the padding; the code already does the right thing
    by filling in zeroes when pickling an empty list.  no proofs yet.
    
    improves in-memory mailbench from 330 msg/s to 367 msg/s.

--- src/RecArrayUtils.v
+++ src/RecArrayUtils.v
@@ -179,9 +179,18 @@ Module RADefs (RA : RASig).
     | O => []
     end.
 
+  Fixpoint nopad_list_chunk' {A} (l : list A) (sz : nat) (nr : nat) : list (list A) :=
+    match nr with
+    | S n => firstn sz l :: (nopad_list_chunk' (skipn sz l) sz n)
+    | O => []
+    end.
+
   (** cut list l into chunks of lists of length sz, pad the tailing list with default value def *)
   Definition list_chunk {A} l sz def : list (list A) :=
     list_chunk' l sz def (divup (length l) sz).
+
+  Definition nopad_list_chunk {A} l sz : list (list A) :=
+    nopad_list_chunk' l sz (divup (length l) sz).
 
   Lemma list_chunk'_length: forall A nr l sz (def : A),
       length (list_chunk' l sz def nr) = nr.
