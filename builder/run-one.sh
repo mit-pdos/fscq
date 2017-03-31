10b16a5d859d8ef75da073f993bc5325ee97b843
Alex Konradi <alexkonradi@gmail.com>
2017-03-31 12:44:39 -0400

    Add commit ID to nightly build output

--- builder/run-one.sh
+++ builder/run-one.sh
@@ -21,6 +21,7 @@ coqtop </dev/null
 cd ~/builder/runs && ( ls | head -n -20 | xargs rm -rf )
 cd $D
 git clone -b $BRANCH git://g.csail.mit.edu/fscq-impl fscq
+echo "Building commit:" "`git show --no-patch --pretty=oneline $BRANCH`"
 cd fscq/src
 script $D/make-out.txt -c 'time make'
 script $D/checkproofs-out.txt -c 'time make checkproofs J=24'
