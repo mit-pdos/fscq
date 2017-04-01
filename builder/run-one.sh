de834db56d849df31a4ac8e06e1bb10bbd8a04e5
Alex Konradi <alexkonradi@gmail.com>
2017-03-31 20:53:57 -0400

    Fix builder script commit printout

--- builder/run-one.sh
+++ builder/run-one.sh
@@ -21,8 +21,8 @@ coqtop </dev/null
 cd ~/builder/runs && ( ls | head -n -20 | xargs rm -rf )
 cd $D
 git clone -b $BRANCH git://g.csail.mit.edu/fscq-impl fscq
-echo "Building commit:" "`git show --no-patch --pretty=oneline $BRANCH`"
 cd fscq/src
+echo "Building commit:" "`git show --no-patch --pretty=oneline $BRANCH`"
 script $D/make-out.txt -c 'time make'
 script $D/checkproofs-out.txt -c 'time make checkproofs J=24'
 cat $D/checkproofs-out.txt | grep -v '^Checking task ' > $D/checkproofs-errors.txt
