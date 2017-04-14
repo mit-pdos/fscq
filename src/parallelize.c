19c508740462482aa075fbb7bddde26c278f5e75
Tej Chajed <tchajed@mit.edu>
2017-04-14 10:32:57 -0400

    Use pthreads to actually parallelize in C

--- src/parallelize.c
+++ src/parallelize.c
@@ -1,8 +1,21 @@
 #include "parallelize.h"
+#include <pthread.h>
+#include <stdlib.h>
+
+void* call_action(void *arg) {
+  action *act = (action *) arg;
+  (*act)();
+  return NULL;
+}
 
 void parallel(int n, action act) {
-  // TODO: do these in parallel, not sequentially
+  pthread_t *threads = (pthread_t*) calloc(n, sizeof(pthread_t *));
+
   for (int i = 0; i < n; i++) {
-    act();
+    pthread_create(&threads[i], NULL, call_action, (void*) act);
+  }
+
+  for (int i = 0; i < n; i++) {
+    pthread_join(threads[i], NULL);
   }
 }
