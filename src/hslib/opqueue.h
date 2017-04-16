cdd3b8613720e8e51188fdbd621e95b0b0c48ead
Tej Chajed <tchajed@mit.edu>
2017-04-15 22:16:40 -0400

    Basic sketch of using an operation queue
    Idea is to avoid expensive upcalls into Haskell, instead using cheap
    "unsafe" downcalls into C to get operations, compute them in Haskell,
    and report their results, using many worker threads in Haskell.
    Currently only handles stat requests and seems a bit buggy. Lookups are
    required for a complete performance tests, since they are used even for
    stat() system calls.

---
+++ src/hslib/opqueue.h
@@ -0,0 +1,20 @@
+#ifndef OPQUEUE_H
+#define OPQUEUE_H
+#include <sys/stat.h>
+#include <pthread.h>
+
+typedef struct {
+  char *path;
+  struct stat *attr;
+  pthread_mutex_t m;
+  pthread_cond_t cond;
+  int err;
+} operation;
+
+operation* get_op();
+
+void send_result(operation *op, int err);
+
+int execute(operation *op);
+
+#endif
