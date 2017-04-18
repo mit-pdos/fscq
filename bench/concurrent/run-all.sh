cde76a2c3acd72e7c4de8a439639920efe1c1994
Tej Chajed <tchajed@mit.edu>
2017-04-18 14:47:38 -0400

    Customize run-all for jerry

--- bench/concurrent/run-all.sh
+++ bench/concurrent/run-all.sh
@@ -16,22 +16,23 @@ cache2=false
 kernelcache=false
 op=stat
 
+clientcpus_split='16/16/16/16/17/17/17/17/18/18/18/18/19/19/19/19'
+
 #for fs in fscq cfscq hfuse c-hello fusexmp native; do
 for iters in 100 300; do
   for fs in fscq cfscq native; do
     info "benchmarking $op on $fs over $iters iters"
     for disjointdirs in "false"; do
       for exists in "true"; do
-        for clientcpu in "0/0/0/0" "4/4/5/5"; do
+        for clientcpu in "$clientcpus_split"; do
           for parallel in 1 2 3 4; do
-            #for rtsopts in "-N2 -qg -A1G -I0" "-N2 -A1G -I0" "-N2 -qg -I0" "-N2 -I0"; do
-            for rtsopts in "-N4 -qg" "-N4 -qg -A2G -I0"; do
+            for rtsopts in "-N20 -qg -I0"; do
               reps="5"
               if [ "$fs" = "native" ]; then
                 reps="100"
               fi
               servercpu="0-$(($parallel-1))"
-              fsbench -work_iters=20 -reps=$reps -iters=$iters -rts-opts="$rtsopts" -server-cpu="$servercpu" -client-cpus=$clientcpu -op=$op -disjoint-dirs=$disjointdirs -exists=$exists -parallel=$parallel -attr-cache=$cache1 -name-cache=$cache1 -neg-cache=$cache2 -kernel-cache=$kernelcache -warmup=t $fs
+              perflock fsbench -work_iters=20 -reps=$reps -iters=$iters -rts-opts="$rtsopts" -server-cpu="$servercpu" -client-cpus=$clientcpu -op=$op -disjoint-dirs=$disjointdirs -exists=$exists -parallel=$parallel -attr-cache=$cache1 -name-cache=$cache1 -neg-cache=$cache2 -kernel-cache=$kernelcache -warmup=t $fs
             done
           done
         done
