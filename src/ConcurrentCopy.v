ae0985702697fc1fdbb670c996778fe150450b1e
Tej Chajed <tchajed@mit.edu>
2017-04-18 10:11:18 -0400

    Implement and prove copy_block operation
    Just need to combine copy_attr and copy_block proofs

--- src/ConcurrentCopy.v
+++ src/ConcurrentCopy.v
@@ -17,7 +17,7 @@ Section ConcurrentCopy.
       | TryAgain => Ret TryAgain (* will not happen *)
       end.
 
-  Definition copy inum dnum dstname :=
+  Definition copy_attrs inum dnum dstname :=
     bind (file_get_attr P inum)
          (fun '(attr, _) =>
             bind (create P dnum dstname)
@@ -33,9 +33,35 @@ Section ConcurrentCopy.
                     | Errno.Err _ => Ret (Done None)
                     end)).
 
+  Definition copy_block inum inum' :=
+    bind (read_fblock P inum 0)
+         (fun '(b, _) =>
+            bind (file_truncate P inum' 1)
+                 (fun '(r, _) =>
+                    match r with
+                    | Errno.OK _ =>
+                      bind (update_fblock_d P inum' 0 b)
+                           (fun _ => Ret (Done true))
+                    | Errno.Err _ => Ret (Done false)
+                    end)).
+
+  Definition copy inum dnum dstname :=
+    bind (copy_attrs inum dnum dstname)
+         (fun r => match r with
+                | Some inum' =>
+                  bind (copy_block inum inum')
+                       (fun r => if r
+                              then Ret (Done (Some inum'))
+                              else Ret (Done None))
+                | None => Ret (Done None)
+                end).
+
   Hint Extern 1 {{ file_get_attr _ _; _ }} => apply file_get_attr_ok : prog.
-  Hint Extern 1 {{ file_set_attr _ _ _; _ }} => apply file_set_attr_ok : prog.
   Hint Extern 1 {{ create _ _ _; _ }} => apply create_ok : prog.
+  Hint Extern 1 {{ file_set_attr _ _ _; _ }} => apply file_set_attr_ok : prog.
+  Hint Extern 1 {{ read_fblock _ _ _; _ }} => apply read_fblock_ok : prog.
+  Hint Extern 1 {{ file_truncate _ _ _; _ }} => apply file_truncate_ok : prog.
+  Hint Extern 1 {{ update_fblock_d _ _ _ _; _ }} => apply update_fblock_d_ok : prog.
 
   Ltac finish := repeat match goal with
                         | [ |- _ /\ _ ] => split; trivial
@@ -76,7 +102,7 @@ Section ConcurrentCopy.
     eauto.
   Qed.
 
-  Theorem copy_ok : forall inum dnum dstname tid,
+  Theorem copy_attrs_ok : forall inum dnum dstname tid,
       cprog_spec (fs_guarantee P) tid
                  (fun '(tree, homedirs, homedir, fpath, dpath, f, dents) sigma =>
                     {| precondition :=
@@ -104,9 +130,9 @@ Section ConcurrentCopy.
                              | TryAgain => False
                              | SyscallFailed => True
                              end |})
-                 (copy inum dnum dstname).
+                 (copy_attrs inum dnum dstname).
   Proof.
-    unfold copy, bind.
+    unfold copy_attrs, bind.
     step; finish.
     destruct r; destruct_goal_matches; try (step; finish).
 
@@ -122,6 +148,75 @@ Section ConcurrentCopy.
 
     eapply DirTreeNames.find_subtree_tree_names_distinct; eauto.
     eapply fs_invariant_tree_names_distinct; eauto.
+
+    Grab Existential Variables.
+    all: auto.
+  Qed.
+
+  Lemma zero_block_extend : forall V (v:V),
+      (emp * 0 |-> v)%pred (GenSepN.list2nmem (v::nil)%list).
+  Proof.
+    intros.
+    apply sep_star_comm.
+    apply (GenSepN.list2nmem_array (v::nil)).
+  Qed.
+
+  Theorem copy_block_ok : forall inum inum' tid,
+      cprog_spec (fs_guarantee P) tid
+                 (fun '(tree, homedirs, homedir, fpath, f, b0, fpath', f') sigma =>
+                    {| precondition :=
+                         fs_inv(P, sigma, tree, homedirs) /\
+                         local_l tid (Sigma.l sigma) = Unacquired /\
+                         homedir_disjoint homedirs tid /\
+                         find_subtree (homedirs tid) tree = Some homedir /\
+                         find_subtree fpath homedir = Some (TreeFile inum f) /\
+                         (0 |-> (b0, nil))%pred (GenSepN.list2nmem (DFData f)) /\
+                         ~DirTreePath.pathname_prefix fpath fpath' /\
+                         ~DirTreePath.pathname_prefix fpath' fpath /\
+                         find_subtree fpath' homedir = Some (TreeFile inum' f') /\
+                         DFData f' = nil;
+                       postcondition :=
+                         fun sigma' r =>
+                           exists tree',
+                             fs_inv(P, sigma', tree', homedirs) /\
+                             local_l tid (Sigma.l sigma') = Unacquired /\
+                             match r with
+                             | Done r =>
+                               match r with
+                               | true =>
+                                 exists data bs,
+                                 (0 |-> (b0, bs))%pred (GenSepN.list2nmem data) /\
+                                 let f' := mk_dirfile data (DFAttr f') in
+                                 let homedir' := update_subtree fpath' (TreeFile inum' f') homedir in
+                                 find_subtree (homedirs tid) tree' = Some homedir'
+                               | false => True
+                               end
+                             | TryAgain => False
+                             | SyscallFailed => True
+                             end |})
+                 (copy_block inum inum').
+  Proof.
+    unfold copy_block, bind.
+    step; finish.
+    SepAuto.pred_apply; SepAuto.cancel.
+    destruct r; destruct_goal_matches; try (step; finish).
+
+    destruct r; destruct_goal_matches; try (step; finish).
+    simpl.
+    replace (DFData f'); simpl.
+    unfold ListUtils.setlen; simpl.
+    apply zero_block_extend.
+
+    destruct r; destruct_goal_matches; try step; (try solve [ finish ]).
+    split; trivial.
+    descend; intuition eauto.
+    exists (DFData f'0).
+    finish.
+    SepAuto.pred_apply; SepAuto.cancel.
+    replace (find_subtree (homedirs tid) tree'1).
+    erewrite update_update_subtree_same; eauto.
+    destruct f'0; simpl in *; subst.
+    auto.
 
     Grab Existential Variables.
     all: auto.
