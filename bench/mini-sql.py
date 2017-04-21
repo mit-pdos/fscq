4729543fc8f11ee016e137d5dcf5bf0b82ed0c71
Alex Konradi <akonradi@mit.edu>
2017-04-21 10:37:05 -0400

    use multiple transactions in sqlite benchmark

--- bench/mini-sql.py
+++ bench/mini-sql.py
@@ -5,6 +5,8 @@
 
 scale = 400
 
+print ".bail ON"
+
 print "create table x (a int, b string);"
 print "create index i on x (a);"
 print "create index j on x (b);"
@@ -13,6 +15,8 @@ print "begin transaction;"
 for i in range(0, 10 * scale):
     print "insert into x (a, b) values ({0}, '{1}');".format(
         i, "foo {0} bar {0} bench {0} mark {0}".format(i) * 10)
+    if i % scale == 0:
+        print "end transaction; begin transaction;"
 print "end transaction;"
 
 for i in range(0, scale):
