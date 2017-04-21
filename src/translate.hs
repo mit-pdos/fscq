5fe1e5b40d1c0280d32205e5498072faef79bf89
Tej Chajed <tchajed@mit.edu>
2017-04-21 13:06:08 -0400

    Make translate output better

--- src/translate.hs
+++ src/translate.hs
@@ -10,15 +10,21 @@ import System.CPUTime.Rdtsc
 timeAction :: IO a -> IO Int
 timeAction io = do
   t1 <- rdtsc
-  _ <- io
+  r <- io
   t2 <- rdtsc
-  return (fromIntegral (t2-t1))
+  return $ r `seq` (fromIntegral (t2-t1))
+
+iterations :: Int
+iterations = 10000000
+
+timePerIter :: Int -> Float
+timePerIter n = (fromIntegral n) / (fromIntegral iterations)
 
 main :: IO ()
 main = do
   ds <- init_disk "/dev/null"
   cs <- I.newState ds
-  t <- timeAction $ replicateM_ 10000 $ I.run cs (add_tuple_concur ((5,6),7))
-  t' <- timeAction $ replicateM_ 10000 $ I.run cs (add_tuple_concur_raw ((5,6),7))
-  putStrLn $ "translate " ++ show t
-  putStrLn $ "raw       " ++ show t'
+  t <- timeAction $ replicateM_ iterations  $ I.run cs (add_tuple_concur ((5,6),7))
+  t' <- timeAction $ replicateM_ iterations $ I.run cs (add_tuple_concur_raw ((5,6),7))
+  putStrLn $ "translate " ++ show (timePerIter t) ++ " cycles/op"
+  putStrLn $ "raw       " ++ show (timePerIter t') ++ " cycles/op"
