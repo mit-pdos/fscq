ac4ad113ce460117a8d3e98500508cb3fd878306
Atalay Ileri <atalay@mit.edu>
2017-08-09 11:49:12 -0400

    Prove up to read_secure

--- src/SecurityCache.v
+++ src/SecurityCache.v
@@ -26,7 +26,6 @@ Lemma mem_disjoint_ptsto_mem_union_eq:
 Proof.
   intros; extensionality x.
   destruct (AEQ a x); subst.
-  Search mem_disjoint None.
   rewrite mem_disjoint_comm in H.
   rewrite mem_disjoint_comm in H0.
   erewrite mem_disjoint_either; eauto.
@@ -34,7 +33,6 @@ Proof.
   eapply ptsto_valid'; pred_apply; cancel.
   eapply ptsto_valid'; pred_apply; cancel.
   
-  Search mem_union mem_disjoint.
   erewrite <- mem_union_sel_l; eauto.
   symmetry; erewrite <- mem_union_sel_l; eauto.
   rewrite H3; auto.
@@ -51,18 +49,18 @@ Theorem writeback_secure:
   (fun m m' => (rep cs d * [[ (F * diskIs m')%pred m]])%pred m)
   (fun m m' => (exists cs', rep cs' d * [[ (F * diskIs m')%pred m]])%pred m)
   (writeback a cs) (a|-> vs) (match Map.find a (CSMap cs) with
-                                              | Some (v, true) => a|->(v, vsmerge vs)
-                                              | _ => a|->vs
-                                              end).
+                              | Some (v, true) => a|->(v, vsmerge vs)
+                              | _ => a|->vs
+                              end).
 Proof.
   unfold writeback; intuition.
   destruct (Map.find a (CSMap cs)) eqn:D; eauto.
   
   Focus 2.
-      eapply world_impl_secure; eauto.
-      intros. apply H.
-      simpl; intros.
-      eexists; eauto.
+    eapply world_impl_secure; eauto.
+    intros. apply H.
+    simpl; intros.
+    eexists; eauto.
 
   destruct p; eauto.
   destruct b0; eauto.
@@ -75,8 +73,7 @@ Proof.
   
 
   eapply bind_secure; intros; eauto.
-  -
-    eapply world_impl_secure. 
+  - eapply world_impl_secure. 
     3: eauto.
     
     + instantiate (1:= (fun m => ([[ rep cs d (insert m a (a0,b))]] * F)%pred m)).
@@ -152,7 +149,6 @@ Proof.
         apply sep_star_comm; apply sep_star_lift_apply'; auto.
         eapply mem_pred_cachepred_add_absorb_clean; eauto.
         apply sep_star_mem_union; eauto.
-        Search mem_except ptsto.
         apply sep_star_comm in H5; apply ptsto_mem_except in H5.
         rewrite mem_except_insert in H5.
         rewrite mem_except_none with (m:= m4) in H5; auto.
@@ -173,3 +169,181 @@ Proof.
         eapply addr_valid_add; eauto.
         apply sep_star_mem_union; eauto.
 Qed.
+
+Hint Resolve writeback_secure.
+
+(* Definition evict a (cs : cachestate) :=
+    cs <- writeback a cs;
+    match Map.find a (CSMap cs) with
+    | Some _ =>
+      Ret (mk_cs (Map.remove a (CSMap cs)) (CSMaxCount cs) (CSCount cs - 1) (CSEvict cs))
+    | None =>
+      Ret (mk_cs (Map.remove a (CSMap cs)) (CSMaxCount cs) (CSCount cs) (CSEvict cs))
+    end.
+*)
+
+Theorem evict_secure:
+  forall a cs d F vs,
+  @prog_secure _ addr_eq_dec _ _ 
+  (fun m m' => (rep cs d * [[ (F * diskIs m')%pred m]])%pred m)
+  (fun m m' => (exists cs', rep cs' d * [[ (F * diskIs m')%pred m]])%pred m)
+  (evict a cs) (a|-> vs) (match Map.find a (CSMap cs) with
+                          | Some (v, true) => a|->(v, vsmerge vs)
+                          | _ => a|->vs
+                          end).
+Proof.
+  unfold evict; intros.
+  eapply bind_secure; eauto; intros.
+  destruct (Map.find a (CSMap x)); eauto.
+Qed.
+Hint Resolve evict_secure.
+
+(* Definition maybe_evict (cs : cachestate) :=
+    If (lt_dec (CSCount cs) (CSMaxCount cs)) {
+      Ret cs
+    } else {
+      let (victim, evictor) := eviction_choose (CSEvict cs) in
+      match (Map.find victim (CSMap cs)) with
+      | Some _ =>
+        cs <- evict victim (mk_cs (CSMap cs) (CSMaxCount cs) (CSCount cs) evictor);
+        Ret cs
+      | None => (* evictor failed, evict first block *)
+        match (Map.elements (CSMap cs)) with
+        | nil => Ret cs
+        | (a, v) :: tl =>
+          cs <- evict a cs;
+          Ret cs
+        end
+      end
+    }.
+    
+     (match find (fst (eviction_choose (CSEvict cs))) (CSMap cs) with
+       | Some _ => Singleton _ (fst (eviction_choose (CSEvict cs)))
+       | None =>  match elements (CSMap cs) with
+                         | nil => Empty_set _
+                         | (a, _) :: _ => Singleton _ a
+                         end
+       end). *)
+    
+Theorem maybe_evict_secure:
+  forall cs d F vs,
+  let (victim, _) := eviction_choose (CSEvict cs) in
+  @prog_secure _ addr_eq_dec _ _ 
+  (fun m m' => (rep cs d * [[ (F * diskIs m')%pred m]])%pred m)
+  (fun m m' => (exists cs', rep cs' d * [[ (F * diskIs m')%pred m]])%pred m)
+  (maybe_evict cs) 
+  (* pre*)
+  (match (Map.find victim (CSMap cs)) with
+  | Some _ => victim |-> vs
+  | None => (* evictor failed, evict first block *)
+    match (Map.elements (CSMap cs)) with
+    | nil => emp
+    | (a, _) :: _ => a|-> vs
+    end
+  end)
+  (* post*)
+  (if (lt_dec (CSCount cs) (CSMaxCount cs)) then
+       match (Map.find victim (CSMap cs)) with
+        | Some _ => victim |-> vs
+        | None => (* evictor failed, evict first block *)
+          match (Map.elements (CSMap cs)) with
+          | nil => emp
+          | (a, _) :: _ => a|-> vs
+          end
+        end
+  else
+      match (Map.find victim (CSMap cs)) with
+      | Some (v, true) => victim |-> (v, vsmerge vs)
+      | Some (v, false) => victim |-> vs
+      | None => (* evictor failed, evict first block *)
+        match (Map.elements (CSMap cs)) with
+        | nil => emp
+        | (a, _) :: _ => 
+          match Map.find a (CSMap cs) with
+          | Some (v, true) => a|->(v, vsmerge vs)
+          | _ => a|->vs
+          end
+        end
+      end).
+Proof.
+  unfold maybe_evict; intros.
+  destruct (lt_dec (CSCount cs) (CSMaxCount cs)); simpl; eauto.
+  - eapply world_impl_secure; eauto.
+    intros.
+    apply H.
+    intros.
+    eexists; apply H2.
+    
+  - destruct (Map.find 0 (CSMap cs)) eqn:D.
+    + destruct p.
+        replace ({|  CSMap := CSMap cs;
+                           CSMaxCount := CSMaxCount cs;
+                           CSCount := CSCount cs;
+                           CSEvict := CSEvict cs |}) with cs by (destruct cs; simpl; auto).
+        eapply bind_secure; eauto; intros.
+        rewrite D; eauto.
+    + destruct (Map.elements (CSMap cs)) eqn:D0; eauto.
+        * eapply world_impl_secure; eauto.
+           intros.
+           apply H.
+           intros.
+           eexists; apply H2.
+        * destruct p.
+           eapply bind_secure; eauto.
+Qed.
+
+
+
+Definition read a (cs : cachestate) :=
+  cs <- maybe_evict cs;
+  match Map.find a (CSMap cs) with
+  | Some (v, dirty) => Ret ^(cs, v)
+  | None =>
+    AlertModified;;
+    v <- Read a;
+    Ret ^(mk_cs (Map.add a (v, false) (CSMap cs))
+               (CSMaxCount cs) (CSCount cs + 1) (eviction_update (CSEvict cs) a), v)
+  end.
+
+Definition write a v (cs : cachestate) :=
+  cs <- maybe_evict cs;
+  match Map.find a (CSMap cs) with
+  | Some _ =>
+    Ret (mk_cs (Map.add a (v, true) (CSMap cs))
+               (CSMaxCount cs) (CSCount cs) (eviction_update (CSEvict cs) a))
+  | None =>
+    Ret (mk_cs (Map.add a (v, true) (CSMap cs))
+               (CSMaxCount cs) (CSCount cs + 1) (eviction_update (CSEvict cs) a))
+  end.
+
+Definition begin_sync (cs : cachestate) :=
+  Ret cs.
+
+Definition sync a (cs : cachestate) :=
+  cs <- writeback a cs;
+  Ret cs.
+
+Definition end_sync (cs : cachestate) :=
+  Sync;;
+  Ret cs.
+
+
+Definition cache0 sz := mk_cs (Map.empty _) sz 0 eviction_init.
+
+Definition init (cachesize : nat) :=
+  Sync;;
+  Ret (cache0 cachesize).
+
+Definition read_array a i cs :=
+  r <- read (a + i) cs;
+  Ret r.
+
+Definition write_array a i v cs :=
+  cs <- write (a + i) v cs;
+  Ret cs.
+
+Definition sync_array a i cs :=
+  cs <- sync (a + i) cs;
+  Ret cs.
+
+
