e8a6972eca64b6c4c35799ce1aa7a4633f036e57
Tej Chajed <tchajed@mit.edu>
2017-12-19 10:48:06 -0500

    Add scripts to run many benchmarks

---
+++ bench/concurrent/haskell/bench-basics.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+info() {
+    echo -e "\e[34m$1\e[0m"
+}
+
+sep() {
+    echo ""
+}
+
+bench() {
+    bencher --RTS +RTS -N4 -qg -RTS "$@"
+}
+
+info "==> metadata-only"
+info "statfs"
+bench --target-ms=1000 --reps=1000 statfs
+sep
+
+info "stat"
+bench --target-ms=1000 --reps=1000 stat
+sep
+
+info "traverse large file directory"
+bench --target-ms=1000 --reps=5 traverse-dir --dir '/large-dir'
+sep
+
+info "traverse small file directory"
+bench --target-ms=1000 --reps=5 traverse-dir --dir '/large-dir-small-files'
+sep
+
+info "==> reading data"
+info "cat linux.tar.xz"
+bench --iters=1 --reps=4 cat-file --file '/linux-source/linux.tar.xz'
+sep
+
+info "cat large"
+bench --iters=1 --reps=2 cat-file --file '/large'
+sep
+
+info "cat large file directory"
+bench --iters=1 --reps=2 cat-dir --dir '/large-dir'
+sep
+
+info "cat small file directory"
+bench --target-ms=1000 --reps=5 cat-dir --dir '/large-dir-small-files'
---
+++ bench/concurrent/haskell/bench-io-concur.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+info() {
+    echo -e "\e[34m$1\e[0m"
+}
+
+sep() {
+    echo ""
+}
+
+summary() {
+    awk '{print $4 " " $5 " " $9/1000 " ms"}'
+}
+
+info "no warmup large read"
+parbench +RTS -N1 -qg -RTS --warmup=false --iters=1 \
+         cat-file --file '/large' --fscq=true | summary
+parbench +RTS -N1 -qg -RTS --warmup=false --iters=1 \
+         cat-file --file '/large' --fscq=false | summary
+
+info "small reads"
+bench --warmup=false --reps=25000 cat-file --file '/small' --fscq=true \
+    | summary
+bench --warmup=false --reps=25000 cat-file --file '/small' --fscq=false \
+    | summary
+sep
+
+ioconcur() {
+    parbench +RTS -qg -RTS io-concur --reps=25000 "$@" --fscq=true | summary
+    parbench +RTS -qg -RTS io-concur --reps=25000 "$@" --fscq=false | summary
+}
+
+info "sequential"
+ioconcur +RTS -N1 -RTS --n=1
+info "N=1"
+ioconcur +RTS -N1 -RTS --n=2
+info "N=2"
+ioconcur +RTS -N2 -RTS --n=2
