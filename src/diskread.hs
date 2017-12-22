fc0b9d8b58b411e04d229b88a11fa9a10e99544d
Tej Chajed <tchajed@mit.edu>
2017-12-22 13:52:35 -0500

    Add option to use CPU in diskread
    Benchmarks confirm that CPU and I/O are effectively parallelized due to
    kernel doing extra I/O for short preads.

--- src/diskread.hs
+++ src/diskread.hs
@@ -4,14 +4,19 @@ module Main where
 import Control.Monad
 import Disk
 import Foreign.Marshal.Alloc
+import Control.Exception (evaluate)
 import Options
 import UnixIO
+
+data ReadType = ReadDisk | Simple | CpuIntensive
+  deriving (Bounded, Enum, Show)
 
 data MainOptions = MainOptions
   { optDiskImg :: FilePath
   , optTotalDataMb :: Int
   , optSkipSizeKb :: Int
-  , optSimpleRead :: Bool }
+  , optReadType :: ReadType
+  , optFibonacciArg :: Int }
 
 instance Options MainOptions where
   defineOptions = pure MainOptions
@@ -21,8 +26,12 @@ instance Options MainOptions where
         "total MB to read"
     <*> simpleOption "skip-kb" 4
         "read 4KB every this many KB"
-    <*> simpleOption "simple" False
-        "simplify reads to avoid buf2i"
+    <*> defineOption (optionType_enum "read type (ReadDisk|Simple|CpuIntensive)") (\o -> o
+        { optionLongFlags = ["read-type"]
+        , optionDefault = Simple
+        })
+    <*> simpleOption "fibonacci" 16
+        "fibonacci argument for CpuIntensive read"
 
 readOffsets :: MainOptions -> [Int]
 readOffsets MainOptions{..} =
@@ -41,13 +50,28 @@ simple_read (S fd _ _ _) a = do
     if cc == 4096 then return ()
     else error $ "simple_read: short read: " ++ show cc ++ " @ " ++ show a
 
+fibonacci :: Int -> Int
+fibonacci 0 = 1
+fibonacci 1 = 1
+fibonacci n = fibonacci (n-1) + fibonacci (n-2)
+
+cpu_read :: Int -> DiskState -> Int -> IO ()
+cpu_read fibArg (S fd _ _ _) a = do
+  allocaBytes 4096 $ \buf -> do
+    cc <- pread fd buf 4096 (fromIntegral $ 4096*a)
+    if cc == 4096 then do
+      _ <- evaluate (fibonacci fibArg)
+      return ()
+    else error $ "simple_read: short read: " ++ show cc ++ " @ " ++ show a
+
 mainCmd :: MainOptions -> [String] -> IO ()
 mainCmd opts@MainOptions{..} _ = do
   ds <- init_disk optDiskImg
   let offsets = readOffsets opts in
-    if optSimpleRead
-    then forM_ offsets $ simple_read ds
-    else forM_ offsets $ read_disk ds . fromIntegral
+    case optReadType of
+      Simple -> forM_ offsets $ simple_read ds
+      ReadDisk -> forM_ offsets $ read_disk ds . fromIntegral
+      CpuIntensive -> forM_ offsets $ cpu_read optFibonacciArg ds
 
 main :: IO ()
 main = runCommand mainCmd
