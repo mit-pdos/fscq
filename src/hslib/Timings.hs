4979a8cc2f242683e2cbf0168c0b4095677a617b
Tej Chajed <tchajed@mit.edu>
2018-01-11 13:48:54 -0500

    Track only sum and count for debug

--- src/hslib/Timings.hs
+++ src/hslib/Timings.hs
@@ -1,22 +1,35 @@
-module Timings where
+module Timings
+  (
+    Timings
+  , insertTime
+  , combineTimes
+  , emptyTimings
+  , printTimings
+  ) where
 
 import           Control.Monad.State
 import qualified Data.Map.Strict as Map
+import           Data.Monoid
 import           System.IO (hPutStrLn, stderr)
 
-type Timings = Map.Map String [Integer]
+data Aggregate = Aggregate Integer Int
+
+type Timings = Map.Map String Aggregate
+
+instance Monoid Aggregate where
+  mempty = Aggregate 0 0
+  mappend (Aggregate s1 c1) (Aggregate s2 c2) = Aggregate (s1+s2) (c1+c2)
 
 insertTime :: String -> Integer -> Timings -> Timings
-insertTime s n = Map.insertWith (++) s [n]
+insertTime s n = Map.insertWith (<>) s (Aggregate n 1)
 
 combineTimes :: Timings -> Timings -> Timings
-combineTimes = Map.unionWith (++)
+combineTimes = Map.unionWith (<>)
 
 emptyTimings :: Timings
 emptyTimings = Map.empty
 
 printTimings :: Timings -> IO ()
 printTimings tm =
-  forM_ (Map.assocs tm) $ \(s, ns) ->
-    forM_ ns $ \n ->
-      hPutStrLn stderr $ "debug: " ++ s ++ " " ++ show n
+  forM_ (Map.assocs tm) $ \(name, Aggregate s c) ->
+    hPutStrLn stderr $ name ++ " " ++ show c ++ " -> " ++ show (fromIntegral s/3333000::Double)
