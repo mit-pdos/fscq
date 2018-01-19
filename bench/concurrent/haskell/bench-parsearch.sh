7a18ec79bc012d142d2c3465069602986f385f9a
Tej Chajed <tchajed@mit.edu>
2018-01-19 17:17:39 -0500

    Use SSD for non-warmed up parsearch

--- bench/concurrent/haskell/bench-parsearch.sh
+++ bench/concurrent/haskell/bench-parsearch.sh
@@ -7,8 +7,8 @@ summary() {
 suite() {
     n="$1"
     shift
-    taskset -c 0-$((n-1)) parbench par-search --img=/tmp/disk.img --reps=1 --fscq=true --n=$n "$@" | summary
-    taskset -c 0-$((n-1)) parbench par-search --img=/tmp/disk.img --reps=1 --fscq=false --n=$n "$@" | summary
+    taskset -c 0-$((n-1)) parbench par-search --reps=1 --fscq=true --n=$n "$@" | summary
+    taskset -c 0-$((n-1)) parbench par-search --reps=1 --fscq=false --n=$n "$@" | summary
 }
 
 info() {
@@ -19,31 +19,32 @@ sep() {
     echo ""
 }
 
+## Linux search (extremely slow)
 #info "==> par-search"
 #suite 1 +RTS -qg -RTS "$@"
 #suite 2 +RTS -qg -RTS "$@"
 #suite 4 +RTS -qg -RTS "$@"
 #sep
-#
+
 #info "==> par-search (parallel GC)"
 #suite 1 "$@"
 #suite 2 "$@"
 #suite 4 "$@"
 #sep
-#
+
 info "==> par-search coq"
-suite 1 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
-suite 2 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
-suite 4 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 1 +RTS -qg -RTS --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 2 +RTS -qg -RTS --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 4 +RTS -qg -RTS --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
 sep
 
 info "==> par-search coq (parallel GC)"
-suite 1 --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
-suite 2 --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
-suite 4 --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 1 --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 2 --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
+suite 4 --img=/tmp/disk.img --dir '/search-benchmarks/coq' --query 'dependency graph' "$@"
 sep
 
 info "==> par-search coq (no warmup)"
-suite 1 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
-suite 2 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
-suite 4 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
+suite 1 --img=/dev/sdg1 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
+suite 2 --img=/dev/sdg1 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
+suite 4 --img=/dev/sdg1 +RTS -qg -RTS --dir '/search-benchmarks/coq' --query 'dependency graph' --warmup=false "$@"
