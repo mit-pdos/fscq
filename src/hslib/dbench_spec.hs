60cf15eef7891d4867be130209cb2bed88e42618
Tej Chajed <tchajed@mit.edu>
2018-02-20 14:55:19 -0500

    Fix dbench tests to reflect forward slash parsing

--- src/hslib/dbench_spec.hs
+++ src/hslib/dbench_spec.hs
@@ -46,24 +46,24 @@ main = hspec $ do
     it "should parse normal paths" $ do
       (path, "\"/foo\"") `shouldParseTo` "/foo"
     it "should parse paths with forward slashes" $ do
-      (path, "\"\\foo\\bar\"") `shouldParseTo` "\\foo\\bar"
+      (path, "\"\\foo\\bar\"") `shouldParseTo` "/foo/bar"
   describe "command parser" $ do
     it "should parse common operations" $ do
       (command, "NTCreateX \"\\clients\\client1\" 0x1 0x2 16385 NT_STATUS_OK") `shouldParseTo`
-        CreateX "\\clients\\client1" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 16385) StatusOk
+        CreateX "/clients/client1" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 16385) StatusOk
       (command, "WriteX 10002 1835008 32768 32768 NT_STATUS_OK") `shouldParseTo`
         WriteX (Handle 10002) 1835008 32768 32768 StatusOk
       (command, "ReadX 10003 0 4096 4096 NT_STATUS_OK") `shouldParseTo`
         ReadX (Handle 10003) 0 4096 4096 StatusOk
       (command, "QUERY_PATH_INFORMATION \"\\PPTB1E4.TMP\" 1004 NT_STATUS_OK") `shouldParseTo`
-        QueryPath "\\PPTB1E4.TMP" 1004 StatusOk
+        QueryPath "/PPTB1E4.TMP" 1004 StatusOk
     it "should parse other operations" $ do
       (command, "Close 10003 NT_STATUS_OK") `shouldParseTo`
         Close (Handle 10003) StatusOk
       (command, "FIND_FIRST \"\\clients\\client1\\~dmtmp\\<.JNK\" 260 1366 0 NT_STATUS_NO_SUCH_FILE") `shouldParseTo`
         FindFirst (Pattern "\\clients\\client1\\~dmtmp\\<.JNK") 260 1366 0 StatusNoSuchFile
       (command, "Unlink \"\\clients\\client1\\~dmtmp\\PWRPNT\\NEWPCB.PPT\" 0x6 NT_STATUS_OK") `shouldParseTo`
-        Unlink "\\clients\\client1\\~dmtmp\\PWRPNT\\NEWPCB.PPT" 0x6 StatusOk
+        Unlink "/clients/client1/~dmtmp/PWRPNT/NEWPCB.PPT" 0x6 StatusOk
       (command, "QUERY_FS_INFORMATION 259 NT_STATUS_OK") `shouldParseTo`
         QueryFilesystem 259 StatusOk
       (command, "QUERY_FILE_INFORMATION 9938 258 NT_STATUS_OK") `shouldParseTo`
@@ -73,15 +73,15 @@ main = hspec $ do
       (command, "Flush 10024 NT_STATUS_OK") `shouldParseTo`
         Flush (Handle 10024) StatusOk
       (command, "Rename \"\\clients\\client1\\~dmtmp\\PWRPNT\\NEWPCB.PPT\" \"\\clients\\client1\\~dmtmp\\PWRPNT\\PPTC112.TMP\" NT_STATUS_OK") `shouldParseTo`
-        Rename "\\clients\\client1\\~dmtmp\\PWRPNT\\NEWPCB.PPT" "\\clients\\client1\\~dmtmp\\PWRPNT\\PPTC112.TMP" StatusOk
+        Rename "/clients/client1/~dmtmp/PWRPNT/NEWPCB.PPT" "/clients/client1/~dmtmp/PWRPNT/PPTC112.TMP" StatusOk
       (command, "LockX 10430 2147483538 1 NT_STATUS_OK") `shouldParseTo`
         LockX (Handle 10430) 2147483538 1 StatusOk
       (command, "UnlockX 10430 2147483538 1 NT_STATUS_OK") `shouldParseTo`
         UnlockX (Handle 10430) 2147483538 1 StatusOk
       (command, "Mkdir \"\\clients\" NT_STATUS_OK") `shouldParseTo`
-        Mkdir "\\clients" StatusOk
+        Mkdir "/clients" StatusOk
       (command, "Deltree \"\\clients\\client1\" NT_STATUS_OK") `shouldParseTo`
-        Deltree "\\clients\\client1" StatusOk
+        Deltree "/clients/client1" StatusOk
     it "should handle whitespace" $ do
       (command, "Close  10003  \tNT_STATUS_OK") `shouldParseTo`
         Close (Handle 10003) StatusOk
@@ -92,12 +92,12 @@ main = hspec $ do
       (script, T.unlines
         ["Mkdir \"\\clients\" NT_STATUS_OK"
         , "NTCreateX \"\\clients\\f\" 0x1 0x2 12 NT_STATUS_OK"]) `shouldParseTo`
-        [Mkdir "\\clients" StatusOk
-        , CreateX "\\clients\\f" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 12) StatusOk]
+        [Mkdir "/clients" StatusOk
+        , CreateX "/clients/f" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 12) StatusOk]
     it "should skip comments" $ do
       (script, T.unlines
         ["Mkdir \"\\clients\" NT_STATUS_OK"
         , "# this is just a comment"
         , "NTCreateX \"\\clients\\f\" 0x1 0x2 12 NT_STATUS_OK"]) `shouldParseTo`
-        [Mkdir "\\clients" StatusOk
-        , CreateX "\\clients\\f" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 12) StatusOk]
+        [Mkdir "/clients" StatusOk
+        , CreateX "/clients/f" (CreateOptions 0x1) (CreateDisposition 0x2) (Handle 12) StatusOk]
