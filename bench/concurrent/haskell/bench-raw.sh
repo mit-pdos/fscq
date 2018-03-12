169864e0c0b1ee01667b2ad366f9845c91484e0f
Tej Chajed <tchajed@mit.edu>
2018-03-12 14:31:00 -0400

    Re-organize bench-raw.sh

--- bench/concurrent/haskell/bench-raw.sh
+++ bench/concurrent/haskell/bench-raw.sh
@@ -17,7 +17,7 @@ runbasic() {
     shift
 
     #echo taskset -c 0-5 parbench "$@"
-    taskset -c 0-5 parbench "$@" | addfield "$desc"
+    parbench "$@" | addfield "$desc"
 }
 
 run() {
@@ -26,10 +26,9 @@ run() {
     local par="$1"
     shift
 
-    runbasic "$desc" --n=$par +RTS -N$par -RTS "$@"
+    runbasic "$desc" --n=$par +RTS -qa -N$par -RTS "$@"
 }
 
-parbench print-header | addfield "description"
 
 info_system() {
     info "> $system"
@@ -58,87 +57,110 @@ parse_disk() {
     esac
 }
 
-info "syscall baseline"
-for system in fscq cfscq; do
-    info_system
-    for par in $(seq 1 6); do
-        info "  > n=$par"
-        args=( $par "--img=/tmp/disk.img" "--target-ms=500" "--fscq=$is_fscq" )
-        run ""            "${args[@]}" statfs --reps=1000
-        run ""            "${args[@]}" stat   --reps=10
-        run ""            "${args[@]}" open   --reps=10
-        run "oob"         "${args[@]}" read   --reps=10 --file="/small" --offset 10000
-        run "off-0"       "${args[@]}" read   --reps=10 --file="/small" --offset 0
-        run "large"       "${args[@]}" cat-file     --file="/large"
-        run "large-dir"   "${args[@]}" traverse-dir --dir="/large-dir"
-        run "small files" "${args[@]}" cat-dir      --dir="/large-dir-small-files"
-        run ""            "${args[@]}" create
-        run ""            "${args[@]}" create-dir
-        run ""            "${args[@]}" write
-    done
-done
-sep
+# benchmarks
 
-info "I/O concurrency"
-for system in fscq cfscq; do
-    info_system
-    for par in 1 2; do
-        for capabilities in 1 2; do
-            for disk in "mem" "ssd"; do
-                parse_disk
-                runbasic "$disk" +RTS -N$capabilities -RTS \
-                         --n=$par --fscq=$is_fscq --img=$img \
-                         io-concur --reps=25000
-            done
-        done
-    done
-done
-sep
+syscalls() {
+	info "syscall baseline"
+	for system in fscq cfscq; do
+		info_system
+		for par in $(seq 1 12); do
+			info "  > n=$par"
+			args=( $par "--img=/tmp/disk.img" "--target-ms=500" "--fscq=$is_fscq" )
+			run ""            "${args[@]}" statfs --reps=1000
+			run ""            "${args[@]}" stat   --reps=10
+			run ""            "${args[@]}" open   --reps=10
+			run "oob"         "${args[@]}" read   --reps=10 --file="/small" --offset 10000
+			run "off-0"       "${args[@]}" read   --reps=10 --file="/small" --offset 0
+			run "large"       "${args[@]}" cat-file     --file="/large"
+			run "large-dir"   "${args[@]}" traverse-dir --dir="/large-dir"
+			run "small files" "${args[@]}" cat-dir      --dir="/large-dir-small-files"
+			run ""            "${args[@]}" create
+			run ""            "${args[@]}" create-dir
+			run ""            "${args[@]}" write
+		done
+	done
+	sep
+}
 
-info "dbench"
-for system in fscq cfscq; do
-    info_system
-    for disk in "mem" "ssd"; do
-        parse_disk
-        run "$disk" 1 --img="$img" --fscq=$is_fscq \
-            dbench --script $HOME/dbench/loadfiles/client.txt
-    done
-done
-sep
+io_concur() {
+	info "I/O concurrency"
+	for system in fscq cfscq; do
+		info_system
+		for par in 1 2; do
+			for capabilities in 1 2; do
+				for disk in "mem" "ssd" "hdd"; do
+					parse_disk
+					runbasic "$disk" +RTS -qa -N$capabilities -RTS \
+						--n=$par --fscq=$is_fscq --img=$img \
+						io-concur --reps=25000
+				done
+			done
+		done
+	done
+	sep
+}
 
-info "par-search"
-for system in fscq cfscq; do
-    info_system
-    for par in $(seq 1 6); do
-        run "warmup" $par --img=/tmp/disk.img --fscq=$is_fscq \
-            par-search --dir '/search-benchmarks/coq' --query 'dependency graph'
-        run "mem" $par --img=/tmp/disk.img --fscq=$is_fscq --warmup=false \
-            par-search --dir '/search-benchmarks/coq' --query 'dependency graph'
-        run "ssd" $par --img=/dev/sdg1 --fscq=$is_fscq --warmup=false \
-            par-search --dir '/search-benchmarks/coq' --query 'dependency graph'
-    done
-done
-sep
 
-info "readers-writer"
-for par in $(seq 0 5); do
-    info "> n=$par"
-    args=( --n=$par +RTS -N$((par+1)) -RTS --img=/tmp/disk.img --fscq=false --iters=5000 )
-    runbasic "" "${args[@]}" readers-writer --reps=10 --write-reps=1
-    if [ $par -eq 1 ]; then
-        runbasic "only-reads" "${args[@]}" \
-                 +RTS -N1 -RTS \
-                 readers-writer --reps=10 --write-reps=1 --only-reads
-    fi
-done
-sep
+dbench() {
+	info "dbench"
+	for system in fscq cfscq; do
+		info_system
+		for disk in "mem" "ssd"; do
+			parse_disk
+			run "$disk" 1 --img="$img" --fscq=$is_fscq \
+				dbench --script $HOME/dbench/loadfiles/client.txt
+		done
+	done
+	sep
+}
 
-info "rw-mix"
-for par in $(seq 1 6); do
-    info "> n=$par"
-    args=( --n=$par +RTS -N$par -RTS --img=/tmp/disk.img --fscq=false --target-ms=1000 )
-    for mix in 0.95 0.9 0.8; do
-        runbasic "mix-$mix" "${args[@]}" \
-                 rw-mix --reps=10 --write-reps=1 --read-perc=$mix
-    done
-done
+parsearch() {
+	info "par-search"
+	for system in fscq cfscq; do
+		info_system
+		for par in $(seq 1 12); do
+			info "  > n=$par"
+			run "warmup" $par --img=/tmp/disk.img --fscq=$is_fscq \
+				par-search --dir '/search-benchmarks/coq' --query 'dependency graph'
+			run "mem" $par --img=/tmp/disk.img --fscq=$is_fscq --warmup=false \
+				par-search --dir '/search-benchmarks/coq' --query 'dependency graph'
+		done
+	done
+	sep
+}
+
+readers_writer() {
+	info "readers-writer"
+	for par in $(seq 0 11); do
+		info "> n=$par"
+		args=( --n=$par +RTS -qa -N$((par+1)) -RTS --img=/tmp/disk.img --fscq=false --iters=5000 )
+		runbasic "" "${args[@]}" readers-writer --reps=10 --write-reps=1
+		if [ $par -eq 1 ]; then
+			runbasic "only-reads" "${args[@]}" \
+				+RTS -N1 -RTS \
+				readers-writer --reps=10 --write-reps=1 --only-reads
+		fi
+	done
+	sep
+}
+
+rw_mix() {
+	info "rw-mix"
+	for par in $(seq 1 12); do
+		info "> n=$par"
+		args=( --n=$par +RTS -qa -N$par -RTS --img=/tmp/disk.img --fscq=false --target-ms=1000 )
+		for mix in 0.95 0.9 0.8; do
+			runbasic "mix-$mix" "${args[@]}" \
+				rw-mix --reps=10 --write-reps=1 --read-perc=$mix
+		done
+	done
+}
+
+parbench print-header | addfield "description"
+
+syscalls
+io_concur
+dbench
+parsearch
+readers_writer
+rw_mix
