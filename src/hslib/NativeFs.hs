4ab6cbf10e5857793b3f61d54014d7e582233470
Tej Chajed <tchajed@mit.edu>
2018-03-29 11:22:23 -0400

    Fix call to makeRelative

--- src/hslib/NativeFs.hs
+++ src/hslib/NativeFs.hs
@@ -21,7 +21,7 @@ import qualified Data.ByteString.Internal as B
 --import qualified Data.ByteString.Unsafe   as B
 
 join :: FilePath -> FilePath -> FilePath
-join p1 p2 = joinPath [p1, makeRelative p2]
+join p1 p2 = joinPath [p1, makeRelative "/" p2]
 
 -- TODO: catch exceptions and return them as errors
 toEither :: IO a -> IO (Either Errno a)
