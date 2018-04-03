b332272a3cae794d29cdaae7544e7b2b4239de06
Atalay Ileri <atalay@mit.edu>
2018-04-03 05:01:45 -0400

    More orp proofs.

--- src/ORP.v
+++ src/ORP.v
@@ -41,6 +41,18 @@ Notation MSICache := BFILE.MSICache.
 Notation MSAlloc := BFILE.MSAlloc.
 Notation MSDBlocks := BFILE.MSDBlocks.
 
+Lemma bind_orp:
+  forall T T' (p1: prog T) (p2: T -> prog T') pr d bm hm,
+    only_reads_permitted pr p1 d bm hm ->
+    (forall tr d' bm' hm' r tr',
+       exec pr tr d bm hm p1 (Finished d' bm' hm' r) tr' ->
+       only_reads_permitted pr (p2 r) d' bm' hm') ->
+    only_reads_permitted pr (Bind p1 p2) d bm hm.
+Proof.
+  simpl; intros.
+  split; eauto.
+Qed.
+
 
 Lemma LOG_begin_orp:
   forall pr a b d bm hm,
@@ -177,9 +189,9 @@ Lemma GLog_read_orp:
 
 
 Lemma LOG_read_orp:
-  forall pr d bm hm lxp a Fr ds sm ms Ftop vs,
+  forall pr d bm hm lxp a Fr ds dx sm ms Ftop vs,
     (Fr * [[ sync_invariant Fr ]] *
-     LOG.rep lxp Ftop (LOG.ActiveTxn ds ds !!) ms sm bm hm *
+     LOG.rep lxp Ftop (LOG.ActiveTxn ds dx) ms sm bm hm *
      [[[ ds!! ::: exists F', (F' * a |-> vs) ]]])%pred d ->
     can_access pr (fst (fst vs)) ->
     only_reads_permitted pr (LOG.read lxp a ms) d bm hm.
@@ -195,3 +207,127 @@ Proof.
   simpl; auto.
 Qed.
 
+
+Theorem INODE_IRec_get_orp:
+forall Fr Fm F m sm items ms cache ix xp lxp pr d bm hm,
+  (Fr * [[ sync_invariant Fr ]] *
+   LOG.rep lxp F (LOG.ActiveTxn m m!!) ms sm bm hm *
+   [[ ix < length items ]] *
+   [[[ m!! ::: Fm * INODE.IRec.rep xp items cache ]]])%pred d ->
+  only_reads_permitted pr (INODE.IRec.get lxp xp ix cache ms) d bm hm.
+Proof.
+  intros; unfold INODE.IRec.get; simpl; intuition.
+  destruct (INODE.IRec.Cache.find ix cache) eqn:D;
+  setoid_rewrite D; simpl; intuition.
+  unfold INODE.IRec.rep, INODE.IRec.LRA.rep in *.
+  destruct_lift H.
+  
+  eapply arrayN_selN with (a:= (INODE.IRecSig.RAStart xp + ix / INODE.IRecSig.items_per_val)) in H0 as Hx; try omega.
+  rewrite minus_plus in Hx.
+  setoid_rewrite synced_list_selN in Hx.
+  setoid_rewrite selN_combine in Hx.
+  rewrite repeat_selN in Hx.
+  erewrite list2nmem_sel_inb in Hx.
+  cleanup.
+  erewrite <- LogReplay.replay_disk_empty with (d:= m!!)in H0; eauto. 
+  eapply LOG_read_orp; eauto.
+  pred_apply; cancel.
+  eexists; eapply list2nmem_ptsto_cancel.
+  eapply LOG.write_range_length_ok; eauto.
+  rewrite synced_list_length;
+  setoid_rewrite combine_length_eq; rewrite repeat_length; eauto.
+  rewrite INODE.IRec.LRA.Defs.ipack_length; apply Rounding.div_lt_divup; eauto.
+  apply INODE.IRec.Defs.items_per_val_not_0.
+  rewrite H2; simpl; auto.
+  apply AddrMap.Map.empty_1.
+  erewrite <- LogReplay.replay_disk_empty with (d:= m!!)in H0; eauto.
+  eapply LOG.write_range_length_ok; eauto.
+  rewrite synced_list_length;
+  setoid_rewrite combine_length_eq; rewrite repeat_length; eauto.
+  rewrite INODE.IRec.LRA.Defs.ipack_length; apply Rounding.div_lt_divup; eauto.
+  apply INODE.IRec.Defs.items_per_val_not_0.
+  apply AddrMap.Map.empty_1.
+  rewrite INODE.IRec.LRA.Defs.ipack_length; apply Rounding.div_lt_divup; eauto.
+  apply INODE.IRec.Defs.items_per_val_not_0.
+  apply repeat_length.
+  rewrite synced_list_length;
+  setoid_rewrite combine_length_eq; rewrite repeat_length; eauto.
+  apply plus_lt_compat_l;  rewrite INODE.IRec.LRA.Defs.ipack_length;
+  apply Rounding.div_lt_divup; eauto.
+  apply INODE.IRec.Defs.items_per_val_not_0.
+
+  Unshelve.
+  exact Public.
+  exact $0.
+  exact valuset0.
+Qed.
+
+Theorem DIRTREE_getowner_orp:
+forall Fr Fm Ftop ds sm pathname f tree mscs fsxp ilist frees pr inum d bm hm,
+  (Fr * [[ sync_invariant Fr ]] *
+     LOG.rep (FSXPLog fsxp) (SB.rep fsxp) (LOG.ActiveTxn ds ds!!) (MSLL mscs) sm bm hm *
+      [[[ ds!! ::: (Fm * rep fsxp Ftop tree ilist frees mscs sm)]]] *
+      [[ find_subtree pathname tree = Some (TreeFile inum f) ]])%pred d ->
+  only_reads_permitted pr (DIRTREE.getowner fsxp inum mscs) d bm hm.
+Proof.
+  intros; unfold getowner; simpl; intuition.
+  unfold rep, BFILE.rep, INODE.rep in *; destruct_lift H.
+  eapply INODE_IRec_get_orp; eauto.
+  pred_apply; cancel.
+  rewrite listmatch_length_pimpl in H0; destruct_lift H0.
+  rewrite listmatch_length_pimpl with (a:= dummy) in H0; destruct_lift H0.
+  setoid_rewrite <- H15.
+  rewrite subtree_extract in H8; eauto.
+  unfold tree_pred in H8; simpl in *.
+  destruct_lift H8.
+  rewrite combine_length_eq; eauto.
+  setoid_rewrite <- H16.
+  eapply list2nmem_inbound; pred_apply; cancel.
+Qed.
+
+
+Theorem INODE_getbnum_orp:
+forall Fr Fm F Fi ino lxp xp IFs bxp m sm ilist pr inum cache ms d bm hm off,
+  (Fr * [[ sync_invariant Fr ]] *
+     LOG.rep lxp F (LOG.ActiveTxn m m!!) ms sm bm hm *
+     [[ off < length (INODE.IBlocks ino) ]] *
+     [[[ m!! ::: (Fm * INODE.rep bxp IFs xp ilist cache) ]]] *
+     [[[ ilist ::: (Fi * inum |-> ino) ]]])%pred d ->
+  only_reads_permitted pr (INODE.getbnum lxp xp inum off cache ms) d bm hm.
+Proof.
+  intros; unfold INODE.getbnum.
+  apply bind_orp; intros.
+  simpl; intuition.
+  unfold INODE.rep in *.
+  destruct_lift H.
+  eapply INODE_IRec_get_orp; eauto.
+  pred_apply; cancel.
+  rewrite listmatch_length_pimpl in H1; destruct_lift H1.
+  setoid_rewrite <- H8.
+  rewrite combine_length_eq; eauto.
+  eapply list2nmem_inbound; eauto.
+
+  simpl; intuition.
+  (** extract postcondition for INODE.IRec.get_array **)
+  admit.
+Admitted.
+
+
+Theorem DIRTREE_dwrite_orp:
+forall Fr Fm Ftop Fd vs ds sm pathname f tree mscs fsxp ilist frees pr inum d bm hm off h,
+  (Fr * [[ sync_invariant Fr ]] *
+     LOG.rep (FSXPLog fsxp) (SB.rep fsxp) (LOG.ActiveTxn ds ds!!) (MSLL mscs) sm bm hm *
+      [[[ ds!! ::: (Fm * rep fsxp Ftop tree ilist frees mscs sm)]]] *
+      [[ find_subtree pathname tree = Some (TreeFile inum f) ]] *
+      [[[ (DFData f) ::: (Fd * off |-> vs) ]]])%pred d ->
+  only_reads_permitted pr (DIRTREE.dwrite fsxp inum off h mscs) d bm hm.
+Proof.
+  intros; unfold dwrite.
+  apply bind_orp; [|simpl; intuition].
+  unfold BFILE.dwrite.
+  apply bind_orp; intros.
+
+  unfold rep, BFILE.rep in *; destruct_lift H.
+  eapply INODE_getbnum_orp; eauto.
+  pred_apply; cancel.
+Abort.
