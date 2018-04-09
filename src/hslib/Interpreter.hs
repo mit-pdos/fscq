32747481044ba342a0c03dc6b28c77fd5991603c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-04-09 13:39:22 -0400

    fix up Interpreter.hs
    enough runtime for mkfs
    
    still need to fix up fscq.hs

--- src/hslib/Interpreter.hs
+++ src/hslib/Interpreter.hs
@@ -1,12 +1,12 @@
 module Interpreter where
 
-import Prog
+import PermProg
 import qualified Disk
 import Word
 import Data.Word
 -- import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.Digest.CRC32 as CRC32
-import System.CPUTime.Rdtsc
+-- import System.CPUTime.Rdtsc
 
 -- import qualified System.Exit
 -- import qualified System.Random
@@ -49,7 +49,7 @@ crc32_word_update c sz (W w) = do
 crc32_word_update c sz (W64 w) = crc32_word_update c sz $ W $ fromIntegral w
 crc32_word_update c _ (WBS bs) = return $ CRC32.crc32Update c bs
 
-run_dcode :: Disk.DiskState -> Prog.Coq_prog a -> IO a
+run_dcode :: Disk.DiskState -> PermProg.Coq_prog a -> IO a
 run_dcode _ (Ret r) = do
   debugmsg $ "Done"
   return r
@@ -65,41 +65,41 @@ run_dcode ds Sync = do
   debugmsg $ "Sync"
   Disk.sync_disk ds
   return $ unsafeCoerce ()
-run_dcode ds (Trim a) = do
-  debugmsg $ "Trim " ++ (show a)
-  Disk.trim_disk ds a
-  return $ unsafeCoerce ()
-run_dcode ds (VarAlloc v) = do
-  debugmsg $ "VarAlloc"
-  i <- Disk.var_alloc ds v
-  return $ unsafeCoerce i
-run_dcode ds (VarGet i) = do
-  debugmsg $ "VarGet " ++ (show i)
-  val <- Disk.var_get ds i
-  return $ unsafeCoerce val
-run_dcode ds (VarSet i v) = do
-  debugmsg $ "VarSet " ++ (show i)
-  Disk.var_set ds i v
-  return $ unsafeCoerce ()
-run_dcode ds (VarDelete i) = do
-  debugmsg $ "VarDelete " ++ (show i)
-  Disk.var_delete ds i
-  return $ unsafeCoerce ()
-run_dcode _ AlertModified = do
-  debugmsg $ "AlertModified"
-  return $ unsafeCoerce ()
-run_dcode _ (Debug s n) = do
-  if output then do
-    putStrLn $ s ++ " " ++ (show n)
-    return $ unsafeCoerce ()
-  else
-    return $ unsafeCoerce ()
-run_dcode _ (Rdtsc) = do
-  if timing then do
-    r <- rdtsc
-    return $ unsafeCoerce r
-  else
-    return $ unsafeCoerce ()
+-- run_dcode ds (Trim a) = do
+--   debugmsg $ "Trim " ++ (show a)
+--   Disk.trim_disk ds a
+--   return $ unsafeCoerce ()
+-- run_dcode ds (VarAlloc v) = do
+--   debugmsg $ "VarAlloc"
+--   i <- Disk.var_alloc ds v
+--   return $ unsafeCoerce i
+-- run_dcode ds (VarGet i) = do
+--   debugmsg $ "VarGet " ++ (show i)
+--   val <- Disk.var_get ds i
+--   return $ unsafeCoerce val
+-- run_dcode ds (VarSet i v) = do
+--   debugmsg $ "VarSet " ++ (show i)
+--   Disk.var_set ds i v
+--   return $ unsafeCoerce ()
+-- run_dcode ds (VarDelete i) = do
+--   debugmsg $ "VarDelete " ++ (show i)
+--   Disk.var_delete ds i
+--   return $ unsafeCoerce ()
+-- run_dcode _ AlertModified = do
+--   debugmsg $ "AlertModified"
+--   return $ unsafeCoerce ()
+-- run_dcode _ (Debug s n) = do
+--   if output then do
+--     putStrLn $ s ++ " " ++ (show n)
+--     return $ unsafeCoerce ()
+--   else
+--     return $ unsafeCoerce ()
+-- run_dcode _ (Rdtsc) = do
+--   if timing then do
+--     r <- rdtsc
+--     return $ unsafeCoerce r
+--   else
+--     return $ unsafeCoerce ()
 run_dcode _ (Hash sz w) = do
   debugmsg $ "Hash " ++ (show sz)
   c <- crc32_word_update 0 sz w
@@ -114,6 +114,13 @@ run_dcode ds (Bind p1 p2) = do
   r1 <- run_dcode ds p1
   r2 <- run_dcode ds (p2 r1)
   return r2
+run_dcode _ (Seal _ b) = do
+  return $ unsafeCoerce b
+run_dcode _ (Unseal h) = do
+  return $ unsafeCoerce h
+run_dcode _ (Auth t) = do
+  -- XXX
+  return $ unsafeCoerce True
 
-run :: Disk.DiskState -> Prog.Coq_prog a -> IO a
+run :: Disk.DiskState -> PermProg.Coq_prog a -> IO a
 run ds p = run_dcode ds p
