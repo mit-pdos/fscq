443c805953320c60a76135057d6225337f7d2c1c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2018-08-14 08:52:02 -0400

    build Atalay's branch with v8.8.1

--- builder/run.sh
+++ builder/run.sh
@@ -10,7 +10,7 @@ $THISDIR/run-one.sh /usr/local/coq-v86/bin master master-coq86
 # $THISDIR/run-one.sh /usr/local/coq-trunk/bin master master-coqtrunk
 # $THISDIR/run-one.sh /usr/local/coq-trunk/bin multicore-reads multicore-reads-coqtrunk
 $THISDIR/run-one.sh /usr/local/coq-trunk/bin security security-coqtrunk
-$THISDIR/run-one.sh /usr/local/coq-trunk/bin Security2 security2-coqtrunk
+$THISDIR/run-one.sh /usr/local/coq-v881/bin Security2 security2-coqtrunk
 # $THISDIR/run-one.sh /usr/local/coq-trunk/bin byteCP byteCP-coqtrunk
 # $THISDIR/run-one.sh /usr/local/coq-trunk/bin io-concur io-concur-coqtrunk
 # $THISDIR/run-one.sh /usr/local/coq-trunk/bin new-extract new-extract-coqtrunk
