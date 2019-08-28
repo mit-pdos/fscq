59bbd4c31b21d012bce375bda9103c41a6d22a17
Tej Chajed <tchajed@mit.edu>
2019-08-28 16:09:32 -0400

    Print disk layout information in mkfs

--- src/mkfs.hs
+++ src/mkfs.hs
@@ -21,8 +21,14 @@ main = do
       res <- I.run ds $ AsyncFS._AFS__mkfs cachesize 4 1 256
       case res of
         Errno.Err _ -> error $ "mkfs failed"
-        Errno.OK (_, fsxp) ->
+        Errno.OK (_, fsxp) -> do
           putStrLn $ "Initialization OK, " ++ (show $ coq_FSXPMaxBlock fsxp) ++ " blocks"
+          putStrLn $ "  === disk layout addresses ==="
+          putStrLn $ "  superblock: 0"
+          putStrLn $ "  data blocks start: 1"
+          putStrLn $ "  inode blocks start: " ++ (show . (+1) . coq_IXStart . coq_FSXPInode $ fsxp)
+          putStrLn $ "  log header: " ++ (show . coq_LogHeader . coq_FSXPLog $ fsxp)
+          putStrLn $ "  log data start: " ++ (show . coq_LogData . coq_FSXPLog $ fsxp)
 
       stats <- close_disk ds
       print_stats stats
